70ca9869b1fe93b2249a1d98cf4b02a6e6f08edb
Lucaskyy <contact@sculas.xyz>
2022-03-21 21:07:05 +0100

    style: reformat code

--- src/main/kotlin/net/revanced/patches/layouts/MinimizedPlayback.kt
+++ src/main/kotlin/net/revanced/patches/layouts/MinimizedPlayback.kt
@@ -5,7 +5,7 @@ import net.revanced.patcher.patch.Patch
 import net.revanced.patcher.patch.PatchResult
 import net.revanced.patcher.patch.PatchResultSuccess
 
-class MinimizedPlayback: Patch("minimized-playback") {
+class MinimizedPlayback : Patch("minimized-playback") {
     override fun execute(cache: Cache): PatchResult {
         cache.methods["minimized-playback-manager"].method.instructions.clear()
         return PatchResultSuccess()
