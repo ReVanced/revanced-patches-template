bdd7cc45ba5f1033d30339d35ae0edd1651d1ffb
Lucaskyy <contact@sculas.xyz>
2022-03-21 21:06:47 +0100

    docs: add docs to `Index` & `patches`
    also typed the patches array

--- src/main/kotlin/net/revanced/patches/Index.kt
+++ src/main/kotlin/net/revanced/patches/Index.kt
@@ -1,13 +1,21 @@
 package net.revanced.patches
 
+import net.revanced.patcher.patch.Patch
 import net.revanced.patches.ads.VideoAds
 import net.revanced.patches.layouts.CreateButtonRemover
 import net.revanced.patches.layouts.MinimizedPlayback
+import kotlin.reflect.KClass
 
-// This object contains all the patches and should be imported when using this library
+/**
+ * Index contains all the patches and should be imported when using this library.
+ */
+@Suppress("Unused")
 object Index {
-    // Array of patches. New patches should be added to the array
-    val patches = arrayOf(
+    /**
+     * Array of patches.
+     * New patches should be added to the array.
+     */
+    val patches: Array<KClass<out Patch>> = arrayOf(
         VideoAds::class,
         MinimizedPlayback::class,
         CreateButtonRemover::class
