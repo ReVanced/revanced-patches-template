08af6e54af79ef9ef4fb3372a348ce9b6fba4d20
Lucaskyy <contact@sculas.xyz>
2022-03-22 22:24:56 +0100

    feat: use supplier instead of KClass
    BREAKING CHANGE: signature of patches was changed

--- src/main/kotlin/app/revanced/patches/Index.kt
+++ src/main/kotlin/app/revanced/patches/Index.kt
@@ -4,7 +4,6 @@ import app.revanced.patcher.patch.Patch
 import app.revanced.patches.ad.VideoAds
 import app.revanced.patches.interaction.EnableSeekbarTapping
 import app.revanced.patches.layout.*
-import kotlin.reflect.KClass
 
 /**
  * Index contains all the patches and should be imported when using this library.
@@ -15,13 +14,13 @@ object Index {
      * Array of patches.
      * New patches should be added to the array.
      */
-    val patches: Array<KClass<out Patch>> = arrayOf(
-        VideoAds::class,
-        MinimizedPlayback::class,
-        CreateButtonRemover::class,
-        HideReels::class,
-        HideSuggestions::class,
-        OldQualityLayout::class,
-        EnableSeekbarTapping::class
+    val patches: Array<() -> Patch> = arrayOf(
+        ::VideoAds,
+        ::MinimizedPlayback,
+        ::CreateButtonRemover,
+        ::HideReels,
+        ::HideSuggestions,
+        ::OldQualityLayout,
+        ::EnableSeekbarTapping
     )
 }
