0da15fb0effac0566d080d7b85e9fbe46c3dd34d
oSumAtrIX <johan.melkonyan1@web.de>
2022-06-14 01:31:37 +0200

    fix: environment variable not found in gradle build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,22 +4,19 @@ plugins {
 
 group = "app.revanced"
 
-val githubUsername: String = project.findProperty("gpr.user") as? String ?: System.getenv("GITHUB_ACTOR")
-val githubPassword: String = project.findProperty("gpr.key") as? String ?: System.getenv("GITHUB_TOKEN")
-
 repositories {
     mavenCentral()
     maven {
         url = uri("https://maven.pkg.github.com/revanced/revanced-patcher")
         credentials {
-            username = githubUsername
-            password = githubPassword
+            username = project.findProperty("gpr.user") as? String ?: System.getenv("GITHUB_ACTOR")
+            password = project.findProperty("gpr.key")  as? String ?: System.getenv("GITHUB_TOKEN")
         }
     }
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.0")
+    implementation(kotlin("stdlib"))
 
     implementation("app.revanced:revanced-patcher:1.1.0")
 }
