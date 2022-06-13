1dc768f20fe367bbe86e35cb58b33e91f73f7670
oSumAtrIX <johan.melkonyan1@web.de>
2022-06-14 01:03:27 +0200

    chore: publish releases instead of packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,19 @@
 plugins {
-    kotlin("jvm") version "1.6.21"
-    java
-    `maven-publish`
+    kotlin("jvm") version "1.7.0"
 }
 
 group = "app.revanced"
 
+val githubUsername: String = project.findProperty("gpr.user") as? String ?: System.getenv("GITHUB_ACTOR")
+val githubPassword: String = project.findProperty("gpr.key") as? String ?: System.getenv("GITHUB_TOKEN")
+
 repositories {
     mavenCentral()
-    mavenLocal()
     maven {
-        url = uri("https://maven.pkg.github.com/revanced/revanced-patcher") // note the "r"!
+        url = uri("https://maven.pkg.github.com/revanced/revanced-patcher")
         credentials {
-            // DO NOT set these variables in the project's gradle.properties.
-            // Instead, you should set them in:
-            // Windows: %homepath%\.gradle\gradle.properties
-            // Linux: ~/.gradle/gradle.properties
-            username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR") // DO NOT CHANGE!
-            password = project.findProperty("gpr.key")  as String? ?: System.getenv("GITHUB_TOKEN") // DO NOT CHANGE!
+            username = githubUsername
+            password = githubPassword
         }
     }
 }
@@ -26,34 +22,4 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.21")
 
     implementation("app.revanced:revanced-patcher:1.1.0")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.21")
-}
-
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
-
-val isGitHubCI = System.getenv("GITHUB_ACTOR") != null
-
-publishing {
-    repositories {
-        if (isGitHubCI) {
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/revanced/revanced-patches") // note the "s"!
-                credentials {
-                    username = System.getenv("GITHUB_ACTOR")
-                    password = System.getenv("GITHUB_TOKEN")
-                }
-            }
-        } else {
-            mavenLocal()
-        }
-    }
-    publications {
-        register<MavenPublication>("gpr") {
-            from(components["java"])
-        }
-    }
 }
