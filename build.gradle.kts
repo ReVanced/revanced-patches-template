f6a8911906dfe52fcdb685daf7a02d6d0052cba9
Lucaskyy <contact@sculas.xyz>
2022-06-16 11:54:39 +0200

    fix: dummy task for Gradle semantic-release plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,3 +20,13 @@ dependencies {
 
     implementation("app.revanced:revanced-patcher:1.1.0")
 }
+
+tasks {
+    // Dummy task to fix the Gradle semantic-release plugin.
+    // Remove this if you forked it to support building only.
+    // Tracking issue: https://github.com/KengoTODA/gradle-semantic-release-plugin/issues/435
+    register<DefaultTask>("publish") {
+        group = "publish"
+        description = "Dummy task"
+    }
+}
