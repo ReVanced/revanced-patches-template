ed6a398403540b9deb8b7b4d7e0701b110769acc
TheJeterLP <joey.peter1998@gmail.com>
2022-06-22 06:41:00 +0200

    add pivotbarEnum back

--- src/main/kotlin/app/revanced/patches/youtube/layout/shorts/button/patch/ShortsButtonRemoverPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/shorts/button/patch/ShortsButtonRemoverPatch.kt
@@ -11,6 +11,7 @@ import app.revanced.patcher.patch.implementation.misc.PatchResult
 import app.revanced.patcher.patch.implementation.misc.PatchResultSuccess
 import app.revanced.patcher.util.smali.toInstruction
 import app.revanced.patches.youtube.layout.shorts.button.annotations.ShortsButtonCompatibility
+import app.revanced.patches.youtube.layout.shorts.button.signatures.PivotBarButtonTabenumSignature
 import app.revanced.patches.youtube.layout.shorts.button.signatures.PivotBarButtonsViewSignature
 import app.revanced.patches.youtube.misc.integrations.patch.IntegrationsPatch
 import org.jf.dexlib2.iface.instruction.formats.Instruction11x
@@ -22,18 +23,31 @@ import org.jf.dexlib2.iface.instruction.formats.Instruction11x
 @ShortsButtonCompatibility
 @Version("0.0.1")
 class ShortsButtonRemoverPatch : BytecodePatch(
-    listOf(PivotBarButtonsViewSignature)
+    listOf(
+        PivotBarButtonTabenumSignature, PivotBarButtonsViewSignature
+    )
 ) {
     override fun execute(data: BytecodeData): PatchResult {
-        val result1 = PivotBarButtonsViewSignature.result!!
+        val result1 = PivotBarButtonTabenumSignature.result!!
         val implementation1 = result1.method.implementation!!
-        val moveViewInstruction = implementation1.instructions[result1.scanResult.endIndex]
+        val moveEnumInstruction = implementation1.instructions[result1.scanResult.endIndex]
+        val enumRegister = (moveEnumInstruction as Instruction11x).registerA
+
+        val result2 = PivotBarButtonsViewSignature.result!!
+        val implementation2 = result2.method.implementation!!
+        val moveViewInstruction = implementation2.instructions[result2.scanResult.endIndex]
         val viewRegister = (moveViewInstruction as Instruction11x).registerA
+
+        // Save the tab enum in XGlobals to avoid smali/register workarounds
+        implementation1.addInstruction(
+            result1.scanResult.endIndex + 1,
+            "sput-object v$enumRegister, Lapp/revanced/integrations/settings/Settings;->lastPivotTab:Ljava/lang/Enum;".toInstruction()
+        )
 
         // Hide the button view via proxy by passing it to the hideShortsButton method
         // It only hides it if the last tab name is "TAB_SHORTS"
-        implementation1.addInstruction(
-            result1.scanResult.endIndex + 1,
+        implementation2.addInstruction(
+            result2.scanResult.endIndex + 2,
             "invoke-static { v$viewRegister }, Lapp/revanced/integrations/adremover/HideShortsButtonPatch;->hideShortsButton(Landroid/view/View;)V".toInstruction()
         )
 
