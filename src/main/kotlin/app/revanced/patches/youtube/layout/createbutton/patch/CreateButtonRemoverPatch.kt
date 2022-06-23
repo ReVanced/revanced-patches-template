e2cdb8c9c7fa43da042c9a32842edaf3fa78509f
bogadana <30848157+bogadana@users.noreply.github.com>
2022-06-23 16:52:42 +0200

    fix: find register by more reliable means

--- src/main/kotlin/app/revanced/patches/youtube/layout/createbutton/patch/CreateButtonRemoverPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/createbutton/patch/CreateButtonRemoverPatch.kt
@@ -4,21 +4,25 @@ import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.data.implementation.BytecodeData
-import app.revanced.patcher.extensions.addInstruction
 import app.revanced.patcher.patch.annotations.Dependencies
 import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patcher.patch.implementation.BytecodePatch
 import app.revanced.patcher.patch.implementation.misc.PatchResult
 import app.revanced.patcher.patch.implementation.misc.PatchResultError
 import app.revanced.patcher.patch.implementation.misc.PatchResultSuccess
+import app.revanced.patcher.util.smali.toInstruction
 import app.revanced.patches.youtube.layout.createbutton.annotations.CreateButtonCompatibility
 import app.revanced.patches.youtube.layout.createbutton.signatures.CreateButtonSignature
 import app.revanced.patches.youtube.misc.integrations.patch.IntegrationsPatch
+import app.revanced.patches.youtube.misc.mapping.patch.ResourceIdMappingProviderResourcePatch
 import org.jf.dexlib2.Opcode
 import org.jf.dexlib2.iface.instruction.formats.Instruction35c
+import org.jf.dexlib2.iface.instruction.formats.Instruction31i
+import org.jf.dexlib2.iface.instruction.formats.Instruction11x
+import org.jf.dexlib2.iface.reference.MethodReference
 
 @Patch
-@Dependencies(dependencies = [IntegrationsPatch::class])
+@Dependencies(dependencies = [IntegrationsPatch::class, ResourceIdMappingProviderResourcePatch::class])
 @Name("disable-create-button")
 @Description("Disable the create button.")
 @CreateButtonCompatibility
@@ -33,14 +37,22 @@ class CreateButtonRemoverPatch : BytecodePatch(
 
         // Get the required register which holds the view object we need to pass to the method hideCreateButton
         val implementation = result.method.implementation!!
-        val instruction = implementation.instructions[result.scanResult.endIndex + 1]
-        if (instruction.opcode != Opcode.INVOKE_STATIC) return PatchResultError("Could not find the correct register")
-        val register = (instruction as Instruction35c).registerC
+
+        val imageOnlyLayout = ResourceIdMappingProviderResourcePatch.resourceMappings.get("image_only_tab")?.toInt()
+        val imageOnlyLayoutConstIndex =  implementation.instructions.indexOfFirst { (it as? Instruction31i)?.narrowLiteral == imageOnlyLayout }
+
+        val (instructionIndex, instruction) = implementation.instructions.drop(imageOnlyLayoutConstIndex).withIndex()
+            .first { (((it.value as? Instruction35c)?.reference) as? MethodReference)?.definingClass?.contains("PivotBar") ?: false }
+
+        if (instruction.opcode != Opcode.INVOKE_VIRTUAL) return PatchResultError("Could not find the correct instruction")
+
+        val moveResultIndex = imageOnlyLayoutConstIndex + instructionIndex + 1
+        val moveResultInstruction = implementation.instructions[moveResultIndex] as Instruction11x
 
         // Hide the button view via proxy by passing it to the hideCreateButton method
-        result.method.addInstruction(
-            result.scanResult.endIndex + 1,
-            "invoke-static { v$register }, Lfi/razerman/youtube/XAdRemover;->hideCreateButton(Landroid/view/View;)V"
+        implementation.addInstruction(
+            moveResultIndex + 1,
+            "invoke-static { v${moveResultInstruction.registerA} }, Lfi/razerman/youtube/XAdRemover;->hideCreateButton(Landroid/view/View;)V".toInstruction()
         )
 
         return PatchResultSuccess()
