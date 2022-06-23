a3ddf52587878d0a7d0621382fa22cf4933cacab
oSumAtrIX <johan.melkonyan1@web.de>
2022-06-24 00:03:38 +0200

    refactor: instruction casts

--- src/main/kotlin/app/revanced/patches/youtube/layout/createbutton/patch/CreateButtonRemoverPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/createbutton/patch/CreateButtonRemoverPatch.kt
@@ -4,21 +4,21 @@ import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.data.implementation.BytecodeData
+import app.revanced.patcher.extensions.addInstruction
 import app.revanced.patcher.patch.annotations.Dependencies
 import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patcher.patch.implementation.BytecodePatch
 import app.revanced.patcher.patch.implementation.misc.PatchResult
 import app.revanced.patcher.patch.implementation.misc.PatchResultError
 import app.revanced.patcher.patch.implementation.misc.PatchResultSuccess
-import app.revanced.patcher.extensions.addInstruction
 import app.revanced.patches.youtube.layout.createbutton.annotations.CreateButtonCompatibility
 import app.revanced.patches.youtube.layout.createbutton.signatures.CreateButtonSignature
 import app.revanced.patches.youtube.misc.integrations.patch.IntegrationsPatch
 import app.revanced.patches.youtube.misc.mapping.patch.ResourceIdMappingProviderResourcePatch
 import org.jf.dexlib2.Opcode
-import org.jf.dexlib2.iface.instruction.formats.Instruction35c
-import org.jf.dexlib2.iface.instruction.formats.Instruction31i
-import org.jf.dexlib2.iface.instruction.formats.Instruction11x
+import org.jf.dexlib2.iface.instruction.OneRegisterInstruction
+import org.jf.dexlib2.iface.instruction.ReferenceInstruction
+import org.jf.dexlib2.iface.instruction.WideLiteralInstruction
 import org.jf.dexlib2.iface.reference.MethodReference
 
 @Patch
@@ -38,16 +38,19 @@ class CreateButtonRemoverPatch : BytecodePatch(
         // Get the required register which holds the view object we need to pass to the method hideCreateButton
         val implementation = result.method.implementation!!
 
-        val imageOnlyLayout = ResourceIdMappingProviderResourcePatch.resourceMappings.get("image_only_tab")?.toInt()
-        val imageOnlyLayoutConstIndex =  implementation.instructions.indexOfFirst { (it as? Instruction31i)?.narrowLiteral == imageOnlyLayout }
+        val imageOnlyLayout = ResourceIdMappingProviderResourcePatch.resourceMappings["image_only_tab"]
+            ?: return PatchResultError("Required resource could not be found in the map")
+
+        val imageOnlyLayoutConstIndex =
+            implementation.instructions.indexOfFirst { (it as? WideLiteralInstruction)?.wideLiteral == imageOnlyLayout }
 
         val (instructionIndex, instruction) = implementation.instructions.drop(imageOnlyLayoutConstIndex).withIndex()
-            .first { (((it.value as? Instruction35c)?.reference) as? MethodReference)?.definingClass?.contains("PivotBar") ?: false }
+            .first { (((it.value as? ReferenceInstruction)?.reference) as? MethodReference)?.definingClass?.contains("PivotBar") ?: false }
 
         if (instruction.opcode != Opcode.INVOKE_VIRTUAL) return PatchResultError("Could not find the correct instruction")
 
         val moveResultIndex = imageOnlyLayoutConstIndex + instructionIndex + 1
-        val moveResultInstruction = implementation.instructions[moveResultIndex] as Instruction11x
+        val moveResultInstruction = implementation.instructions[moveResultIndex] as OneRegisterInstruction
 
         // Hide the button view via proxy by passing it to the hideCreateButton method
         result.method.addInstruction(
