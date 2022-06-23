955766917a96fc8cee405086bcde81ffc2502c62
pranshoe <pranshu.goyal71@gmail.com>
2022-06-23 08:36:29 +0000

    refactor: nuke non-existent resource `yt_status_bar_background_light`  from `materialyou`

--- src/main/kotlin/app/revanced/patches/youtube/layout/materialyou/patch/MaterialYouPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/materialyou/patch/MaterialYouPatch.kt
@@ -36,7 +36,7 @@ class MaterialYouPatch : ResourcePatch() {
                 val element = resourcesNode.childNodes.item(i) as Element
                 element.textContent = when (element.getAttribute("name")) {
                     "yt_black1", "yt_black1_opacity95", "yt_black2", "yt_black3", "yt_black4", "yt_status_bar_background_dark" -> "@android:color/system_neutral1_900"
-                    "yt_white1", "yt_white1_opacity95", "yt_white2", "yt_white3", "yt_white4", "yt_status_bar_background_light" -> "@android:color/system_neutral1_10"
+                    "yt_white1", "yt_white1_opacity95", "yt_white2", "yt_white3", "yt_white4" -> "@android:color/system_neutral1_10"
                     "yt_selected_nav_label_dark", "yt_selected_nav_label_light" -> "#ffdf0000"
                     else -> continue
                 }
