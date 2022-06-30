98c8a548984beddb7d9d63779f7df36b767baec2
bogadana <30848157+bogadana@users.noreply.github.com>
2022-06-30 19:09:23 +0200

    fix: make panels not freeze when video changed in fullscreen

--- src/main/kotlin/app/revanced/patches/youtube/layout/fullscreenpanels/patch/FullscreenPanelsRemoverPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/fullscreenpanels/patch/FullscreenPanelsRemoverPatch.kt
@@ -4,31 +4,47 @@ import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.data.impl.BytecodeData
-import app.revanced.patcher.extensions.addInstructions
-import app.revanced.patcher.patch.annotations.Patch
-import app.revanced.patcher.patch.impl.BytecodePatch
+import app.revanced.patcher.extensions.addInstruction
+import app.revanced.patcher.extensions.removeInstruction
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.patch.impl.BytecodePatch
 import app.revanced.patches.youtube.layout.fullscreenpanels.annotations.FullscreenPanelsCompatibility
+import app.revanced.patches.youtube.layout.fullscreenpanels.fingerprints.FullscreenViewAdderFingerprint
+import org.jf.dexlib2.Opcode
+import org.jf.dexlib2.iface.instruction.FiveRegisterInstruction
+import org.jf.dexlib2.iface.instruction.ReferenceInstruction
+import org.jf.dexlib2.iface.reference.MethodReference
 
 @Patch
 @Name("disable-fullscreen-panels")
 @Description("Disable comments panel in fullscreen view.")
 @FullscreenPanelsCompatibility
 @Version("0.0.1")
-class FullscreenPanelsRemovalPatch : BytecodePatch(listOf()) {
+class FullscreenPanelsRemovalPatch : BytecodePatch(
+    listOf(
+        FullscreenViewAdderFingerprint
+    )
+) {
     override fun execute(data: BytecodeData): PatchResult {
-        val classDef = data.classes.first { it.type.endsWith("FullscreenEngagementPanelOverlay;") }
-        val method = data.proxy(classDef).resolve().methods.first { it.name == "<init>" }
+        val method = FullscreenViewAdderFingerprint.result?.mutableMethod!!
         val implementation = method.implementation!!
 
-        method.addInstructions(
-            implementation.instructions.count() - 1,
-            """
-			 const/4 v1, 0x0
-             iput-boolean v1, v0, ${classDef.type}->a:Z
-       		"""
-        )
+        val (visibilityCallIndex, visibilityCall) =
+            implementation.instructions.withIndex()
+                .first { ((it.value as? ReferenceInstruction)?.reference as? MethodReference)?.name == ("setVisibility") }
+
+        val gotoIndex =
+            implementation.instructions.subList(0, visibilityCallIndex).indexOfLast { it.opcode == Opcode.GOTO }
+
+        //force the if
+        method.removeInstruction(gotoIndex)
+
+        val visibilityIntRegister = (visibilityCall as FiveRegisterInstruction).registerD
+
+        //set the visibility to GONE
+        method.addInstruction(visibilityCallIndex - 1, "const/16 v$visibilityIntRegister, 0x8")
 
         return PatchResultSuccess()
     }
