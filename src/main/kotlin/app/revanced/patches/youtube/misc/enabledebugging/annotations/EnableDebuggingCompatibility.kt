bb355e7b7e78e602a10b346fe7e5795463615a81
Chris <52449218+shadow578@users.noreply.github.com>
2022-07-04 18:10:49 +0200

    feat: `enable-debugging` patch (#116)

---
+++ src/main/kotlin/app/revanced/patches/youtube/misc/enabledebugging/annotations/EnableDebuggingCompatibility.kt
@@ -0,0 +1,25 @@
+package app.revanced.patches.youtube.misc.enabledebugging.annotations
+
+import app.revanced.patcher.annotation.Compatibility
+import app.revanced.patcher.annotation.Package
+
+@Compatibility(
+    [Package(
+        "com.google.android.youtube",
+        arrayOf(
+            "17.14.35",
+            "17.17.34",
+            "17.19.36",
+            "17.20.37",
+            "17.22.36",
+            "17.23.35",
+            "17.23.36",
+            "17.24.34",
+            "17.24.35",
+            "17.25.34"
+        )
+    )]
+)
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+internal annotation class EnableDebuggingCompatibility
