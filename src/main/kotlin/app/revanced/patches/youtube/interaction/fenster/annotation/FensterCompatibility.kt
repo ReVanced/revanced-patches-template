4d84c1914f8ecf51cee25667219bc6cf635a6c1c
TheJeterLP <joey.peter1998@gmail.com>
2022-07-06 20:31:59 +0200

    fix: add 17.25.34 as supported version for swipe-controls patch

--- src/main/kotlin/app/revanced/patches/youtube/interaction/fenster/annotation/FensterCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/youtube/interaction/fenster/annotation/FensterCompatibility.kt
@@ -3,10 +3,9 @@ package app.revanced.patches.youtube.interaction.fenster.annotation
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-//TODO the patch may be compatible with more versions, but this is the one i'm testing on right now...
 @Compatibility(
     [Package(
-        "com.google.android.youtube", arrayOf("17.24.34")
+        "com.google.android.youtube", arrayOf("17.24.34", "17.25.34")
     )]
 )
 @Target(AnnotationTarget.CLASS)
