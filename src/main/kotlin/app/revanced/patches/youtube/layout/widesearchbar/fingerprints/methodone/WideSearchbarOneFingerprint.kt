4a2ddcfce48447f609199264981b3a14cdb17cd9
TheJeterLP <joey.peter1998@gmail.com>
2022-07-07 20:50:34 +0200

    create first two fingerprints

---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/widesearchbar/fingerprints/methodone/WideSearchbarOneFingerprint.kt
@@ -0,0 +1,48 @@
+package app.revanced.patches.youtube.layout.widesearchbar.fingerprints.methodone
+
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.extensions.or
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import app.revanced.patcher.fingerprint.method.annotation.FuzzyPatternScanMethod
+import app.revanced.patcher.fingerprint.method.annotation.MatchingMethod
+import app.revanced.patches.youtube.layout.reels.annotations.HideReelsCompatibility
+import org.jf.dexlib2.AccessFlags
+
+@Name("wide-searchbar-methodone-fingerprint")
+@MatchingMethod(
+    "Ljkg;", "i"
+)
+@FuzzyPatternScanMethod(3)
+@HideReelsCompatibility
+@Version("0.0.1")
+
+/*
+This finds the following method:
+public final View i(akrl akrlVar, adpd adpdVar) {
+}
+
+Method we search for is located in smali now.
+See:
+.method public final i(Lakrl;Ladpd;)Landroid/view/View;
+    iget-object v0, p0, Ljkg;->a:Landroid/app/Activity;
+    invoke-static {v0}, Landroid/view/LayoutInflater;->from(Landroid/content/Context;)Landroid/view/LayoutInflater;
+    move-result-object v0
+    iget-object v1, p0, Ljkg;->g:Ljis;
+    const/4 v2, 0x0
+    invoke-interface {v1, v2}, Ljis;->h(Z)V
+    iget-object p1, p1, Lakrl;->f:Lahnq;
+    if-nez p1, :cond_12
+    sget-object p1, Lahnq;->a:Lahnq;
+    :cond_12
+    iget-object v1, p0, Ljkg;->x:Ltxm;
+    invoke-static {v1}, Lfbn;->aF(Ltxm;)Z //THIS IS WHAT WE SEARCH FOR (Method fbn.aF)
+    move-result v1
+
+
+ */
+
+object WideSearchbarOneFingerprint : MethodFingerprint(
+    "Landroid/view/View;", AccessFlags.PUBLIC or AccessFlags.FINAL, null, null,
+    null, null
+)
--- src/main/kotlin/app/revanced/patches/youtube/layout/widesearchbar/test.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/widesearchbar/test.kt
@@ -1,4 +1,69 @@
 package app.revanced.patches.youtube.layout.widesearchbar
 
 class test {
+
+    /**
+     * get class jkg with parent-parent fp searching for strings:
+     * "FEhistory"
+     *  "FEmy_videos"
+     *  "FEpurchases"
+     */
+    /**
+     * //get this using parent fingerprint
+     *  in jkg
+     *
+     * public final View i(akrl akrlVar, adpd adpdVar) {
+    LayoutInflater from = LayoutInflater.from(this.a);
+    this.g.h(false);
+    ahnq ahnqVar = akrlVar.f;
+    if (ahnqVar == null) {
+    ahnqVar = ahnq.a;
+    }
+    HERE: if (fbn.aF(this.x)) {
+
+     */
+
+    /**
+     * class fbn
+     *
+     * //old bH method from vanced
+     *
+     * public static boolean aF(txm txmVar) {
+    akfd akfdVar = txmVar.b().e;
+    if (akfdVar == null) {
+    akfdVar = akfd.a;
+    }
+ //inject call to patch here   return akfdVar.V;
+    }
+     */
+
+
+    //second method
+    /*
+    get class krf by searching for this method in krf using the string
+    public static ies i(br brVar) {
+        bp f = brVar.getSupportFragmentManager().f("VIDEO_QUALITIES_QUICK_MENU_BOTTOM_SHEET_FRAGMENT");
+        if (f != null) {
+            return (kga) f;
+        }
+        return new kga();
+    }
+:
+    then get method
+    public static jis h(Context context, txm txmVar, uag uagVar, txp txpVar) {
+        return fbn.aB(txmVar) ? new jhx(context, txmVar, uagVar, txpVar) : jis.d;
+    }
+    in the same class by using opcodes, etc
+
+    to then finally get the method in fbn.aB
+
+    public static boolean aB(txm txmVar) {
+        akfd akfdVar = txmVar.b().e;
+        if (akfdVar == null) {
+            akfdVar = akfd.a;
+        }
+ //inject call to patch here       return akfdVar.y;
+    }
+     */
+
 }
