1f4dbf93e2194fc0e336868aa8b3852add5474f5
bogadana <30848157+bogadana@users.noreply.github.com>
2022-07-08 02:09:52 +0200

    formatting

--- src/main/kotlin/app/revanced/meta/ReadmeGenerator.kt
+++ src/main/kotlin/app/revanced/meta/ReadmeGenerator.kt
@@ -8,15 +8,13 @@ import app.revanced.patcher.extensions.PatchExtensions.compatiblePackages
 import app.revanced.patcher.extensions.PatchExtensions.patchName
 import app.revanced.patcher.extensions.PatchExtensions.description
 
-class ReadmeGenerator()
-{
-    companion object 
-    {
+class ReadmeGenerator {
+    companion object {
         @JvmStatic
         fun main(args: Array<String>) {
             //should be moved to a file?
-            val generalReadme = 
-            """
+            val generalReadme =
+                """
             # revanced-patches
 
             A repository of patches for use with ReVanced tooling
@@ -24,24 +22,24 @@ class ReadmeGenerator()
             # List of patches
             """.trimIndent()
 
-            val tableHeader = 
-            """
+            val tableHeader =
+                """
             |💊Patch|📜Description|🎯Target package|🏹Target package version|
             |:-----:|:-----------:|:--------------:|:----------------------:|
             """.trimIndent()
 
             val readmeFile = File("README.md")
-         
+
             val bundle = JarPatchBundle("build/libs/revanced-patches-2.9.4.jar").loadPatches()
 
             val builder = StringBuilder()
-            
+
             builder.appendLine(generalReadme)
             builder.appendLine(tableHeader)
 
-            for (patch in bundle) 
-            {
-                val humanName = patch.patchName.split('-').map { it.replaceFirstChar { it.uppercase() } }.joinToString(" ") 
+            for (patch in bundle) {
+                val humanName =
+                    patch.patchName.split('-').map { it.replaceFirstChar { it.uppercase() } }.joinToString(" ")
 
                 val compatiblePackage = patch.compatiblePackages?.first()
                 val latestVersion = compatiblePackage?.versions?.maxByOrNull { it.replace(".", "").toInt() } ?: "all"
