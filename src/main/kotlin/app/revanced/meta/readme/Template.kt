26bf1d818f953abc061126d8b91f17cd9008ba1d
oSumAtrIX <johan.melkonyan1@web.de>
2022-07-10 19:50:53 +0200

    fix: invalid regex

--- src/main/kotlin/app/revanced/meta/readme/Template.kt
+++ src/main/kotlin/app/revanced/meta/readme/Template.kt
@@ -4,7 +4,7 @@ class Template(template: String) {
     val result: StringBuilder = StringBuilder(template)
 
     fun replaceVariable(name: String, value: String) {
-        val regex = Regex("{{\\s?$name\\s?}}")
+        val regex = Regex("\\{\\{\\s?$name\\s?}}")
         val range = regex.find(result)!!.range
 
         result.replace(range.first, range.last + 1, value)
