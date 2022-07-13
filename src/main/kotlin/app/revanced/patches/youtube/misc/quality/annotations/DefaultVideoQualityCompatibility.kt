25cd66cc6d2cd56a4170c5c69a708011fe7eab1d
Canny1913 <94744045+Canny1913@users.noreply.github.com>
2022-07-13 23:42:23 +0300

    feat: add version 17.26.35

--- src/main/kotlin/app/revanced/patches/youtube/misc/quality/annotations/DefaultVideoQualityCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/quality/annotations/DefaultVideoQualityCompatibility.kt
@@ -5,7 +5,7 @@ import app.revanced.patcher.annotation.Package
 
 @Compatibility(
     [Package(
-        "com.google.android.youtube", arrayOf("17.22.36", "17.24.35")
+        "com.google.android.youtube", arrayOf("17.22.36", "17.24.35", "17.26.35")
     )]
 )
 @Target(AnnotationTarget.CLASS)
