8e55feedd798ab5eb9e5fd3f7ff2681f69df1dff
Palm <palmpasuthorn@gmail.com>
2022-07-17 17:28:53 +0700

    chore: update codec patch
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patches/music/audio/codecs/patch/CodecsUnlockPatch.kt
+++ src/main/kotlin/app/revanced/patches/music/audio/codecs/patch/CodecsUnlockPatch.kt
@@ -16,7 +16,7 @@ import app.revanced.patches.music.audio.codecs.fingerprints.CodecsLockFingerprin
 
 @Patch
 @Name("codecs-unlock")
-@Description("Adds more audio codec options. Changing audio codecs usually results in better audio quality but may depend on song and device.")
+@Description("Adds more audio codec options. The new audio codecs usually result in better audio quality.")
 @CodecsUnlockCompatibility
 @Version("0.0.1")
 class CodecsUnlockPatch : BytecodePatch(
