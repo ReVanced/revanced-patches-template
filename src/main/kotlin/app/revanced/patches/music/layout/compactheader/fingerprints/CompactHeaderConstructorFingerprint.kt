83753bacf8f56ad16f8abafc02034f1595c12532
Alberto Ponces <ponces26@gmail.com>
2022-07-19 00:31:01 +0100

    feat: `compact-header` patch

---
+++ src/main/kotlin/app/revanced/patches/music/layout/compactheader/fingerprints/CompactHeaderConstructorFingerprint.kt
@@ -0,0 +1,33 @@
+package app.revanced.patches.music.layout.compactheader.fingerprints
+
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.extensions.or
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import app.revanced.patcher.fingerprint.method.annotation.MatchingMethod
+import app.revanced.patches.music.layout.compactheader.annotations.CompactHeaderCompatibility
+import org.jf.dexlib2.AccessFlags
+import org.jf.dexlib2.Opcode
+
+@Name("compact-header-constructor-fingerprint")
+@MatchingMethod(
+    "Llcz;", "<init>"
+)
+@CompactHeaderCompatibility
+@Version("0.0.1")
+object CompactHeaderConstructorFingerprint : MethodFingerprint(
+    "V", AccessFlags.PUBLIC or AccessFlags.CONSTRUCTOR, listOf("L", "L", "L", "L", "L"), listOf(
+        Opcode.INVOKE_DIRECT,
+        Opcode.IPUT_OBJECT,
+        Opcode.IPUT_OBJECT,
+        Opcode.IPUT_OBJECT,
+        Opcode.NEW_INSTANCE,
+        Opcode.INVOKE_DIRECT,
+        Opcode.IPUT_OBJECT,
+        Opcode.CONST,
+        Opcode.CONST_4,
+        Opcode.INVOKE_STATIC,
+        Opcode.MOVE_RESULT_OBJECT,
+        Opcode.CHECK_CAST
+    )
+)
