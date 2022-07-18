761180749029104bd9b6c78afa8f7e92a20d1250
Alberto Ponces <ponces26@gmail.com>
2022-07-18 13:58:11 +0100

    fix: `minimize-playback` - unblocks minimized playback on kids videos

--- src/main/kotlin/app/revanced/patches/youtube/layout/minimizedplayback/fingerprints/MinimizedPlaybackKidsFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/minimizedplayback/fingerprints/MinimizedPlaybackKidsFingerprint.kt
@@ -3,34 +3,32 @@ package app.revanced.patches.youtube.layout.minimizedplayback.fingerprints
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.extensions.or
-import app.revanced.patcher.fingerprint.method.annotation.FuzzyPatternScanMethod
 import app.revanced.patcher.fingerprint.method.annotation.MatchingMethod
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import app.revanced.patches.youtube.layout.minimizedplayback.annotations.MinimizedPlaybackCompatibility
 import org.jf.dexlib2.AccessFlags
 import org.jf.dexlib2.Opcode
-import org.jf.dexlib2.iface.instruction.WideLiteralInstruction
 
-@Name("minimized-playback-manager-fingerprint")
+@Name("minimized-playback-kids-fingerprint")
 @MatchingMethod(
-    "Ladj", "w"
+    "LMinimizedPlaybackPolicyController;", "k"
 )
-@FuzzyPatternScanMethod(2)
 @MinimizedPlaybackCompatibility
 @Version("0.0.1")
-object MinimizedPlaybackSettingsFingerprint : MethodFingerprint(
-    "L",
+object MinimizedPlaybackKidsFingerprint : MethodFingerprint(
+    "V",
     AccessFlags.PUBLIC or AccessFlags.FINAL,
-    null,
+    listOf("I", "L", "L"),
     listOf(
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.MOVE_RESULT,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.MOVE_RESULT,
         Opcode.IF_EQZ,
-        Opcode.IF_NEZ,
-        Opcode.GOTO,
-        Opcode.IGET_OBJECT,
-        Opcode.CHECK_CAST
+        Opcode.SGET_OBJECT,
+        Opcode.IF_NE,
+        Opcode.CONST_4,
+        Opcode.IPUT_BOOLEAN,
+        Opcode.IF_EQZ,
+        Opcode.IGET,
+        Opcode.INVOKE_STATIC
     ),
+    null,
+    { it.definingClass.endsWith("MinimizedPlaybackPolicyController;") }
 )
