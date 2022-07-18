c364baa5e761b9166853ea0beaa471986e383066
TheJeterLP <joey.peter1998@gmail.com>
2022-07-18 18:27:21 +0200

    add settings for HideAutoplayButton Patch

--- src/main/kotlin/app/revanced/patches/youtube/layout/autoplaybutton/patch/HideAutoplayButton.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/autoplaybutton/patch/HideAutoplayButton.kt
@@ -4,7 +4,7 @@ import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.data.impl.BytecodeData
-import app.revanced.patcher.extensions.removeInstructions
+import app.revanced.patcher.extensions.addInstruction
 import app.revanced.patcher.extensions.replaceInstructions
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
@@ -15,6 +15,7 @@ import app.revanced.patches.youtube.layout.autoplaybutton.annotations.AutoplayBu
 import app.revanced.patches.youtube.layout.autoplaybutton.fingerprints.AutonavInformerFingerprint
 import app.revanced.patches.youtube.layout.autoplaybutton.fingerprints.LayoutConstructorFingerprint
 import app.revanced.patches.youtube.misc.mapping.patch.ResourceIdMappingProviderResourcePatch
+import org.jf.dexlib2.iface.instruction.OneRegisterInstruction
 import org.jf.dexlib2.iface.instruction.WideLiteralInstruction
 
 @Patch
@@ -36,22 +37,38 @@ class HideAutoplayButton : BytecodePatch(
         val autonavPreviewStub =
             ResourceIdMappingProviderResourcePatch.resourceMappings.single { it.type == "id" && it.name == "autonav_preview_stub" }
 
-        val autonavToggleConstIndex =
-            layoutGenMethod.implementation!!.instructions.indexOfFirst { (it as? WideLiteralInstruction)?.wideLiteral == autonavToggle.id }
-        val autonavPreviewStubConstIndex =
-            layoutGenMethod.implementation!!.instructions.indexOfFirst { (it as? WideLiteralInstruction)?.wideLiteral == autonavPreviewStub.id }
+        val instructions = layoutGenMethod.implementation!!.instructions
 
-        //remove adding autoplay button to the layout
-        layoutGenMethod.removeInstructions(autonavToggleConstIndex, 5)
-        layoutGenMethod.removeInstructions(autonavPreviewStubConstIndex, 5)
+        val autonavToggleConstIndex =
+            instructions.indexOfFirst { (it as? WideLiteralInstruction)?.wideLiteral == autonavToggle.id }
+        val autonavPreviewStubConstIndex =
+            instructions.indexOfFirst { (it as? WideLiteralInstruction)?.wideLiteral == autonavPreviewStub.id }
+
+        val autonavToggleViewStubInsn = instructions.get(autonavToggleConstIndex + 2)
+        val autonavPreviewStubInsn = instructions.get(autonavPreviewStubConstIndex + 2)
+
+        //Register of the actual viewstub, should be v8
+        val autonavToggleViewStubRegister = (autonavToggleViewStubInsn as? OneRegisterInstruction)?.registerA
+        val autonavPreviewStubRegister = (autonavPreviewStubInsn as? OneRegisterInstruction)?.registerA
+
+        layoutGenMethod.addInstruction(
+            autonavToggleConstIndex + 4, """
+           invoke-static {v$autonavToggleViewStubRegister}, Lapp/revanced/integrations/patches/HideAutoplayButtonPatch;->hideAutoplayButton(Landroid/view/ViewStub;)V  
+        """
+        )
+        layoutGenMethod.addInstruction(
+            autonavPreviewStubConstIndex + 4, """
+            invoke-static {v$autonavPreviewStubRegister}, Lapp/revanced/integrations/patches/HideAutoplayButtonPatch;->hideAutoplayButton(Landroid/view/ViewStub;)V
+        """
+        )
 
         val autonavInformerMethod = AutonavInformerFingerprint.result!!.mutableMethod
 
         //force disable autoplay since it's hard to do without the button
         autonavInformerMethod.replaceInstructions(
-        0,
-        """
-            const/4 v0, 0x0
+            0, """
+            invoke-static {}, Lapp/revanced/integrations/patches/HideAutoplayButtonPatch;->autoPlayEnabled()Z
+            move-result v0            
             return v0
         """
         )
