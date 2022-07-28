404b43dcc98807fc3d80659d32e563360868aa27
oSumAtrIX <johan.melkonyan1@web.de>
2022-07-29 01:09:38 +0200

    refactor: use `ResourceUtils.copyResources` in `swipe-controls-resource-patch`

--- src/main/kotlin/app/revanced/patches/youtube/interaction/swipecontrols/patch/resource/SwipeControlsResourcePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/interaction/swipecontrols/patch/resource/SwipeControlsResourcePatch.kt
@@ -1,6 +1,5 @@
 package app.revanced.patches.youtube.interaction.swipecontrols.patch.resource
 
-import app.revanced.extensions.injectResources
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.data.impl.ResourceData
@@ -8,25 +7,26 @@ import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.impl.ResourcePatch
 import app.revanced.patches.youtube.interaction.swipecontrols.annotation.SwipeControlsCompatibility
+import app.revanced.util.resources.ResourceUtils
+import app.revanced.util.resources.ResourceUtils.copyResources
 
 @Name("swipe-controls-resource-patch")
 @SwipeControlsCompatibility
 @Version("0.0.1")
 class SwipeControlsResourcePatch : ResourcePatch() {
     override fun execute(data: ResourceData): PatchResult {
-        val resourcesDir = "swipecontrols"
 
-        data.injectResources(
-            this.javaClass.classLoader,
-            resourcesDir,
-            "drawable",
-            listOf(
-                "ic_sc_brightness_auto",
-                "ic_sc_brightness_manual",
-                "ic_sc_volume_mute",
-                "ic_sc_volume_normal"
-            ).map { "$it.xml" }
+        data.copyResources(
+            "swipecontrols",
+            ResourceUtils.ResourceGroup(
+                "drawable",
+                "ic_sc_brightness_auto.xml",
+                "ic_sc_brightness_manual.xml",
+                "ic_sc_volume_mute.xml",
+                "ic_sc_volume_normal.xml"
+            )
         )
+
         return PatchResultSuccess()
     }
 }
