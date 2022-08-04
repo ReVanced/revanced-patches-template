0c58c3321d27aa0d770d9740354af45ea561918f
Itroublve <51215044+Itroublve@users.noreply.github.com>
2022-08-04 21:37:01 +0200

    fix: incorrect compatibilty attribute

--- src/main/kotlin/app/revanced/patches/warnwetter/misc/promocode/fingerprints/PromoCodeUnlockFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/warnwetter/misc/promocode/fingerprints/PromoCodeUnlockFingerprint.kt
@@ -4,15 +4,13 @@ import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import app.revanced.patcher.fingerprint.method.annotation.MatchingMethod
-import app.revanced.patches.youtube.layout.createbutton.annotations.CreateButtonCompatibility
-import org.jf.dexlib2.AccessFlags
-import org.jf.dexlib2.Opcode
+import app.revanced.patches.warnwetter.misc.promocode.annotations.PromoCodeUnlockCompatibility
 
 @Name("promo-code-unlock-fingerprint")
 @MatchingMethod(
     "Lde/dwd/warnapp/model/PromoTokenVerification;", "isValid"
 )
-@CreateButtonCompatibility
+@PromoCodeUnlockCompatibility
 @Version("0.0.1")
 object PromoCodeUnlockFingerprint : MethodFingerprint(
     null,
