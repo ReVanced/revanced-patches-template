7cd148b5be4826b18a34c184a50a7983956f925d
oSumAtrIX <johan.melkonyan1@web.de>
2022-08-07 03:32:29 +0200

    fix: wrong type for `InputType.String`

--- src/main/kotlin/app/revanced/patches/youtube/misc/settings/framework/components/impl/InputType.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/settings/framework/components/impl/InputType.kt
@@ -1,6 +1,6 @@
 package app.revanced.patches.youtube.misc.settings.framework.components.impl
 
 enum class InputType(val type: String) {
-    STRING("string"),
+    STRING("text"),
     NUMBER("number"),
 }
