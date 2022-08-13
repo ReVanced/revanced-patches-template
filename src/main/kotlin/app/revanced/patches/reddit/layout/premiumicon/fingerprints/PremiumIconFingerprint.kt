bbf81045796c3641fe2db2a9a92ed0d807eb9bb0
Technikte <technikte@mail.de>
2022-08-13 17:03:05 +0200

    copy-paste errors

--- src/main/kotlin/app/revanced/patches/reddit/layout/premiumicon/fingerprints/PremiumIconFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/reddit/layout/premiumicon/fingerprints/PremiumIconFingerprint.kt
@@ -4,13 +4,13 @@ import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import app.revanced.patcher.fingerprint.method.annotation.MatchingMethod
-import app.revanced.patches.warnwetter.misc.promocode.annotations.PromoCodeUnlockCompatibility
+import app.revanced.patches.reddit.layout.premiumicon.annotations.PremiumIconCompatibility
 
 @Name("premium-icon-fingerprint")
 @MatchingMethod(
     "Lcom/reddit/domain/model/MyAccount;", "isPremiumSubscriber"
 )
-@PromoCodeUnlockCompatibility
+@PremiumIconCompatibility
 @Version("0.0.1")
 object PremiumIconFingerprint : MethodFingerprint(
     "Z",
