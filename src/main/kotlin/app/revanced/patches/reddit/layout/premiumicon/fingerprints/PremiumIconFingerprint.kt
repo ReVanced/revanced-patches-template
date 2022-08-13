b717f3f56aa404a1b54d6df280e671a7a8902f51
Technikte <technikte@mail.de>
2022-08-13 16:55:40 +0200

    add PremiumIcon patch, first successful build

---
+++ src/main/kotlin/app/revanced/patches/reddit/layout/premiumicon/fingerprints/PremiumIconFingerprint.kt
@@ -0,0 +1,24 @@
+package app.revanced.patches.reddit.layout.premiumicon.fingerprints
+
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import app.revanced.patcher.fingerprint.method.annotation.MatchingMethod
+import app.revanced.patches.warnwetter.misc.promocode.annotations.PromoCodeUnlockCompatibility
+
+@Name("premium-icon-fingerprint")
+@MatchingMethod(
+    "Lcom/reddit/domain/model/MyAccount;", "isPremiumSubscriber"
+)
+@PromoCodeUnlockCompatibility
+@Version("0.0.1")
+object PremiumIconFingerprint : MethodFingerprint(
+    "Z",
+    null,
+    null,
+    null,
+    null,
+    { methodDef ->
+        methodDef.definingClass.endsWith("MyAccount;") && methodDef.name == "isPremiumSubscriber"
+    }
+)
