e40bff50c2a83380a2aab9e14cfcea5249a8d3d3
epicsampler <102923070+epicsampler@users.noreply.github.com>
2022-08-27 00:05:21 +0200

    feat: `client-spoof` patch
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>

---
+++ src/main/kotlin/app/revanced/patches/youtube/misc/clientspoof/annotations/ClientSpoofCompatibility.kt
@@ -0,0 +1,14 @@
+package app.revanced.patches.youtube.misc.clientspoof.annotations
+
+import app.revanced.patcher.annotation.Compatibility
+import app.revanced.patcher.annotation.Package
+
+@Compatibility(
+    [
+        Package("com.google.android.youtube", arrayOf()),
+        Package("com.vanced.android", arrayOf())
+    ]
+)
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+internal annotation class ClientSpoofCompatibility
