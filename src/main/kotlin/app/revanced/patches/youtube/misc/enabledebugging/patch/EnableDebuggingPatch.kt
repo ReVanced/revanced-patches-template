348041e4c1805162c4197fcf325c7d3100335c29
Canny <94744045+Canny1913@users.noreply.github.com>
2022-08-27 03:25:26 +0300

    feat: move debug switch to `enable-debugging`

--- src/main/kotlin/app/revanced/patches/youtube/misc/enabledebugging/patch/EnableDebuggingPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/enabledebugging/patch/EnableDebuggingPatch.kt
@@ -9,15 +9,30 @@ import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patcher.patch.impl.ResourcePatch
 import app.revanced.patches.youtube.misc.enabledebugging.annotations.EnableDebuggingCompatibility
+import app.revanced.patches.youtube.misc.integrations.patch.IntegrationsPatch
+import app.revanced.patches.youtube.misc.settings.bytecode.patch.SettingsPatch
+import app.revanced.patches.youtube.misc.settings.framework.components.impl.StringResource
+import app.revanced.patches.youtube.misc.settings.framework.components.impl.SwitchPreference
 import org.w3c.dom.Element
 
 @Patch(false)
 @Name("enable-debugging")
+@DependsOn([IntegrationsPatch::class, SettingsPatch::class])
 @Description("Enables app debugging by patching the manifest file.")
 @EnableDebuggingCompatibility
 @Version("0.0.1")
 class EnableDebuggingPatch : ResourcePatch() {
     override fun execute(data: ResourceData): PatchResult {
+        SettingsPatch.PreferenceScreen.MISC.addPreferences(
+            SwitchPreference(
+                "revanced_debug_enabled",
+                StringResource("revanced_debug_title", "Debug"),
+                false,
+                StringResource("revanced_debug_on", "ReVanced logs are enabled"),
+                StringResource("revanced_debug_off", "ReVanced logs are disabled")
+            )
+        )
+
         // create an xml editor instance
         data.xmlEditor["AndroidManifest.xml"].use { dom ->
             // get the application node
