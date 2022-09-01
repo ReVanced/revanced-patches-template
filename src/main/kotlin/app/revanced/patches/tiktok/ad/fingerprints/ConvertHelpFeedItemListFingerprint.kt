089646c47cb7841c2347d61f38ad48fb0ec70a5d
d4rkk3y <vuhoanhuy99@gmail.com>
2022-09-01 20:05:08 +0700

    fix tiktok-ads resolve the suggestion.

---
+++ src/main/kotlin/app/revanced/patches/tiktok/ad/fingerprints/ConvertHelpFeedItemListFingerprint.kt
@@ -0,0 +1,21 @@
+package app.revanced.patches.tiktok.ad.fingerprints
+
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.fingerprint.method.annotation.MatchingMethod
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import app.revanced.patches.tiktok.ad.annotations.TiktokAdsCompatibility
+
+@Name("feed-item-clone-fingerprint")
+@MatchingMethod(
+    "Lbeancopy/ConvertHelp;",
+    "com${'$'}ss${'$'}ugc${'$'}aweme${'$'}proto${'$'}aweme_v2_feed_response${'$'}${'$'}com${'$'}ss${'$'}android${'$'}ugc${'$'}aweme${'$'}feed${'$'}model${'$'}FeedItemList",
+)
+@TiktokAdsCompatibility
+@Version("0.0.1")
+object ConvertHelpFeedItemListFingerprint : MethodFingerprint(
+    customFingerprint = { methodDef ->
+        methodDef.definingClass.endsWith("/ConvertHelp;") &&
+                methodDef.name == "com${'$'}ss${'$'}ugc${'$'}aweme${'$'}proto${'$'}aweme_v2_feed_response${'$'}${'$'}com${'$'}ss${'$'}android${'$'}ugc${'$'}aweme${'$'}feed${'$'}model${'$'}FeedItemList"
+    }
+)
