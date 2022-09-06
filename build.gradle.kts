d3f86689a6ad2f797d00ccb06da4ee6a6780d732
Sculas <contact@sculas.xyz>
2022-09-06 23:45:54 +0200

    build: update patcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    implementation("app.revanced:revanced-patcher:3.4.0")
+    implementation("app.revanced:revanced-patcher:3.5.1")
     implementation("app.revanced:multidexlib2:2.5.2.r2")
 
     // Required for meta
