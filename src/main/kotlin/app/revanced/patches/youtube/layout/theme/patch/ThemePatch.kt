161256afd391d4d42ff64afb2cf9289b785ec29f
Sculas <contact@sculas.xyz>
2022-09-08 18:35:08 +0200

    fix: cleanup & trigger release for ThemePatch
    Closes #447
    Closes revanced/revanced-patcher#99

--- src/main/kotlin/app/revanced/patches/youtube/layout/theme/patch/ThemePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/theme/patch/ThemePatch.kt
@@ -50,7 +50,9 @@ class ThemePatch : ResourcePatch() {
     enum class Themes(val apply: (String) -> String?) {
         Amoled({ attr ->
             when (attr) {
-                "yt_black1", "yt_black1_opacity95", "yt_black2", "yt_black3", "yt_black4", "yt_status_bar_background_dark" -> "@android:color/black"
+                "yt_black1", "yt_black1_opacity95", "yt_black2", "yt_black3", "yt_black4",
+                "yt_status_bar_background_dark" -> "@android:color/black"
+
                 "yt_selected_nav_label_dark" -> "#ffdf0000"
                 else -> null
             }
