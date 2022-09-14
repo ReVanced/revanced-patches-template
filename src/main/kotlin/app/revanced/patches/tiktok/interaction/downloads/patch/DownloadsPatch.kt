0267c2ae942437b9951ce11e33fee482654801e9
d4rkk3y <vuhoanhuy99@gmail.com>
2022-09-15 04:30:50 +0700

    feat: Custom download path.

--- src/main/kotlin/app/revanced/patches/tiktok/interaction/downloads/patch/DownloadsPatch.kt
+++ src/main/kotlin/app/revanced/patches/tiktok/interaction/downloads/patch/DownloadsPatch.kt
@@ -4,24 +4,34 @@ import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.data.impl.BytecodeData
+import app.revanced.patcher.data.impl.toMethodWalker
+import app.revanced.patcher.extensions.replaceInstruction
 import app.revanced.patcher.extensions.replaceInstructions
-import app.revanced.patcher.patch.PatchResult
-import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.*
 import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patcher.patch.impl.BytecodePatch
+import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
 import app.revanced.patches.tiktok.interaction.downloads.annotations.DownloadsCompatibility
-import app.revanced.patches.tiktok.interaction.downloads.fingerprints.*
+import app.revanced.patches.tiktok.interaction.downloads.fingerprints.ACLCommonShareFingerprint
+import app.revanced.patches.tiktok.interaction.downloads.fingerprints.ACLCommonShareFingerprint2
+import app.revanced.patches.tiktok.interaction.downloads.fingerprints.ACLCommonShareFingerprint3
+import app.revanced.patches.tiktok.interaction.downloads.fingerprints.DownloadPathParentFingerprint
+import org.jf.dexlib2.Opcode
+import org.jf.dexlib2.iface.instruction.OneRegisterInstruction
+import org.jf.dexlib2.iface.instruction.ReferenceInstruction
+import org.jf.dexlib2.iface.reference.StringReference
 
 @Patch
 @Name("tiktok-download")
-@Description("Remove restrictions on downloads video.")
+@Description("Remove restrictions on downloads video and change download path.")
 @DownloadsCompatibility
 @Version("0.0.1")
 class DownloadsPatch : BytecodePatch(
     listOf(
         ACLCommonShareFingerprint,
         ACLCommonShareFingerprint2,
-        ACLCommonShareFingerprint3
+        ACLCommonShareFingerprint3,
+        DownloadPathParentFingerprint
     )
 ) {
     override fun execute(data: BytecodeData): PatchResult {
@@ -50,7 +60,62 @@ class DownloadsPatch : BytecodePatch(
                 return v0
             """
         )
+        //Change the download path patch
+        val method4 = DownloadPathParentFingerprint.result!!.mutableMethod
+        val implementation4 = method4.implementation
+        val instructions = implementation4!!.instructions
+        var targetOffset = -1
+        //Search for the target method called instruction offset.
+        instructions.forEachIndexed { index, instruction ->
+            if (instruction.opcode != Opcode.CONST_STRING) return@forEachIndexed
+            val reference = (instruction as ReferenceInstruction).reference as StringReference
+            if (reference.string != "video/mp4") return@forEachIndexed
+            val targetInstruction = instructions[index + 1] as ReferenceInstruction
+            if (targetInstruction.opcode != Opcode.INVOKE_STATIC) return@forEachIndexed
+            targetOffset = index + 1
+        }
+        if (targetOffset == -1) return PatchResultError("Can not find download path uri method.")
+        //Change videos' download path.
+        val downloadUriMethod = data
+            .toMethodWalker(DownloadPathParentFingerprint.result!!.method)
+            .nextMethod(targetOffset, true)
+            .getMethod() as MutableMethod
+        downloadUriMethod.implementation!!.instructions.forEachIndexed { index, instruction ->
+            if (instruction.opcode == Opcode.SGET_OBJECT) {
+                val overrideRegister = (instruction as OneRegisterInstruction).registerA
+                downloadUriMethod.replaceInstruction(
+                    index,
+                    """
+                        const-string v$overrideRegister, "${downloadPath!!}"
+                    """
+                )
+            }
+            if (instruction.opcode == Opcode.CONST_STRING) {
+                val string = ((instruction as ReferenceInstruction).reference as StringReference).string
+                if (string.contains("/Camera")) {
+                    val overrideRegister = (instruction as OneRegisterInstruction).registerA
+                    val overrideString = string.replace("/Camera", "")
+                    downloadUriMethod.replaceInstruction(
+                        index,
+                        """
+                            const-string v$overrideRegister, "$overrideString"
+                        """
+                    )
+                }
+            }
+        }
         return PatchResultSuccess()
     }
 
+    companion object : OptionsContainer() {
+        private var downloadPath: String? by option(
+            PatchOption.StringOption(
+                key = "downloadPath",
+                default = "DCIM/Tiktok",
+                title = "Download Path",
+                description = "The custom download path. Allow directories are DCIM, Movies, Pictures.",
+                required = true
+            )
+        )
+    }
 }
