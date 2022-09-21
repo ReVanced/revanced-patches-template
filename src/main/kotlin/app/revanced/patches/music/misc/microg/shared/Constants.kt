2b74cab785df3df504c6a61deb82e1961a2ef0c4
GeekCorner <45696571+GeekCornerGH@users.noreply.github.com>
2022-09-21 08:24:41 +0200

    feat: Rename YT Music

--- src/main/kotlin/app/revanced/patches/music/misc/microg/shared/Constants.kt
+++ src/main/kotlin/app/revanced/patches/music/misc/microg/shared/Constants.kt
@@ -2,6 +2,6 @@ package app.revanced.patches.music.misc.microg.shared
 
 object Constants {
     internal const val BASE_MICROG_PACKAGE_NAME = "com.mgoogle"
-    internal const val REVANCED_MUSIC_APP_NAME = "YouTube Music ReVanced"
+    internal const val REVANCED_MUSIC_APP_NAME = "YT Music ReVanced"
     internal const val REVANCED_MUSIC_PACKAGE_NAME = "app.revanced.android.apps.youtube.music"
 }
