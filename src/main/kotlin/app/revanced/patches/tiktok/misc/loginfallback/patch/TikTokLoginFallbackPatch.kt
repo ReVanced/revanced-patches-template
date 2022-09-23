e64f4d7c43da1abb97161f75ecf35f98f8f271d5
d4rkk3y <vuhoanhuy99@gmail.com>
2022-09-23 17:17:08 +0700

    feat(tiktok): login with google by web auth.

---
+++ src/main/kotlin/app/revanced/patches/tiktok/misc/loginfallback/patch/TikTokLoginFallbackPatch.kt
@@ -0,0 +1,41 @@
+package app.revanced.patches.tiktok.misc.loginfallback.patch
+
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.impl.BytecodeData
+import app.revanced.patcher.extensions.addInstructions
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.patch.impl.BytecodePatch
+import app.revanced.patches.tiktok.misc.loginfallback.annotations.TikTokWebLoginCompatibility
+
+@Patch
+@Name("tiktok-web-login")
+@Description("Login with google use web auth.")
+@TikTokWebLoginCompatibility
+@Version("0.0.1")
+class TikTokLoginFallbackPatch : BytecodePatch() {
+    override fun execute(data: BytecodeData): PatchResult {
+        val googleOneTapAuth = data.findClass("Lcom/bytedance/lobby/google/GoogleOneTapAuth;")!!.resolve()
+        val googleOneTapAuthCheck = googleOneTapAuth.methods.first { it.returnType == "Z" && it.parameters.size == 0 }
+        googleOneTapAuthCheck.addInstructions(
+            0,
+            """
+                const/4 v0, 0x0
+                return v0
+            """
+        )
+        val googleAuth = data.findClass("Lcom/bytedance/lobby/google/GoogleAuth;")!!.resolve()
+        val googleAuthCheck = googleAuth.methods.first { it.returnType == "Z" && it.parameters.size == 0 }
+        googleAuthCheck.addInstructions(
+            0,
+            """
+                const/4 v0, 0x0
+                return v0
+            """
+        )
+        return PatchResultSuccess()
+    }
+}
