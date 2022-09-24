0db4f4079b59437e6109b6bec6c4baeb820c65ba
shadow578 <52449218+shadow578@users.noreply.github.com>
2022-09-24 11:08:59 +0200

    refactor(microg-support): share code between `music-microg-support` patch

--- src/main/kotlin/app/revanced/patches/youtube/misc/microg/shared/Constants.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/microg/shared/Constants.kt
@@ -1,6 +1,9 @@
 package app.revanced.patches.youtube.misc.microg.shared
 
 object Constants {
-    internal const val BASE_MICROG_PACKAGE_NAME = "com.mgoogle"
+    internal const val REVANCED_APP_NAME = "YouTube ReVanced"
     internal const val REVANCED_PACKAGE_NAME = "app.revanced.android.youtube"
+    internal const val PACKAGE_NAME = "com.google.android.youtube"
+    internal const val SPOOFED_PACKAGE_NAME = PACKAGE_NAME
+    internal const val SPOOFED_PACKAGE_SIGNATURE = "24bb24c05e47e0aefa68a58a766179d9b613a600"
 }
