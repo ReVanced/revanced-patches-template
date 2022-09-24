c9a4b59ae9e23a33d640ee3420a95e1e1621d248
Oizaro <108184954+OxrxL@users.noreply.github.com>
2022-09-24 05:53:10 +0200

    fix: improved hdr brightness fingerprint compatibility
    😵

--- src/main/kotlin/app/revanced/patches/youtube/misc/hdrbrightness/fingerprints/HDRBrightnessFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/hdrbrightness/fingerprints/HDRBrightnessFingerprint.kt
@@ -5,6 +5,10 @@ import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.fingerprint.method.annotation.MatchingMethod
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import app.revanced.patches.youtube.misc.hdrbrightness.annotations.HDRBrightnessCompatibility
+import org.jf.dexlib2.Opcode
+import org.jf.dexlib2.iface.instruction.ReferenceInstruction
+import org.jf.dexlib2.iface.reference.FieldReference
+import org.jf.dexlib2.iface.reference.StringReference
 
 @Name("hdr-brightness-fingerprint")
 @MatchingMethod(
@@ -14,5 +18,18 @@ import app.revanced.patches.youtube.misc.hdrbrightness.annotations.HDRBrightness
 @Version("0.0.1")
 object HDRBrightnessFingerprint : MethodFingerprint(
     "V",
-    strings = listOf("c.SettingNotFound;", "screen_brightness", "android.mediaview", "lux"),
+    customFingerprint = { methodDef ->
+        methodDef.implementation?.instructions?.any {
+            ((it.opcode.ordinal == Opcode.IPUT.ordinal
+                    ||
+            it.opcode.ordinal == Opcode.IGET.ordinal) &&
+            ((it as? ReferenceInstruction)?.reference as FieldReference).name.contains("screenBrightness"))
+        } == true &&
+        methodDef.implementation?.instructions?.any {
+            (it.opcode.ordinal == Opcode.CONST_STRING.ordinal &&
+            (((it as? ReferenceInstruction)?.reference as StringReference).string == "screen_brightness"
+            ||
+            ((it as? ReferenceInstruction)?.reference as StringReference).string == "mediaViewambientBrightnessSensor"))
+        } == true
+    }
 )
