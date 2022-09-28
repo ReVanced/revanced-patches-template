84781c9b5f61c6ac883ad188c44426f0f0280ad5
Oizaro <108184954+OxrxL@users.noreply.github.com>
2022-09-28 15:28:00 +0200

    fix: improved hdr brightness fingerprint compatibility

--- src/main/kotlin/app/revanced/patches/youtube/misc/hdrbrightness/fingerprints/HDRBrightnessFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/hdrbrightness/fingerprints/HDRBrightnessFingerprint.kt
@@ -5,10 +5,7 @@ import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.fingerprint.method.annotation.MatchingMethod
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import app.revanced.patches.youtube.misc.hdrbrightness.annotations.HDRBrightnessCompatibility
-import org.jf.dexlib2.Opcode
-import org.jf.dexlib2.iface.instruction.ReferenceInstruction
-import org.jf.dexlib2.iface.reference.FieldReference
-import org.jf.dexlib2.iface.reference.StringReference
+import org.jf.dexlib2.AccessFlags
 
 @Name("hdr-brightness-fingerprint")
 @MatchingMethod(
@@ -17,19 +14,6 @@ import org.jf.dexlib2.iface.reference.StringReference
 @HDRBrightnessCompatibility
 @Version("0.0.1")
 object HDRBrightnessFingerprint : MethodFingerprint(
-    "V",
-    customFingerprint = { methodDef ->
-        methodDef.implementation?.instructions?.any {
-            ((it.opcode.ordinal == Opcode.IPUT.ordinal
-                    ||
-            it.opcode.ordinal == Opcode.IGET.ordinal) &&
-            ((it as? ReferenceInstruction)?.reference as FieldReference).name.contains("screenBrightness"))
-        } == true &&
-        methodDef.implementation?.instructions?.any {
-            (it.opcode.ordinal == Opcode.CONST_STRING.ordinal &&
-            (((it as? ReferenceInstruction)?.reference as StringReference).string == "screen_brightness"
-            ||
-            ((it as? ReferenceInstruction)?.reference as StringReference).string == "mediaViewambientBrightnessSensor"))
-        } == true
-    }
+    "V", AccessFlags.FINAL.value,
+    strings = listOf("c.SettingNotFound;", "screen_brightness", "android.mediaview"),
 )
