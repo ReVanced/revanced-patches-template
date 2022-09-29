d1b22296e98711855a85a937ed84f429d26b419a
sandip_yadav <sandip@gmail.com>
2022-09-29 20:27:09 +0530

    feat: bump YouTube Music patches to v5.25.51

--- src/main/kotlin/app/revanced/patches/music/layout/tastebuilder/annotations/RemoveTasteBuilderCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/music/layout/tastebuilder/annotations/RemoveTasteBuilderCompatibility.kt
@@ -10,7 +10,7 @@ import app.revanced.patcher.annotation.Package
  */
 @Compatibility(
     [Package(
-        "com.google.android.apps.youtube.music", arrayOf("5.21.52", "5.22.54", "5.23.50")
+        "com.google.android.apps.youtube.music", arrayOf("5.21.52", "5.22.54", "5.23.50","5.25.51")
     )]
 )
 @Target(AnnotationTarget.CLASS)
