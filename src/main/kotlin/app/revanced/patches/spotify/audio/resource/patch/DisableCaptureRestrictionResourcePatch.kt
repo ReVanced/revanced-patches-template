11094248f0bb54089ab6569d09a95c82bfa88fc1
Tim Schneeberger <thebone.main@gmail.com>
2022-09-30 00:00:16 +0200

    feat: `disable-capture-restriction` patch (#655)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

---
+++ src/main/kotlin/app/revanced/patches/spotify/audio/resource/patch/DisableCaptureRestrictionResourcePatch.kt
@@ -0,0 +1,35 @@
+package app.revanced.patches.spotify.audio.resource.patch
+
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.impl.ResourceData
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.DependsOn
+import app.revanced.patcher.patch.impl.ResourcePatch
+import app.revanced.patches.spotify.audio.annotation.DisableCaptureRestrictionCompatibility
+import app.revanced.patches.youtube.misc.manifest.patch.FixLocaleConfigErrorPatch
+import org.w3c.dom.Element
+
+@Name("disable-capture-restriction-resource-patch")
+@Description("Sets allowAudioPlaybackCapture in manifest to true.")
+@DisableCaptureRestrictionCompatibility
+@Version("0.0.1")
+class DisableCaptureRestrictionResourcePatch : ResourcePatch() {
+    override fun execute(data: ResourceData): PatchResult {
+        // create an xml editor instance
+        data.xmlEditor["AndroidManifest.xml"].use { dom ->
+            // get the application node
+            val applicationNode = dom
+                .file
+                .getElementsByTagName("application")
+                .item(0) as Element
+
+            // set allowAudioPlaybackCapture attribute to true
+            applicationNode.setAttribute("android:allowAudioPlaybackCapture", "true")
+        }
+
+        return PatchResultSuccess()
+    }
+}
