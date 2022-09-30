5197a24cc5ecc1b51cdc5c00c77c873b86394994
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-09-30 19:58:09 +0200

    fix(disable-startup-shorts-player): remove redundant opcode pattern (#679)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patches/youtube/layout/startupshortsreset/fingerprints/ActionOpenShortsFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/startupshortsreset/fingerprints/ActionOpenShortsFingerprint.kt
@@ -14,27 +14,6 @@ import org.jf.dexlib2.Opcode
 @StartupShortsResetCompatibility
 @Version("0.0.1")
 object ActionOpenShortsFingerprint : MethodFingerprint(
-    "V", AccessFlags.PUBLIC or AccessFlags.FINAL, listOf("L", "L"), listOf(
-        Opcode.IF_EQZ,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.CONST_STRING,
-        Opcode.CONST_4,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.MOVE_RESULT,
-        Opcode.IPUT_BOOLEAN,
-        Opcode.IGET_BOOLEAN,
-        Opcode.IF_NEZ,
-        Opcode.IF_EQZ,
-        Opcode.INVOKE_STATIC,
-        Opcode.MOVE_RESULT,
-        Opcode.IF_EQZ,
-        Opcode.IGET_OBJECT,
-        Opcode.IF_EQZ,
-        Opcode.IGET_OBJECT,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.IF_EQZ,
-        Opcode.CONST_CLASS,
-    ),
-    listOf("com.google.android.youtube.action.open.shorts"),
+    "V", AccessFlags.PUBLIC or AccessFlags.FINAL, listOf("L", "L"),
+    strings = listOf("com.google.android.youtube.action.open.shorts"),
 )
