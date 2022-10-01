e864c6eef94ca3a1a38728f713dd7efd72058142
oSumAtrIX <johan.melkonyan1@web.de>
2022-10-01 03:58:19 +0200

    perf(general-ads): hook pathBuilder

--- src/main/kotlin/app/revanced/patches/youtube/ad/general/bytecode/extensions/MethodExtensions.kt
+++ src/main/kotlin/app/revanced/patches/youtube/ad/general/bytecode/extensions/MethodExtensions.kt
@@ -1,12 +1,8 @@
 package app.revanced.patches.youtube.ad.general.bytecode.extensions
 
-import app.revanced.patcher.extensions.addInstructions
 import app.revanced.patcher.extensions.softCompareTo
 import app.revanced.patcher.patch.PatchResultError
 import app.revanced.patcher.util.proxy.mutableTypes.MutableClass
-import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
-import org.jf.dexlib2.builder.BuilderInstruction
-import org.jf.dexlib2.builder.MutableMethodImplementation
 import org.jf.dexlib2.iface.Method
 import org.jf.dexlib2.iface.instruction.Instruction
 import org.jf.dexlib2.iface.instruction.ReferenceInstruction
@@ -16,21 +12,6 @@ import org.jf.dexlib2.iface.reference.Reference
 import org.jf.dexlib2.immutable.reference.ImmutableMethodReference
 
 internal object MethodExtensions {
-    internal fun MutableMethodImplementation.insertBlocks(
-        startIndex: Int,
-        vararg blocks: List<BuilderInstruction>,
-    ) {
-        blocks.reversed().forEach {
-            this.addInstructions(
-                startIndex, it
-            )
-        }
-    }
-
-    internal fun MutableClass.addMethod(mutableMethod: MutableMethod) {
-        this.methods.add(mutableMethod)
-    }
-
     internal fun MutableClass.findMutableMethodOf(
         method: Method
     ) = this.methods.first {
