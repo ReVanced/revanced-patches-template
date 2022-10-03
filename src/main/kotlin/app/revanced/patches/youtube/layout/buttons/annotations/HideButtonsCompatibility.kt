81dd5961a46f4c60c052e990344a6a5586986de3
oSumAtrIX <johan.melkonyan1@web.de>
2022-10-03 13:37:30 +0200

    feat: `hide-video-buttons` patch

---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/buttons/annotations/HideButtonsCompatibility.kt
@@ -0,0 +1,13 @@
+package app.revanced.patches.youtube.layout.buttons.annotations
+
+import app.revanced.patcher.annotation.Compatibility
+import app.revanced.patcher.annotation.Package
+
+@Compatibility(
+    [Package(
+        "com.google.android.youtube", arrayOf("17.25.34", "17.26.35", "17.27.39", "17.28.34", "17.29.34", "17.32.35", "17.33.42", "17.34.35", "17.34.36", "17.36.37")
+    )]
+)
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+internal annotation class HideButtonsCompatibility
