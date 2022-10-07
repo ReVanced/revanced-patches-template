5147404c9ee8c439b5417e5a48c6fb445c8c67c2
oSumAtrIX <johan.melkonyan1@web.de>
2022-10-07 23:01:41 +0200

    fix(video-ads): block remaining video ads

--- src/main/kotlin/app/revanced/patches/youtube/ad/video/fingerprints/LoadAdsFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/ad/video/fingerprints/LoadAdsFingerprint.kt
@@ -2,16 +2,17 @@ package app.revanced.patches.youtube.ad.video.fingerprints
 
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
-import app.revanced.patcher.extensions.or
 
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import app.revanced.patches.youtube.ad.video.annotations.VideoAdsCompatibility
-import org.jf.dexlib2.AccessFlags
 
-@Name("show-video-ads-method-fingerprint")
+@Name("load-ads-fingerprint")
 
 @VideoAdsCompatibility
 @Version("0.0.1")
-object ShowVideoAdsFingerprint : MethodFingerprint(
-    "V", AccessFlags.PUBLIC or AccessFlags.FINAL, listOf("Z")
+object LoadAdsFingerprint : MethodFingerprint(
+    strings = listOf("Unexpected playerAd type: "),
+    customFingerprint = { method ->
+        method.parameterTypes.size > 0 && method.parameterTypes.first() == "Ljava/lang/String;"
+    }
 )
