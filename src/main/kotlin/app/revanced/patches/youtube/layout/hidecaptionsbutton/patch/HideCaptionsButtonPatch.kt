0f1f56a514a97f5274559bb70ff72bde25675a0e
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-10-14 16:38:04 +0200

    fix: changed code logic (directly hide root element)

--- src/main/kotlin/app/revanced/patches/youtube/layout/hidecaptionsbutton/patch/HideCaptionsButtonPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hidecaptionsbutton/patch/HideCaptionsButtonPatch.kt
@@ -5,32 +5,27 @@ import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.addInstructions
-import app.revanced.patcher.extensions.instruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patcher.patch.annotations.Patch
-import app.revanced.patcher.util.smali.ExternalLabel
 import app.revanced.patches.youtube.layout.autocaptions.annotations.AutoCaptionsCompatibility
 import app.revanced.patches.youtube.layout.autocaptions.fingerprints.SubtitleButtonControllerFingerprint
-import app.revanced.patches.youtube.layout.hidecaptionsbutton.fingerprints.CaptionsButtonOnClickFingerprint
-import app.revanced.patches.youtube.layout.hidecaptionsbutton.fingerprints.CaptionsButtonOnLongClickFingerprint
 import app.revanced.patches.youtube.misc.integrations.patch.IntegrationsPatch
 import app.revanced.patches.youtube.misc.settings.bytecode.patch.SettingsPatch
 import app.revanced.patches.youtube.misc.settings.framework.components.impl.StringResource
 import app.revanced.patches.youtube.misc.settings.framework.components.impl.SwitchPreference
+import org.jf.dexlib2.Opcode
 
 @Patch
 @DependsOn([IntegrationsPatch::class, SettingsPatch::class])
-@Name("hide-captions-button-buttons")
+@Name("hide-captions-button")
 @Description("Hide the captions button on video player.")
 @AutoCaptionsCompatibility
 @Version("0.0.1")
 class HideCaptionsButtonPatch : BytecodePatch(listOf(
     SubtitleButtonControllerFingerprint,
-    CaptionsButtonOnClickFingerprint,
-    CaptionsButtonOnLongClickFingerprint,
 )) {
     override fun execute(context: BytecodeContext): PatchResult {
         SettingsPatch.PreferenceScreen.LAYOUT.addPreferences(
@@ -44,34 +39,16 @@ class HideCaptionsButtonPatch : BytecodePatch(listOf(
         )
 
         val subtitleButtonControllerMethod = SubtitleButtonControllerFingerprint.result!!.mutableMethod
+
+        //Due to previosly applied patches, scanResult index cannot be used in this context
+        val igetBooleanIndex = subtitleButtonControllerMethod.implementation!!.instructions.indexOfFirst {
+            it.opcode == Opcode.IGET_BOOLEAN
+        }
+
         subtitleButtonControllerMethod.addInstructions(
-            0, """
-                invoke-static {}, Lapp/revanced/integrations/patches/HideCaptionsButtonPatch;->hideCaptionsButton()Z
-                move-result v0
-                if-eqz v0, :hide_captions_button
-                return-void
-            """, listOf(ExternalLabel("hide_captions_button", subtitleButtonControllerMethod.instruction(0)))
-        )
-
-        val captionsButtonOnClickMethod = CaptionsButtonOnClickFingerprint.result!!.mutableMethod
-        captionsButtonOnClickMethod.addInstructions(
-            0, """
-                invoke-static {}, Lapp/revanced/integrations/patches/HideCaptionsButtonPatch;->hideCaptionsButton()Z
-                move-result v0
-                if-eqz v0, :hide_captions_button
-                return-void
-            """, listOf(ExternalLabel("hide_captions_button", captionsButtonOnClickMethod.instruction(0)))
-        )
-
-        val captionsButtonOnLongClickMethod = CaptionsButtonOnLongClickFingerprint.result!!.mutableMethod
-        captionsButtonOnLongClickMethod.addInstructions(
-            0, """
-                invoke-static {}, Lapp/revanced/integrations/patches/HideCaptionsButtonPatch;->hideCaptionsButton()Z
-                move-result v0
-                if-eqz v0, :hide_captions_button
-                const/4 v0, 0x0
-                return v0
-            """, listOf(ExternalLabel("hide_captions_button", captionsButtonOnLongClickMethod.instruction(0)))
+            igetBooleanIndex + 1, """
+                invoke-static {v0}, Lapp/revanced/integrations/patches/HideCaptionsButtonPatch;->hideCaptionsButton(Landroid/widget/ImageView;)V
+            """
         )
 
         return PatchResultSuccess()
