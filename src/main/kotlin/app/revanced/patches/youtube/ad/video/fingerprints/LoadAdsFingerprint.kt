82e639a4df27cdd80de67fa06091a1a6e0a6aa24
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-10-19 11:28:37 +0200

    fix: reduced fingerprint

--- src/main/kotlin/app/revanced/patches/youtube/ad/video/fingerprints/LoadAdsFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/ad/video/fingerprints/LoadAdsFingerprint.kt
@@ -2,18 +2,15 @@ package app.revanced.patches.youtube.ad.video.fingerprints
 
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
-import app.revanced.patcher.extensions.or
 
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import app.revanced.patches.youtube.ad.video.annotations.VideoAdsCompatibility
-import org.jf.dexlib2.AccessFlags
 
 @Name("load-ads-fingerprint")
 
 @VideoAdsCompatibility
 @Version("0.0.1")
 object LoadAdsFingerprint : MethodFingerprint(
-    "V", AccessFlags.PUBLIC or AccessFlags.FINAL, listOf("L"),
     strings = listOf(
         "OnFulfillmentTriggersActivated has non registered slot",
         "markFillRequested",
