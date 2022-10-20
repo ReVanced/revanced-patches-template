33ea03f68dca09ddd6cae1c59a2b4f8f721cd79a
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-10-20 17:44:07 +0200

    fix(youtube/video ads): revert last change

--- src/main/kotlin/app/revanced/patches/youtube/ad/video/patch/VideoAdsPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/ad/video/patch/VideoAdsPatch.kt
@@ -14,7 +14,6 @@ import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patcher.util.smali.ExternalLabel
 import app.revanced.patches.youtube.ad.video.annotations.VideoAdsCompatibility
 import app.revanced.patches.youtube.ad.video.fingerprints.LoadVideoAdsFingerprint
-import app.revanced.patches.youtube.ad.video.fingerprints.ShowVideoAdsFingerprint
 import app.revanced.patches.youtube.misc.integrations.patch.IntegrationsPatch
 import app.revanced.patches.youtube.misc.settings.bytecode.patch.SettingsPatch
 import app.revanced.patches.youtube.misc.settings.framework.components.impl.StringResource
@@ -29,7 +28,6 @@ import app.revanced.patches.youtube.misc.settings.framework.components.impl.Swit
 class VideoAdsPatch : BytecodePatch(
     listOf(
         LoadVideoAdsFingerprint,
-        ShowVideoAdsFingerprint,
     )
 ) {
     override fun execute(context: BytecodeContext): PatchResult {
@@ -43,22 +41,15 @@ class VideoAdsPatch : BytecodePatch(
             )
         )
 
-        val lithoAdsFingerprintMethod = LoadVideoAdsFingerprint.result!!.mutableMethod
+        val loadVideoAdsFingerprintMethod = LoadVideoAdsFingerprint.result!!.mutableMethod
 
-        lithoAdsFingerprintMethod.addInstructions(
+        loadVideoAdsFingerprintMethod.addInstructions(
             0, """
                 invoke-static { }, Lapp/revanced/integrations/patches/VideoAdsPatch;->shouldShowAds()Z
                 move-result v0
                 if-nez v0, :show_video_ads
                 return-void
-            """, listOf(ExternalLabel("show_video_ads", lithoAdsFingerprintMethod.instruction(0)))
-        )
-
-        ShowVideoAdsFingerprint.result!!.mutableMethod.addInstructions(
-            0, """
-                invoke-static { }, Lapp/revanced/integrations/patches/VideoAdsPatch;->shouldShowAds()Z
-                move-result v1
-            """
+            """, listOf(ExternalLabel("show_video_ads", loadVideoAdsFingerprintMethod.instruction(0)))
         )
 
         return PatchResultSuccess()
