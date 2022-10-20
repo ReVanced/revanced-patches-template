8f1d1092b919f06595f001d4df8add6b0f82e676
johnconner122 <107796137+johnconner122@users.noreply.github.com>
2022-10-20 06:28:20 +0500

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    kotlin("jvm") version "1.7.0"
+}
+
+group = "app.revanced"
+
+val githubUsername: String = project.findProperty("gpr.user") as? String ?: System.getenv("GITHUB_ACTOR")
+val githubPassword: String = project.findProperty("gpr.key") as? String ?: System.getenv("GITHUB_TOKEN")
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    maven {
+        url = uri("https://maven.pkg.github.com/revanced/revanced-patcher")
+        credentials {
+            username = githubUsername
+            password = githubPassword
+        }
+    }
+}
+
+dependencies {
+    implementation("app.revanced:revanced-patcher:6.0.0")
+    implementation("app.revanced:multidexlib2:2.5.2.r2")
+    // Required for meta
+    implementation("com.google.code.gson:gson:2.9.1")
+}
+
+tasks {
+    register<DefaultTask>("generateBundle") {
+        description = "Generate dex files from build and bundle them in the jar file"
+        dependsOn(build)
+
+        doLast {
+            val androidHome = System.getenv("ANDROID_HOME") ?: throw GradleException("ANDROID_HOME not found")
+            val d8 = "${androidHome}/build-tools/32.0.0/d8"
+            val input = configurations.archives.get().allArtifacts.files.files.first().absolutePath
+            val work = File("${buildDir}/libs")
+
+            exec {
+                workingDir = work
+                commandLine = listOf(d8, input)
+            }
+
+            exec {
+                workingDir = work
+                commandLine = listOf("zip", "-u", input, "classes.dex")
+            }
+        }
+    }
+    register<JavaExec>("generateMeta") {
+        description = "Generate metadata for this bundle"
+        dependsOn(build)
+
+        classpath = sourceSets["main"].runtimeClasspath
+        mainClass.set("app.revanced.meta.Meta")
+    }
+    // Dummy task to fix the Gradle semantic-release plugin.
+    // Remove this if you forked it to support building only.
+    // Tracking issue: https://github.com/KengoTODA/gradle-semantic-release-plugin/issues/435
+    register<DefaultTask>("publish") {
+        group = "publish"
+        description = "Dummy task"
+        dependsOn(named("generateBundle"), named("generateMeta"))
+    }
+}
