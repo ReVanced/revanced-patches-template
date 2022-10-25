18c5661ce4da3b74d058c86453aba3c2bf4478db
Rizwan <connerjohn2214@gmail.com>
2022-10-25 19:28:58 +0500

    playerbuttonoverlaypatch

---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/playerbuttonoverlay/annotations/PlayerButtonOverlayCompatibility.kt
@@ -0,0 +1,12 @@
+package app.revanced.patches.youtube.layout.playerbuttonoverlay.annotations
+import app.revanced.patcher.annotation.Compatibility
+import app.revanced.patcher.annotation.Package
+
+@Compatibility(
+    [
+        Package("com.google.android.youtube", arrayOf("17.34.36","17.36.37", "17.36.39", "17.38.36", "17.39.35" ))
+    ]
+)
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+internal annotation class PlayerButtonOverlayCompatibility
---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/playerbuttonoverlay/patch/PlayerButtonOverlayPatch.kt
@@ -0,0 +1,53 @@
+package app.revanced.patches.youtube.layout.playerbuttonoverlay.patch
+
+import app.revanced.extensions.doRecursively
+import app.revanced.extensions.startsWithAny
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.DependsOn
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.patch.ResourcePatch
+import app.revanced.patches.youtube.layout.playerbuttonoverlay.annotations.PlayerButtonOverlayCompatibility
+import app.revanced.patches.youtube.misc.manifest.patch.FixLocaleConfigErrorPatch
+import org.w3c.dom.Element
+
+@Patch
+@DependsOn([FixLocaleConfigErrorPatch::class])
+@Name("remove-playerbutton-background")
+@Description("Disable Player Button Overlay Background")
+@PlayerButtonOverlayCompatibility
+@Version("0.0.1")
+class PlayerButtonOverlayPatch : ResourcePatch {
+    private val resourceFileNames = arrayOf(
+        "player_button_circle_background.xml"
+    )
+
+    // the attributes to change the value of
+    private val replacements = arrayOf(
+        "color"
+    )
+
+    override fun execute(context: ResourceContext): PatchResult {
+        context.forEach {
+            if (!it.name.startsWithAny(*resourceFileNames)) return@forEach
+
+            // for each file in the "layouts" directory replace all necessary attributes content
+            context.xmlEditor[it.absolutePath].use { editor ->
+                editor.file.doRecursively { node ->
+                    replacements.forEach replacement@{ replacement ->
+                        if (node !is Element) return@replacement
+
+                        node.getAttributeNode("android:$replacement")?.let { attribute ->
+                            attribute.textContent = "@android:color/transparent"
+                        }
+                    }
+                }
+            }
+        }
+        return PatchResultSuccess()
+    }
+}
