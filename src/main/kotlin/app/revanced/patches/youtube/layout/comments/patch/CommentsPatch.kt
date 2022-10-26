0053a0e6192f32a214c1405d2087088e3bf5b575
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-10-26 10:31:52 +0200

    feat: hide shorts comments button

--- src/main/kotlin/app/revanced/patches/youtube/layout/comments/patch/CommentsPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/comments/patch/CommentsPatch.kt
@@ -3,28 +3,40 @@ package app.revanced.patches.youtube.layout.comments.patch
 import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
-import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.addInstructions
+import app.revanced.patcher.extensions.instruction
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint.Companion.resolve
+import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
-import app.revanced.patcher.patch.ResourcePatch
 import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patches.youtube.ad.general.bytecode.patch.GeneralBytecodeAdsPatch
 import app.revanced.patches.youtube.layout.comments.annotations.CommentsCompatibility
+import app.revanced.patches.youtube.layout.comments.fingerprints.ShortsCommentsButtonFingerprint
+import app.revanced.patches.youtube.misc.integrations.patch.IntegrationsPatch
 import app.revanced.patches.youtube.misc.mapping.patch.ResourceMappingResourcePatch
 import app.revanced.patches.youtube.misc.settings.bytecode.patch.SettingsPatch
 import app.revanced.patches.youtube.misc.settings.framework.components.impl.PreferenceScreen
 import app.revanced.patches.youtube.misc.settings.framework.components.impl.StringResource
 import app.revanced.patches.youtube.misc.settings.framework.components.impl.SwitchPreference
+import org.jf.dexlib2.Opcode
+import org.jf.dexlib2.iface.instruction.OneRegisterInstruction
 
 @Patch
-@DependsOn([ResourceMappingResourcePatch::class, GeneralBytecodeAdsPatch::class])
+@DependsOn([IntegrationsPatch::class, SettingsPatch::class, ResourceMappingResourcePatch::class, GeneralBytecodeAdsPatch::class])
 @Name("comments")
 @Description("Hides comments components below the video player.")
 @CommentsCompatibility
 @Version("0.0.1")
-class CommentsPatch : ResourcePatch {
-    override fun execute(context: ResourceContext): PatchResult {
+class CommentsPatch : BytecodePatch(
+    listOf(
+        ShortsCommentsButtonFingerprint
+    )
+) {
+    override fun execute(context: BytecodeContext): PatchResult {
         SettingsPatch.PreferenceScreen.LAYOUT.addPreferences(
             PreferenceScreen(
                 "revanced_comments",
@@ -44,10 +56,48 @@ class CommentsPatch : ResourcePatch {
                         StringResource("revanced_hide_preview_comment_on", "Preview comment is hidden"),
                         StringResource("revanced_hide_preview_comment_off", "Preview comment is shown")
                     ),
+                    SwitchPreference(
+                        "revanced_hide_shorts_comments_button",
+                        StringResource("revanced_hide_shorts_comments_button_title", "Hide shorts comments button"),
+                        false,
+                        StringResource("revanced_hide_shorts_comments_button_on", "Shorts comments button is hidden"),
+                        StringResource("revanced_hide_shorts_comments_button_off", "Shorts comments button is shown")
+                    ),
                 ),
                 StringResource("revanced_comments_summary", "Manage the visibility of comments section components")
             )
         )
+
+        val shortsCommentsButtonResult = ShortsCommentsButtonFingerprint.result!!
+        val shortsCommentsButtonMethod = shortsCommentsButtonResult.mutableMethod
+
+        val checkCastAnchorFingerprint = object : MethodFingerprint(
+            opcodes = listOf(
+                Opcode.CONST,
+                Opcode.CONST_HIGH16,
+                Opcode.IF_EQZ,
+                Opcode.CONST,
+                Opcode.INVOKE_STATIC,
+                Opcode.MOVE_RESULT_OBJECT,
+                Opcode.CHECK_CAST,
+            )
+        ) {}
+        val checkCastAnchorIndex = checkCastAnchorFingerprint.also {
+            it.resolve(context, shortsCommentsButtonMethod, shortsCommentsButtonResult.classDef)
+        }.result!!.scanResult.patternScanResult!!.endIndex
+
+        shortsCommentsButtonMethod.addInstructions(
+            checkCastAnchorIndex + 1, """
+                invoke-static {v${(shortsCommentsButtonMethod.instruction(checkCastAnchorIndex) as OneRegisterInstruction).registerA}}, Lapp/revanced/integrations/patches/HideShortsCommentsButtonPatch;->hideShortsCommentsButton(Landroid/view/View;)V
+            """
+        )
+
         return PatchResultSuccess()
+    }
+
+    internal companion object {
+        internal var shortsCommentsButtonId: Long = ResourceMappingResourcePatch.resourceMappings.single {
+            it.type == "drawable" && it.name == "ic_right_comment_32c"
+        }.id
     }
 }
