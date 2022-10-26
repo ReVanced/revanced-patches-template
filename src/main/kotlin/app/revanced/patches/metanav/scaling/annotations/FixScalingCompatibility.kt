2fffa4e17cceb87f22d223a66ae663796a5077cd
oSumAtrIX <johan.melkonyan1@web.de>
2022-10-26 06:56:18 +0200

    feat: remove partially complete patch

--- src/main/kotlin/app/revanced/patches/metanav/scaling/annotations/FixScalingCompatibility.kt
+++
@@ -1,9 +0,0 @@
-package app.revanced.patches.metanav.scaling.annotations
-
-import app.revanced.patcher.annotation.Compatibility
-import app.revanced.patcher.annotation.Package
-
-@Compatibility([Package("com.metanav", arrayOf("0.1.0"))])
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-internal annotation class FixScalingCompatibility
