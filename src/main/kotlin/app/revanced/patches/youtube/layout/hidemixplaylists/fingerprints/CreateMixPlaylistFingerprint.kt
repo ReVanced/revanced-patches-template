acde5f066053ca3d58ca09fc2a6a1381ba27d84b
johnconner122 <107796137+johnconner122@users.noreply.github.com>
2022-10-27 01:51:49 +0500

    feat(youtube/hide-mix-playlists): hide in video suggestions (#854)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patches/youtube/layout/hidemixplaylists/fingerprints/CreateMixPlaylistFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hidemixplaylists/fingerprints/CreateMixPlaylistFingerprint.kt
@@ -8,10 +8,10 @@ import app.revanced.patches.youtube.layout.hidemixplaylists.annotations.MixPlayl
 import org.jf.dexlib2.AccessFlags
 import org.jf.dexlib2.Opcode
 
-@Name("mix-playlists-fingerprint")
+@Name("create-mix-playlist-fingerprint")
 @MixPlaylistsPatchCompatibility
 @Version("0.0.1")
-object MixPlaylistsPatchFingerprint : MethodFingerprint(
+object CreateMixPlaylistFingerprint : MethodFingerprint(
     "V", AccessFlags.PUBLIC or AccessFlags.CONSTRUCTOR, listOf("L", "L", "L", "L", "L", "L", "L"), listOf(
         Opcode.INVOKE_DIRECT,
         Opcode.IPUT_OBJECT,
@@ -26,6 +26,5 @@ object MixPlaylistsPatchFingerprint : MethodFingerprint(
         Opcode.IPUT_OBJECT,
         Opcode.IPUT_OBJECT,
         Opcode.INVOKE_VIRTUAL,
-        Opcode.RETURN_VOID
     )
 )
