9072986f99e624386ff51c7eeb1d65158bd9249a
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-10-28 01:40:24 +0200

    feat: bump YouTube patches to v17.41.37 (#878)

--- src/main/kotlin/app/revanced/patches/youtube/layout/pivotbar/shortsbutton/fingerprints/PivotBarEnumFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/pivotbar/shortsbutton/fingerprints/PivotBarEnumFingerprint.kt
@@ -11,11 +11,11 @@ import org.jf.dexlib2.Opcode
 @Version("0.0.1")
 object PivotBarEnumFingerprint : MethodFingerprint(
     opcodes = listOf(
-        Opcode.IF_NEZ,
+        Opcode.INVOKE_STATIC,
+        Opcode.MOVE_RESULT_OBJECT,
+        Opcode.IF_NEZ, // target reference
         Opcode.SGET_OBJECT,
         Opcode.INVOKE_VIRTUAL,
         Opcode.MOVE_RESULT,
-        Opcode.IGET,
-        Opcode.AND_INT_LIT8, // unique instruction anchor
     )
 )
