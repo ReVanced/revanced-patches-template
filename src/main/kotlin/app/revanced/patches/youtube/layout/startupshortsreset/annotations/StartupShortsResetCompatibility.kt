5e25d2e03e38840cc24527114901fe637e83872b
oSumAtrIX <johan.melkonyan1@web.de>
2022-10-28 11:54:05 +0200

    Revert "feat: bump YouTube patches to v17.41.37 (#878)"
    This reverts commit 9072986f99e624386ff51c7eeb1d65158bd9249a.

--- src/main/kotlin/app/revanced/patches/youtube/layout/startupshortsreset/annotations/StartupShortsResetCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/startupshortsreset/annotations/StartupShortsResetCompatibility.kt
@@ -5,7 +5,7 @@ import app.revanced.patcher.annotation.Package
 
 @Compatibility(
     [Package(
-        "com.google.android.youtube", arrayOf("17.33.42", "17.36.37", "17.41.37")
+        "com.google.android.youtube", arrayOf("17.33.42", "17.36.37")
     )]
 )
 @Target(AnnotationTarget.CLASS)
