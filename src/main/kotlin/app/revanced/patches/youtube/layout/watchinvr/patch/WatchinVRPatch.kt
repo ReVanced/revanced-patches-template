ffda42acb86897271209a8b3346c1e888c9f35e5
oSumAtrIX <johan.melkonyan1@web.de>
2022-10-30 13:00:36 +0100

    Update src/main/kotlin/app/revanced/patches/youtube/layout/watchinvr/patch/WatchinVRPatch.kt

--- src/main/kotlin/app/revanced/patches/youtube/layout/watchinvr/patch/WatchinVRPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/watchinvr/patch/WatchinVRPatch.kt
@@ -34,7 +34,7 @@ class WatchinVRPatch : BytecodePatch(
                 "revanced_hide_watch_in_vr",
                 StringResource("revanced_hide_watch_in_vr_title", "Hide Watch in VR option"),
                 false,
-                StringResource("revanced_hide_watch_in_vr_summary_on", "Watch in vr item is hidden from menu item"),
+                StringResource("revanced_hide_watch_in_vr_summary_on", "Watch in VR option is hidden"),
                 StringResource("revanced_hide_watch_in_vr_summary_off", "Watch in vr item is shown from menu item")
             )
         )
