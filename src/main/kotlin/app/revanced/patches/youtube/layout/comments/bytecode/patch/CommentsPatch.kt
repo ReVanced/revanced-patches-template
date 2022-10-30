2b30748eabd23cbc254ffd205dfe35af5421387e
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-10-30 16:11:54 +0100

    fix: improved code readability

--- src/main/kotlin/app/revanced/patches/youtube/layout/comments/bytecode/patch/CommentsPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/comments/bytecode/patch/CommentsPatch.kt
@@ -26,6 +26,7 @@ import org.jf.dexlib2.Opcode
 import org.jf.dexlib2.builder.instruction.BuilderInstruction35c
 import org.jf.dexlib2.iface.instruction.OneRegisterInstruction
 import org.jf.dexlib2.iface.instruction.WideLiteralInstruction
+import org.jf.dexlib2.util.MethodUtil
 
 @Patch
 @DependsOn([IntegrationsPatch::class, CommentsResourcePatch::class])
@@ -68,10 +69,10 @@ class CommentsPatch : BytecodePatch(
         )
 
     // hide fullscreen live chat button
-        // save the wide literal resource value
         val liveChatFullscreenButtonMethod = LiveChatFullscreenButtonFingerprint.result!!.mutableMethod
         val liveChatButtonIndex = liveChatFullscreenButtonMethod.implementation?.instructions?.indexOfFirst {
-            it.opcode.ordinal == Opcode.CONST.ordinal && (it as WideLiteralInstruction).wideLiteral == CommentsResourcePatch.liveChatButtonId
+            it.opcode.ordinal == Opcode.CONST.ordinal &&
+            (it as WideLiteralInstruction).wideLiteral == CommentsResourcePatch.liveChatButtonId
         }!!
 
         liveChatFullscreenButtonMethod.addInstruction(
@@ -80,52 +81,23 @@ class CommentsPatch : BytecodePatch(
             """
         )
 
-        // apply the patch to hide the view, whose id is equal to the previous wide literal value
-        // total setVisibilityInstructions to patch inside the class = 4
-        fun visibilityPatchBuilder(viewRegister: Int, visibilityRegister: Int) = """
-            invoke-static {v$viewRegister, v$visibilityRegister}, Lapp/revanced/integrations/patches/HideLiveChatFullScreenButtonPatch;->hideLiveChatFullScreenButton(Landroid/view/View;I)I
-            move-result v$visibilityRegister
-        """
-
-        val liveChatFullScreenButtonVisibilityResult = LiveChatFullscreenButtonVisibilityFingerprint.result!!
-        val liveChatFullScreenButtonVisibilityMethod = liveChatFullScreenButtonVisibilityResult.mutableMethod
-        val setVisibilityValueAnchorFingerprint = object : MethodFingerprint(
-            opcodes = listOf(
-                Opcode.INVOKE_VIRTUAL,
-                Opcode.CONST_4,
-                Opcode.INVOKE_DIRECT,
-                Opcode.IGET_OBJECT,
-                Opcode.IGET_OBJECT,
-                Opcode.NEW_INSTANCE,
-                Opcode.INVOKE_DIRECT,
-                Opcode.INVOKE_INTERFACE,
-                Opcode.RETURN_VOID,
-                Opcode.IGET_WIDE,
-                Opcode.IGET_OBJECT,
-                Opcode.INVOKE_VIRTUAL,
-            )
-        ) {}
-        val setVisibilityValueAnchorScan = setVisibilityValueAnchorFingerprint.also {
-            it.resolve(context, liveChatFullScreenButtonVisibilityMethod, liveChatFullScreenButtonVisibilityResult.classDef)
-        }.result!!.scanResult.patternScanResult!!
-
-        for (i in 0..1) {
-            val invokeVirtualIndex =
-                if (i == 0) setVisibilityValueAnchorScan.endIndex
-                else setVisibilityValueAnchorScan.startIndex
-            val invokeVirtualInstruction =
-                (liveChatFullScreenButtonVisibilityMethod.instruction(invokeVirtualIndex) as BuilderInstruction35c)
-
-            liveChatFullScreenButtonVisibilityMethod.addInstructions(
-                invokeVirtualIndex,
-                visibilityPatchBuilder(
-                    invokeVirtualInstruction.registerC,
-                    invokeVirtualInstruction.registerD
+        val setVisibilityAnchorFingerprints = listOf(
+            object : MethodFingerprint(
+                opcodes = listOf(
+                    Opcode.INVOKE_VIRTUAL,
+                    Opcode.CONST_4,
+                    Opcode.INVOKE_DIRECT,
+                    Opcode.IGET_OBJECT,
+                    Opcode.IGET_OBJECT,
+                    Opcode.NEW_INSTANCE,
+                    Opcode.INVOKE_DIRECT,
+                    Opcode.INVOKE_INTERFACE,
+                    Opcode.RETURN_VOID,
+                    Opcode.IGET_WIDE,
+                    Opcode.IGET_OBJECT,
+                    Opcode.INVOKE_VIRTUAL,
                 )
-            )
-        }
-
-        val SetVisibilityAnchorFingerprints = listOf(
+            ) {},
             object : MethodFingerprint(
                 "V", AccessFlags.PUBLIC or AccessFlags.FINAL, null, listOf(
                     Opcode.IGET_OBJECT,
@@ -141,24 +113,47 @@ class CommentsPatch : BytecodePatch(
                 )
             ) {}
         )
-        SetVisibilityAnchorFingerprints.resolve(
+        setVisibilityAnchorFingerprints.resolve(
             context,
-            listOf(liveChatFullScreenButtonVisibilityResult.classDef)
+            listOf(LiveChatFullscreenButtonVisibilityFingerprint.result!!.classDef)
         )
 
-        for (SetVisibilityAnchorFingerprint in SetVisibilityAnchorFingerprints) {
-            val setVisibilityAnchorResult = SetVisibilityAnchorFingerprint.result!!
-            val setVisibilityAnchorMethod = setVisibilityAnchorResult.mutableMethod
-            val setVisibilityAnchorIndex = setVisibilityAnchorResult.scanResult.patternScanResult!!.endIndex
-            val invokeVirtualInstruction = (setVisibilityAnchorMethod.instruction(setVisibilityAnchorIndex) as BuilderInstruction35c)
+        for (i in 0..setVisibilityAnchorFingerprints.size) {
+            with (setVisibilityAnchorFingerprints[i].result!!) {
+                val setVisibilityAnchorMethod = mutableMethod
 
-            setVisibilityAnchorMethod.addInstructions(
-                setVisibilityAnchorIndex,
-                visibilityPatchBuilder(
-                    invokeVirtualInstruction.registerC,
-                    invokeVirtualInstruction.registerD
+                fun visibilityPatchBuilder(viewRegister: Int, visibilityRegister: Int) = """
+                    invoke-static {v$viewRegister, v$visibilityRegister}, Lapp/revanced/integrations/patches/HideLiveChatFullScreenButtonPatch;->hideLiveChatFullScreenButton(Landroid/view/View;I)I
+                    move-result v$visibilityRegister
+                """
+
+                val invokeVirtualEndIndex =
+                    scanResult.patternScanResult!!.endIndex
+                val invokeVirtualEndInstruction = (setVisibilityAnchorMethod.instruction(
+                    invokeVirtualEndIndex) as BuilderInstruction35c
                 )
-            )
+                setVisibilityAnchorMethod.addInstructions(
+                    invokeVirtualEndIndex,
+                    visibilityPatchBuilder(
+                        invokeVirtualEndInstruction.registerC,
+                        invokeVirtualEndInstruction.registerD
+                    )
+                )
+
+                if (i == 0) {
+                    val invokeVirtualStartIndex = scanResult.patternScanResult!!.startIndex
+                    val invokeVirtualStartInstruction = (setVisibilityAnchorMethod.instruction(
+                        invokeVirtualStartIndex) as BuilderInstruction35c
+                    )
+                    setVisibilityAnchorMethod.addInstructions(
+                        invokeVirtualStartIndex,
+                        visibilityPatchBuilder(
+                            invokeVirtualStartInstruction.registerC,
+                            invokeVirtualStartInstruction.registerD
+                        )
+                    )
+                }
+            }
         }
 
         return PatchResultSuccess()
