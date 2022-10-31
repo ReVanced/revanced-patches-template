6440298a43622eb8ad9758d2958b743ebed0b5e8
FineFindus <63370021+FineFindus@users.noreply.github.com>
2022-10-31 17:30:48 +0100

    Reworked implementation

--- src/main/kotlin/app/revanced/patches/youtube/layout/branding/icon/patch/CustomBrandingPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/branding/icon/patch/CustomBrandingPatch.kt
@@ -17,12 +17,29 @@ import java.nio.file.Files
 @Patch
 @DependsOn([FixLocaleConfigErrorPatch::class])
 @Name("custom-branding")
-@Description("Changes the YouTube launcher icon and name to your choice (defaults to ReVanced).")
+@Description("Changes the YouTube / YT Music launcher icon and name to your choice (defaults to ReVanced).")
 @CustomBrandingCompatibility
 @Version("0.0.1")
 class CustomBrandingPatch : ResourcePatch {
     override fun execute(context: ResourceContext): PatchResult {
+        val manifest = context["AndroidManifest.xml"]
+
+        val is_music = manifest.readText().contains("package=\"com.google.android.apps.youtube.music");
+
+        // change the name of the app
+        manifest.writeText(
+            manifest.readText()
+                .replace(
+                    "android:label=\"@string/application_name",
+                    "android:label=\"${ if (is_music) musicName else youtubeName}"
+                )
+        )
+
+
         fun copyResources(resourceGroups: List<ResourceUtils.ResourceGroup>) {
+            val iconPath = if (is_music) musicIconPath else youtubeIconPath
+            val default = if (is_music) "branding/music" else "branding"
+
             iconPath?.let { iconPathString ->
                 val iconPath = File(iconPathString)
                 val resourceDirectory = context["res"]
@@ -38,12 +55,14 @@ class CustomBrandingPatch : ResourcePatch {
                         )
                     }
                 }
-            } ?: resourceGroups.forEach { context.copyResources("branding", it) }
+            } ?: resourceGroups.forEach { context.copyResources(default, it) }
         }
 
+        val appName = if (is_music) "youtube_music" else "youtube" 
+
         val iconResourceFileNames = arrayOf(
-            "adaptiveproduct_youtube_background_color_108",
-            "adaptiveproduct_youtube_foreground_color_108",
+            "adaptiveproduct_${appName}_background_color_108",
+            "adaptiveproduct_${appName}_foreground_color_108",
             "ic_launcher",
             "ic_launcher_round"
         ).map { "$it.png" }.toTypedArray()
@@ -58,35 +77,44 @@ class CustomBrandingPatch : ResourcePatch {
             .map(::createGroup)
             .let(::copyResources)
 
-        // change the name of the app
-        val manifest = context["AndroidManifest.xml"]
-        manifest.writeText(
-            manifest.readText()
-                .replace(
-                    "android:label=\"@string/application_name",
-                    "android:label=\"$appName"
-                )
-        )
-
         return PatchResultSuccess()
     }
 
     companion object : OptionsContainer() {
-        private var appName: String? by option(
+        private var youtubeName: String? by option(
             PatchOption.StringOption(
-                key = "appName",
+                key = "youtubeAppName",
                 default = "YouTube ReVanced",
                 title = "Application Name",
-                description = "The name of the application it will show on your home screen.",
+                description = "The name of the YouTube app on the home screen.",
+                required = true
+            )
+        )
+        
+        private var musicName: String? by option(
+            PatchOption.StringOption(
+                key = "musicAppName",
+                default = "YouTube Music ReVanced",
+                title = "Application Name",
+                description = "The name of the YouTube Music app on the home screen.",
                 required = true
             )
         )
 
-        private var iconPath: String? by option(
+        private var youtubeIconPath: String? by option(
             PatchOption.StringOption(
-                key = "iconPath",
+                key = "youtubeIconPath",
                 default = null,
-                title = "App Icon Path",
+                title = "YouTube App Icon Path",
+                description = "A path containing mipmap resource folders with icons."
+            )
+        )
+
+        private var musicIconPath: String? by option(
+            PatchOption.StringOption(
+                key = "musicIconPath",
+                default = null,
+                title = "YouTube Music App Icon Path",
                 description = "A path containing mipmap resource folders with icons."
             )
         )
