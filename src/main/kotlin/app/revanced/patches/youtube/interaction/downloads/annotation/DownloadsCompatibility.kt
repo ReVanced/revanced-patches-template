93c9e941ff9acab5be8fd21247add10784bcbe17
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-11-01 15:35:53 +0100

    fix: remove compatibility for 17.34.46 and older

--- src/main/kotlin/app/revanced/patches/youtube/interaction/downloads/annotation/DownloadsCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/youtube/interaction/downloads/annotation/DownloadsCompatibility.kt
@@ -5,7 +5,7 @@ import app.revanced.patcher.annotation.Package
 
 @Compatibility(
     [Package(
-        "com.google.android.youtube", arrayOf("17.27.39", "17.29.34", "17.32.35", "17.33.42", "17.34.35", "17.34.36", "17.36.37", "17.41.37", "17.42.35", "17.43.36")
+        "com.google.android.youtube", arrayOf("17.36.37", "17.41.37", "17.42.35", "17.43.36")
     )]
 )
 @Target(AnnotationTarget.CLASS)
