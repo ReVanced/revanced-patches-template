8ff7c9e62804a8f704d6b2e7192654633f5a73ee
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-11-02 02:08:39 +0100

    fix: removed unused imports

--- src/main/kotlin/app/revanced/patches/youtube/layout/comments/bytecode/fingerprints/ShortsCommentsButtonFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/comments/bytecode/fingerprints/ShortsCommentsButtonFingerprint.kt
@@ -2,11 +2,9 @@ package app.revanced.patches.youtube.layout.comments.bytecode.fingerprints
 
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
-import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import app.revanced.patches.youtube.layout.comments.annotations.CommentsCompatibility
 import app.revanced.patches.youtube.layout.comments.resource.patch.CommentsResourcePatch
-import org.jf.dexlib2.AccessFlags
 import org.jf.dexlib2.Opcode
 import org.jf.dexlib2.iface.instruction.WideLiteralInstruction
 
