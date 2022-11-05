b42e04884bb9a60c3df30b668bfba55b1cf8eaa4
oSumAtrIX <johan.melkonyan1@web.de>
2022-11-05 22:00:37 +0100

    fix(youtube/video-information): use correct compatibility annotation

--- src/main/kotlin/app/revanced/patches/youtube/misc/video/information/fingerprints/VideoLengthFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/video/information/fingerprints/VideoLengthFingerprint.kt
@@ -5,11 +5,11 @@ import app.revanced.patcher.annotation.Version
 
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import app.revanced.patches.youtube.layout.sponsorblock.annotations.SponsorBlockCompatibility
+import app.revanced.patches.youtube.misc.video.information.annotation.VideoInformationCompatibility
 import org.jf.dexlib2.Opcode
 
 @Name("video-length-fingerprint")
-
-@SponsorBlockCompatibility
+@VideoInformationCompatibility
 @Version("0.0.1")
 object VideoLengthFingerprint : MethodFingerprint(
     opcodes = listOf(
--- src/main/kotlin/app/revanced/patches/youtube/misc/video/information/fingerprints/VideoTimeFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/video/information/fingerprints/VideoTimeFingerprint.kt
@@ -5,9 +5,10 @@ import app.revanced.patcher.annotation.Version
 
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import app.revanced.patches.youtube.layout.sponsorblock.annotations.SponsorBlockCompatibility
+import app.revanced.patches.youtube.misc.video.information.annotation.VideoInformationCompatibility
 
 @Name("video-time-fingerprint")
-@SponsorBlockCompatibility
+@VideoInformationCompatibility
 @Version("0.0.1")
 object VideoTimeFingerprint : MethodFingerprint(
     strings = listOf("MedialibPlayerTimeInfo{currentPositionMillis=")
