19f3f038585c313a969adf3d4095a60ab4c83ede
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-11-08 00:11:12 +0100

    fix(youtube/hide-info-cards): remove initial popup of info-cards (#992)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hideinfocards/patch/HideInfocardsPatch.kt
@@ -0,0 +1,71 @@
+package app.revanced.patches.youtube.layout.hideinfocards.patch
+
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.addInstructions
+import app.revanced.patcher.extensions.instruction
+import app.revanced.patcher.extensions.replaceInstruction
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint.Companion.resolve
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.DependsOn
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.util.smali.ExternalLabel
+import app.revanced.patches.youtube.layout.hideinfocards.annotations.HideInfocardsCompatibility
+import app.revanced.patches.youtube.layout.hideinfocards.fingerprints.InfocardsIncognitoFingerprint
+import app.revanced.patches.youtube.layout.hideinfocards.fingerprints.InfocardsMethodCallFingerprint
+import app.revanced.patches.youtube.layout.hideinfocards.fingerprints.InfocardsIncognitoParentFingerprint
+import app.revanced.patches.youtube.layout.hideinfocards.resource.patch.HideInfocardsResourcePatch
+import app.revanced.patches.youtube.misc.integrations.patch.IntegrationsPatch
+import org.jf.dexlib2.Opcode
+import org.jf.dexlib2.builder.instruction.BuilderInstruction35c
+
+@Patch
+@DependsOn([IntegrationsPatch::class, HideInfocardsResourcePatch::class])
+@Name("hide-info-cards")
+@Description("Hides info-cards in videos.")
+@HideInfocardsCompatibility
+@Version("0.0.1")
+class HideInfocardsPatch : BytecodePatch(
+    listOf(
+        InfocardsIncognitoParentFingerprint,
+        InfocardsMethodCallFingerprint,
+    )
+) {
+    override fun execute(context: BytecodeContext): PatchResult {
+        with(InfocardsIncognitoFingerprint.also {
+            it.resolve(context, InfocardsIncognitoParentFingerprint.result!!.classDef)
+        }.result!!.mutableMethod) {
+                val invokeInstructionIndex = implementation!!.instructions.indexOfFirst {
+                    it.opcode.ordinal == Opcode.INVOKE_VIRTUAL.ordinal &&
+                    ((it as? BuilderInstruction35c)?.reference.toString() ==
+                        "Landroid/view/View;->setVisibility(I)V")
+                }
+
+                replaceInstruction(invokeInstructionIndex, """
+                    invoke-static {v${(instruction(invokeInstructionIndex) as? BuilderInstruction35c)?.registerC}}, Lapp/revanced/integrations/patches/HideInfocardsPatch;->hideInfocardsIncognito(Landroid/view/View;)V
+                    """
+                )
+        }
+
+        with(InfocardsMethodCallFingerprint.result!!) {
+            val hideInfocardsCallMethod = mutableMethod
+
+            val invokeInterfaceIndex = scanResult.patternScanResult!!.endIndex
+            val toggleRegister = hideInfocardsCallMethod.implementation!!.registerCount - 1
+
+            hideInfocardsCallMethod.addInstructions(
+                invokeInterfaceIndex, """
+                    invoke-static {}, Lapp/revanced/integrations/patches/HideInfocardsPatch;->hideInfocardsMethodCall()Z
+                    move-result v$toggleRegister
+                    if-nez v$toggleRegister, :hide_info_cards
+                """, listOf(ExternalLabel("hide_info_cards", hideInfocardsCallMethod.instruction(invokeInterfaceIndex + 1)))
+            )
+        }
+
+        return PatchResultSuccess()
+    }
+}
---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hideinfocards/resource/patch/HideInfocardsResourcePatch.kt
@@ -0,0 +1,40 @@
+package app.revanced.patches.youtube.layout.hideinfocards.resource.patch
+
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.ResourcePatch
+import app.revanced.patcher.patch.annotations.DependsOn
+import app.revanced.patches.youtube.layout.hideinfocards.annotations.HideInfocardsCompatibility
+import app.revanced.patches.youtube.misc.mapping.patch.ResourceMappingResourcePatch
+import app.revanced.patches.youtube.misc.settings.bytecode.patch.SettingsPatch
+import app.revanced.patches.youtube.misc.settings.framework.components.impl.StringResource
+import app.revanced.patches.youtube.misc.settings.framework.components.impl.SwitchPreference
+
+@HideInfocardsCompatibility
+@DependsOn([SettingsPatch::class, ResourceMappingResourcePatch::class])
+@Version("0.0.1")
+class HideInfocardsResourcePatch : ResourcePatch {
+    internal companion object {
+        var drawerResourceId: Long = -1
+    }
+
+    override fun execute(context: ResourceContext): PatchResult {
+        SettingsPatch.PreferenceScreen.LAYOUT.addPreferences(
+            SwitchPreference(
+                "revanced_hide_infocards",
+                StringResource("revanced_hide_infocards_title", "Hide info-cards"),
+                true,
+                StringResource("revanced_hide_infocards_summary_on", "Info-cards are hidden"),
+                StringResource("revanced_hide_infocards_summary_off", "Info-cards are shown")
+            )
+        )
+
+        drawerResourceId = ResourceMappingResourcePatch.resourceMappings.single {
+            it.type == "id" && it.name == "info_cards_drawer_header"
+        }.id
+
+        return PatchResultSuccess()
+    }
+}
