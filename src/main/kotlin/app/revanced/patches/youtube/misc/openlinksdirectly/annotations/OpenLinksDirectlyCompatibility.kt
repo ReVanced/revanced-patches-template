19a9e02e946f41f160a649a6a0b59672983b7d2f
johnconner122 <107796137+johnconner122@users.noreply.github.com>
2022-11-08 22:52:13 +0500

    Create OpenLinksDirectlyCompatibility.kt

---
+++ src/main/kotlin/app/revanced/patches/youtube/misc/openlinksdirectly/annotations/OpenLinksDirectlyCompatibility.kt
@@ -0,0 +1,13 @@
+package app.revanced.patches.youtube.misc.openlinksdirectly.annotations
+
+import app.revanced.patcher.annotation.Compatibility
+import app.revanced.patcher.annotation.Package
+
+@Compatibility(
+    [Package(
+        "com.google.android.youtube", arrayOf("17.34.36", "17.36.39", "17.38.36", "17.39.35" , "17.40.41", "17.42.35", "17.43.36")
+    )]
+)
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+internal annotation class OpenLinksDirectlyCompatibility
