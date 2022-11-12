9ba074784d31c9d7f0d0ae2c60860e2265126a74
FineFindus <63370021+FineFindus@users.noreply.github.com>
2022-11-12 15:03:26 +0100

    feat(ticktick): unlock all themes

---
+++ src/main/kotlin/app/revanced/patches/ticktick/misc/themeunlock/patch/UnlockThemePatch.kt
@@ -0,0 +1,43 @@
+package app.revanced.patches.ticktick.misc.themeunlock.patch
+
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.addInstructions
+import app.revanced.patcher.extensions.removeInstructions
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patches.ticktick.misc.themeunlock.annotations.UnlockThemesCompatibility
+import app.revanced.patches.ticktick.misc.themeunlock.fingerprints.CheckLockedThemesFingerprint
+import app.revanced.patches.ticktick.misc.themeunlock.fingerprints.ProThemesFingerprint
+
+@Patch
+@Name("unlock-themes")
+@Description("Unlocks all themes.")
+@UnlockThemesCompatibility
+@Version("0.0.1")
+class UnlockProPatch : BytecodePatch(
+    listOf(
+        CheckLockedThemesFingerprint,
+        ProThemesFingerprint
+    )
+) {
+    override fun execute(context: BytecodeContext): PatchResult {
+        val lockedThemesMethod = CheckLockedThemesFingerprint.result!!.mutableMethod
+        lockedThemesMethod.addInstructions(
+            0,
+            """
+                const/4 v0, 0x0
+                return v0
+            """
+        )
+        
+        val proThemesMethod = ProThemesFingerprint.result!!.mutableMethod
+        proThemesMethod.removeInstructions(0, 9)
+        
+        return PatchResultSuccess()
+    }
+}
