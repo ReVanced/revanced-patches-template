1ce0625a432b271566cb21d0f0a6e6d82df3ca31
Tim Schneeberger <tim.schneeberger@outlook.de>
2022-11-14 00:27:59 +0100

    feat(twitch): `block-audio-ads` patch (#1041)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

---
+++ src/main/kotlin/app/revanced/patches/twitch/ad/audio/annotations/AudioAdsCompatibility.kt
@@ -0,0 +1,10 @@
+package app.revanced.patches.twitch.ad.audio.annotations
+
+import app.revanced.patcher.annotation.Compatibility
+import app.revanced.patcher.annotation.Package
+
+@Compatibility([Package("tv.twitch.android.app")])
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+internal annotation class AudioAdsCompatibility
+
