1c909525f80ae5a8e8fb5b75f9b4ec2694c2230e
d4rkk3y <vuhoanhuy99@gmail.com>
2022-11-18 17:02:37 +0700

    refactor(tiktok-web-login): use fingerprint to find method.

--- src/main/kotlin/app/revanced/patches/tiktok/misc/loginfallback/patch/TikTokLoginFallbackPatch.kt
+++ src/main/kotlin/app/revanced/patches/tiktok/misc/loginfallback/patch/TikTokLoginFallbackPatch.kt
@@ -3,39 +3,42 @@ package app.revanced.patches.tiktok.misc.loginfallback.patch
 import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
-import app.revanced.patcher.data.impl.BytecodeData
+import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.addInstructions
+import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.annotations.Patch
-import app.revanced.patcher.patch.impl.BytecodePatch
 import app.revanced.patches.tiktok.misc.loginfallback.annotations.TikTokWebLoginCompatibility
+import app.revanced.patches.tiktok.misc.loginfallback.fingerprints.GoogleAuthAvailableFingerprint
+import app.revanced.patches.tiktok.misc.loginfallback.fingerprints.GoogleOneTapAuthAvailableFingerprint
 
 @Patch
 @Name("tiktok-web-login")
 @Description("Allows logging in with a Google account.")
 @TikTokWebLoginCompatibility
 @Version("0.0.1")
-class TikTokLoginFallbackPatch : BytecodePatch() {
-    override fun execute(data: BytecodeData): PatchResult {
-        val googleOneTapAuth = data.findClass("Lcom/bytedance/lobby/google/GoogleOneTapAuth;")!!.resolve()
-        val googleOneTapAuthCheck = googleOneTapAuth.methods.first { it.returnType == "Z" && it.parameters.size == 0 }
-        googleOneTapAuthCheck.addInstructions(
-            0,
-            """
-                const/4 v0, 0x0
-                return v0
-            """
-        )
-        val googleAuth = data.findClass("Lcom/bytedance/lobby/google/GoogleAuth;")!!.resolve()
-        val googleAuthCheck = googleAuth.methods.first { it.returnType == "Z" && it.parameters.size == 0 }
-        googleAuthCheck.addInstructions(
-            0,
-            """
-                const/4 v0, 0x0
-                return v0
-            """
-        )
+class TikTokLoginFallbackPatch : BytecodePatch(
+    listOf(
+        GoogleOneTapAuthAvailableFingerprint,
+        GoogleAuthAvailableFingerprint
+    )
+) {
+    override fun execute(context: BytecodeContext): PatchResult {
+        listOf(
+            GoogleOneTapAuthAvailableFingerprint,
+            GoogleAuthAvailableFingerprint
+        ).forEach {
+            with(it.result!!.mutableMethod) {
+                addInstructions(
+                    0,
+                    """
+                        const/4 v0, 0x0
+                        return v0
+                    """
+                )
+            }
+        }
         return PatchResultSuccess()
     }
 }
