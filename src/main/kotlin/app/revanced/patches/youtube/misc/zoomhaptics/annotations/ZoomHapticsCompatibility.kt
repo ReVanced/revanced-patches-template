041adc981b714e514bc12748bf88d134b1308572
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-11-20 16:24:41 +0300

    feat(youtube): `disable-zoom-haptics` patch (#1079)

---
+++ src/main/kotlin/app/revanced/patches/youtube/misc/zoomhaptics/annotations/ZoomHapticsCompatibility.kt
@@ -0,0 +1,9 @@
+package app.revanced.patches.youtube.misc.zoomhaptics.annotations
+
+import app.revanced.patcher.annotation.Compatibility
+import app.revanced.patcher.annotation.Package
+
+@Compatibility([Package("com.google.android.youtube")])
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+internal annotation class ZoomHapticsCompatibility
