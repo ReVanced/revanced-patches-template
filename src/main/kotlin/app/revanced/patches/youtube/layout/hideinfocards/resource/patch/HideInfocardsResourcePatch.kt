133265da900d25f342a75d0e29ca680209f766b3
oSumAtrIX <johan.melkonyan1@web.de>
2022-11-20 23:13:28 +0100

    refactor(youtube/resource-mapping): move to appropriate package

--- src/main/kotlin/app/revanced/patches/youtube/layout/hideinfocards/resource/patch/HideInfocardsResourcePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hideinfocards/resource/patch/HideInfocardsResourcePatch.kt
@@ -7,7 +7,7 @@ import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.ResourcePatch
 import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patches.youtube.layout.hideinfocards.annotations.HideInfocardsCompatibility
-import app.revanced.patches.youtube.misc.mapping.patch.ResourceMappingPatch
+import app.revanced.patches.shared.mapping.patch.ResourceMappingPatch
 import app.revanced.patches.youtube.misc.settings.bytecode.patch.SettingsPatch
 import app.revanced.patches.youtube.misc.settings.framework.components.impl.StringResource
 import app.revanced.patches.youtube.misc.settings.framework.components.impl.SwitchPreference
