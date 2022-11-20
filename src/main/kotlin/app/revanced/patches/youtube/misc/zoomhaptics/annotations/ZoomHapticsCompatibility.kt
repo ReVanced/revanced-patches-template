9e4877448f955b7cabb3a34a21c0757215871e1b
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-11-20 16:22:36 +0300

    refactor(youtube/disable-zoom-haptics): remove versions array

--- src/main/kotlin/app/revanced/patches/youtube/misc/zoomhaptics/annotations/ZoomHapticsCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/zoomhaptics/annotations/ZoomHapticsCompatibility.kt
@@ -3,11 +3,7 @@ package app.revanced.patches.youtube.misc.zoomhaptics.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility(
-    [Package(
-        "com.google.android.youtube", arrayOf("17.43.36")
-    )]
-)
+@Compatibility([Package("com.google.android.youtube")])
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 internal annotation class ZoomHapticsCompatibility
