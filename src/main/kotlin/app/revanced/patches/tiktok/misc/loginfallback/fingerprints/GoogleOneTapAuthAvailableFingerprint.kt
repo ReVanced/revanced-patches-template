233e578b4d1cd5e783369e0c855e8246d158f8ed
d4rkk3y <43563783+d4rkk3y@users.noreply.github.com>
2022-11-21 22:14:58 +0700

    feat(tiktok): `tiktok-web-login` patch (#593)

---
+++ src/main/kotlin/app/revanced/patches/tiktok/misc/loginfallback/fingerprints/GoogleOneTapAuthAvailableFingerprint.kt
@@ -0,0 +1,20 @@
+package app.revanced.patches.tiktok.misc.loginfallback.fingerprints
+
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.extensions.or
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import app.revanced.patches.tiktok.misc.loginfallback.annotations.TikTokWebLoginCompatibility
+import org.jf.dexlib2.AccessFlags
+
+@Name("google-one-tap-auth-available-fingerprint")
+@TikTokWebLoginCompatibility
+@Version("0.0.1")
+object GoogleOneTapAuthAvailableFingerprint : MethodFingerprint(
+    returnType = "Z",
+    access = AccessFlags.PUBLIC or AccessFlags.FINAL,
+    parameters = listOf(),
+    customFingerprint = { methodDef ->
+        methodDef.definingClass == "Lcom/bytedance/lobby/google/GoogleOneTapAuth;"
+    }
+)
