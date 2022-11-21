13388c3cd0c0f8867edece09daa0f62c30021149
Tim Schneeberger <tim.schneeberger@outlook.de>
2022-11-21 22:01:50 +0100

    refactor(twitch/integrations): Use integrations patch abstraction

--- src/main/kotlin/app/revanced/patches/twitch/misc/integrations/fingerprints/InitFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/twitch/misc/integrations/fingerprints/InitFingerprint.kt
@@ -2,13 +2,13 @@ package app.revanced.patches.twitch.misc.integrations.fingerprints
 
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import app.revanced.patches.twitch.misc.integrations.annotations.TwitchIntegrationsCompatibility
+import app.revanced.shared.patches.AbstractIntegrationsPatch.IntegrationsFingerprint
 
 @Name("init-fingerprint")
 @TwitchIntegrationsCompatibility
 @Version("0.0.1")
-object InitFingerprint : MethodFingerprint(
+object InitFingerprint : IntegrationsFingerprint(
     customFingerprint = { methodDef ->
         methodDef.definingClass.endsWith("/TwitchApplication;") &&
                 methodDef.name == "onCreate"
--- src/main/kotlin/app/revanced/patches/twitch/misc/integrations/patch/TwitchIntegrationsPatch.kt
+++ src/main/kotlin/app/revanced/patches/twitch/misc/integrations/patch/TwitchIntegrationsPatch.kt
@@ -1,39 +1,13 @@
 package app.revanced.patches.twitch.misc.integrations.patch
 
-import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
-import app.revanced.patcher.annotation.Version
-import app.revanced.patcher.data.BytecodeContext
-import app.revanced.patcher.extensions.addInstruction
-import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.PatchResult
-import app.revanced.patcher.patch.PatchResultError
-import app.revanced.patcher.patch.PatchResultSuccess
-import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patches.twitch.misc.integrations.fingerprints.InitFingerprint
 import app.revanced.patches.twitch.misc.integrations.annotations.TwitchIntegrationsCompatibility
+import app.revanced.shared.patches.AbstractIntegrationsPatch
 
-@Patch
 @Name("twitch-integrations")
-@Description("Applies mandatory patches to implement the ReVanced integrations into the application.")
 @TwitchIntegrationsCompatibility
-@Version("0.0.1")
-class TwitchIntegrationsPatch : BytecodePatch(
-    listOf(
-        InitFingerprint
-    )
-) {
-    override fun execute(context: BytecodeContext): PatchResult {
-        if (context.findClass("Lapp/revanced/twitch/utils/ReVancedUtils") == null)
-            return PatchResultError("Integrations have not been merged yet. This patch can not succeed without the integrations.")
-
-        with(InitFingerprint.result!!.mutableMethod) {
-            val count = implementation!!.registerCount - 1
-            addInstruction(
-                0, "sput-object v$count, Lapp/revanced/twitch/utils/ReVancedUtils;->context:Landroid/content/Context;"
-            )
-        }
-
-        return PatchResultSuccess()
-    }
-}
+class TwitchIntegrationsPatch : AbstractIntegrationsPatch(
+    "Lapp/revanced/tiktok/utils/ReVancedUtils;",
+    listOf(InitFingerprint)
+)
