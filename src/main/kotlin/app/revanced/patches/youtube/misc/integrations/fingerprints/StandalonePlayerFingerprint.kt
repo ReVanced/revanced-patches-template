f745602a802568655b95fb20877b163fdcb25749
Sculas <contact@sculas.xyz>
2022-11-21 22:26:43 +0100

    refactor(integrations): merge integrations code (#1052)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patches/youtube/misc/integrations/fingerprints/StandalonePlayerFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/integrations/fingerprints/StandalonePlayerFingerprint.kt
@@ -2,15 +2,15 @@ package app.revanced.patches.youtube.misc.integrations.fingerprints
 
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import app.revanced.patches.youtube.misc.integrations.annotations.IntegrationsCompatibility
+import app.revanced.shared.patches.AbstractIntegrationsPatch.IntegrationsFingerprint
 
 @Name("standalone-player-fingerprint")
 @IntegrationsCompatibility
 @Version("0.0.1")
-object StandalonePlayerFingerprint : MethodFingerprint(
+object StandalonePlayerFingerprint : IntegrationsFingerprint(
     strings = listOf(
         "Invalid PlaybackStartDescriptor. Returning the instance itself.",
         "com.google.android.music",
-    )
+    ),
 )
