a989a5900d9948e1c596af01cb478dee6c95ac24
Tim Schneeberger <tim.schneeberger@outlook.de>
2022-11-21 23:01:00 +0100

    style: rename patch

--- src/main/kotlin/app/revanced/patches/twitch/misc/integrations/fingerprints/InitFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/twitch/misc/integrations/fingerprints/InitFingerprint.kt
@@ -2,11 +2,11 @@ package app.revanced.patches.twitch.misc.integrations.fingerprints
 
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
-import app.revanced.patches.twitch.misc.integrations.annotations.TwitchIntegrationsCompatibility
+import app.revanced.patches.twitch.misc.integrations.annotations.IntegrationsCompatibility
 import app.revanced.shared.patches.AbstractIntegrationsPatch.IntegrationsFingerprint
 
 @Name("init-fingerprint")
-@TwitchIntegrationsCompatibility
+@IntegrationsCompatibility
 @Version("0.0.1")
 object InitFingerprint : IntegrationsFingerprint(
     customFingerprint = { methodDef ->
--- src/main/kotlin/app/revanced/patches/twitch/misc/integrations/patch/IntegrationsPatch.kt
+++ src/main/kotlin/app/revanced/patches/twitch/misc/integrations/patch/IntegrationsPatch.kt
@@ -2,12 +2,12 @@ package app.revanced.patches.twitch.misc.integrations.patch
 
 import app.revanced.patcher.annotation.Name
 import app.revanced.patches.twitch.misc.integrations.fingerprints.InitFingerprint
-import app.revanced.patches.twitch.misc.integrations.annotations.TwitchIntegrationsCompatibility
+import app.revanced.patches.twitch.misc.integrations.annotations.IntegrationsCompatibility
 import app.revanced.shared.patches.AbstractIntegrationsPatch
 
-@Name("twitch-integrations")
-@TwitchIntegrationsCompatibility
-class TwitchIntegrationsPatch : AbstractIntegrationsPatch(
-    "Lapp/revanced/tiktok/utils/ReVancedUtils;",
+@Name("integrations")
+@IntegrationsCompatibility
+class IntegrationsPatch : AbstractIntegrationsPatch(
+    "Lapp/revanced/twitch/utils/ReVancedUtils;",
     listOf(InitFingerprint)
 )
