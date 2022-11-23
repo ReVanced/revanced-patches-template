c3818befc6e342ff04bc8e9fde84a667c123d7ab
Shervin Koushan <59822141+shervinkoushan@users.noreply.github.com>
2022-11-23 17:19:48 +0100

    feat(anytracker): remove `unlock-premium` patch (#1094)

--- src/main/kotlin/app/revanced/patches/anytracker/misc/premium/annotations/UnlockPremiumCompatibility.kt
+++
@@ -1,9 +0,0 @@
-package app.revanced.patches.anytracker.misc.premium.annotations
-
-import app.revanced.patcher.annotation.Compatibility
-import app.revanced.patcher.annotation.Package
-
-@Compatibility([Package("com.shervinkoushan.anyTracker")])
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-internal annotation class UnlockPremiumCompatibility
