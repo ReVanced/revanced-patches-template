2466b9ae3165b415472ac7f2743728768447d686
Tim Schneeberger <tim.schneeberger@outlook.de>
2022-11-24 13:01:34 +0100

    refactor: use apply() in BasePreference

--- src/main/kotlin/app/revanced/shared/components/settings/BasePreference.kt
+++ src/main/kotlin/app/revanced/shared/components/settings/BasePreference.kt
@@ -22,10 +22,10 @@ internal abstract class BasePreference(
      * @param resourceCallback Called when a resource has been processed.
      */
     open fun serialize(ownerDocument: Document, resourceCallback: ((IResource) -> Unit)? = null): Element {
-        val preferenceElement = ownerDocument.createElement(tag)
-        if(key.isNotEmpty())
-            preferenceElement.setAttribute("android:key", key)
-        preferenceElement.setAttribute("android:title", "@string/${title.also { resourceCallback?.invoke(it) }.name}")
-        return preferenceElement
+        return ownerDocument.createElement(tag).apply {
+            if(key.isNotEmpty())
+                setAttribute("android:key", key)
+            setAttribute("android:title", "@string/${title.also { resourceCallback?.invoke(it) }.name}")
+        }
     }
 }
