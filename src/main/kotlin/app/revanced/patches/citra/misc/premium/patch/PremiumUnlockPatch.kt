24e32243077a4322d9f9694ff15c90be9b2d18b1
José Silveira <josesilveiraa@proton.me>
2022-11-25 18:00:37 -0300

    feat(citra): unlock-premium patch

---
+++ src/main/kotlin/app/revanced/patches/citra/misc/premium/patch/PremiumUnlockPatch.kt
@@ -0,0 +1,37 @@
+package app.revanced.patches.citra.misc.premium.patch
+
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.addInstructions
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultError
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patches.citra.misc.premium.annotations.PremiumUnlockCompatbility
+import app.revanced.patches.citra.misc.premium.fingerprints.PremiumUnlockFingerprint
+
+@Patch
+@Name("premium-unlock")
+@Description("Unlocks premium functions on the emulator.")
+@PremiumUnlockCompatbility
+@Version("0.0.1")
+class PremiumUnlockPatch : BytecodePatch(
+    listOf(PremiumUnlockFingerprint)
+) {
+    override fun execute(context: BytecodeContext): PatchResult {
+        val result = PremiumUnlockFingerprint.result ?: return PatchResultError("PremiumUnlockFingerprint not found")
+
+        result.mutableMethod.addInstructions(
+            0,
+            """
+                const v0, 0x1
+                return v0
+            """
+        )
+        return PatchResultSuccess()
+    }
+
+}
