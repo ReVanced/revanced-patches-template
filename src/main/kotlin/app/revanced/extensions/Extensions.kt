e9e66834c525317962627536b4205528ef6418d4
d4rkk3y <43563783+d4rkk3y@users.noreply.github.com>
2022-11-25 17:20:04 +0700

    feat(tiktok): `sim-spoof` patch (#1088)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/extensions/Extensions.kt
+++ src/main/kotlin/app/revanced/extensions/Extensions.kt
@@ -1,22 +1,21 @@
 package app.revanced.extensions
 
 import app.revanced.patcher.data.BytecodeContext
-import app.revanced.patcher.data.ResourceContext
-import app.revanced.patcher.extensions.addInstructions
-import app.revanced.patcher.patch.PatchResultError
 import app.revanced.patcher.util.proxy.mutableTypes.MutableClass
 import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
-import app.revanced.patcher.util.smali.toInstruction
-import org.jf.dexlib2.AccessFlags
-import org.jf.dexlib2.Opcode
-import org.jf.dexlib2.builder.MutableMethodImplementation
-import org.jf.dexlib2.builder.instruction.BuilderInstruction11n
-import org.jf.dexlib2.builder.instruction.BuilderInstruction11x
-import org.jf.dexlib2.builder.instruction.BuilderInstruction21t
-import org.jf.dexlib2.builder.instruction.BuilderInstruction35c
-import org.jf.dexlib2.immutable.reference.ImmutableMethodReference
+import org.jf.dexlib2.iface.Method
+import org.jf.dexlib2.util.MethodUtil
 import org.w3c.dom.Node
-import java.nio.file.Files
+
+/**
+ * Find the [MutableMethod] from a given [Method] in a [MutableClass].
+ *
+ * @param method The [Method] to find.
+ * @return The [MutableMethod].
+ */
+fun MutableClass.findMutableMethodOf(method: Method) = this.methods.first {
+    MethodUtil.methodSignaturesMatch(it, method)
+}
 
 /**
  * traverse the class hierarchy starting from the given root class
