031668d09735b8c36dd2ea3913f2b095cbf3f658
FineFindus <63370021+FineFindus@users.noreply.github.com>
2022-11-26 15:19:22 +0100

    fix(myexpenses): remove fingerprint class name check

--- src/main/kotlin/app/revanced/patches/myexpenses/misc/pro/fingerprints/IsEnabledFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/myexpenses/misc/pro/fingerprints/IsEnabledFingerprint.kt
@@ -4,6 +4,5 @@ import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 
 object IsEnabledFingerprint : MethodFingerprint(
     "Z",
-    strings = listOf("feature", "feature.licenceStatus"),
-    customFingerprint = { methodDef -> methodDef.definingClass.endsWith("Liv/l;") }
+    strings = listOf("feature", "feature.licenceStatus")
 )
