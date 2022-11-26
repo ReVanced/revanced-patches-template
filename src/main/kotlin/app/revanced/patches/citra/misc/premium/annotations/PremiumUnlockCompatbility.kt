6a96df183cc416a1e88c74a1660a28259c49a0ce
José Silveira <josesilveiraa@proton.me>
2022-11-25 21:46:23 -0300

    feat(citra): premium-unlock patch

---
+++ src/main/kotlin/app/revanced/patches/citra/misc/premium/annotations/PremiumUnlockCompatbility.kt
@@ -0,0 +1,8 @@
+package app.revanced.patches.citra.misc.premium.annotations
+
+import app.revanced.patcher.annotation.Compatibility
+import app.revanced.patcher.annotation.Package
+
+@Compatibility([Package("org.citra.citra_emu")])
+internal annotation class PremiumUnlockCompatbility {
+}
