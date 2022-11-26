88bf35f0e1f70628bbef967c9d63445ea763622a
josesilveiraa <josesilveiraa@proton.me>
2022-11-26 19:07:59 -0300

    fix(backdrops): dynamically get register index

--- src/main/kotlin/app/revanced/patches/backdrops/misc/pro/patch/ProUnlockPatch.kt
+++ src/main/kotlin/app/revanced/patches/backdrops/misc/pro/patch/ProUnlockPatch.kt
@@ -6,6 +6,7 @@ import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.MethodFingerprintExtensions.name
 import app.revanced.patcher.extensions.addInstructions
+import app.revanced.patcher.extensions.instruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultError
@@ -13,6 +14,7 @@ import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patches.backdrops.misc.pro.annotations.ProUnlockCompatibility
 import app.revanced.patches.backdrops.misc.pro.fingerprints.ProUnlockFingerprint
+import org.jf.dexlib2.iface.instruction.OneRegisterInstruction
 
 @Patch
 @Name("pro-unlock")
@@ -25,11 +27,13 @@ class ProUnlockPatch : BytecodePatch(
     override fun execute(context: BytecodeContext): PatchResult {
         val result = ProUnlockFingerprint.result ?: return PatchResultError("${ProUnlockFingerprint.name} not found")
 
+        val moveRegisterInstruction = result.mutableMethod.instruction(result.scanResult.patternScanResult!!.endIndex - 1)
+        val register = (moveRegisterInstruction as OneRegisterInstruction).registerA
+
         result.mutableMethod.addInstructions(
             result.scanResult.patternScanResult!!.endIndex,
             """
-                const/4 v4, 0x6
-                const/4 v2, 0x1
+                const/4 v$register, 0x1
             """
         )
 
