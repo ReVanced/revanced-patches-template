96f4ed6fe1354b51b8e9ce974ae436975f6a634e
Jonathan <63370021+FineFindus@users.noreply.github.com>
2022-11-26 15:43:04 +0100

    feat(myexpenses): `unlock-pro` patch (#1120)

---
+++ src/main/kotlin/app/revanced/patches/myexpenses/misc/pro/fingerprints/IsEnabledFingerprint.kt
@@ -0,0 +1,8 @@
+package app.revanced.patches.myexpenses.misc.pro.fingerprints
+
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+
+object IsEnabledFingerprint : MethodFingerprint(
+    "Z",
+    strings = listOf("feature", "feature.licenceStatus")
+)
