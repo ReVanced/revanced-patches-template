3b7e56b6b6b9296e52e367a6a3899ee8e1475222
FineFindus <63370021+FineFindus@users.noreply.github.com>
2022-11-28 10:15:02 +0100

    fix(youtube/custom-video-speed): add fuzzy scan

--- src/main/kotlin/app/revanced/patches/youtube/misc/video/speed/custom/fingerprints/SpeedArrayGeneratorFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/video/speed/custom/fingerprints/SpeedArrayGeneratorFingerprint.kt
@@ -1,10 +1,12 @@
 package app.revanced.patches.youtube.misc.video.speed.custom.fingerprints
 
 import app.revanced.patcher.extensions.or
+import app.revanced.patcher.fingerprint.method.annotation.FuzzyPatternScanMethod
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import org.jf.dexlib2.AccessFlags
 import org.jf.dexlib2.Opcode
 
+@FuzzyPatternScanMethod(2)
 object SpeedArrayGeneratorFingerprint : MethodFingerprint(
     "[L",
     AccessFlags.PUBLIC or AccessFlags.STATIC,
