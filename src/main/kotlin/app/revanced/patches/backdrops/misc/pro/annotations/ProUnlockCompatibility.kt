76b5449c61c97e8ad9f87fb7173c9819815744ba
josesilveiraa <85128820+josesilveiraa@users.noreply.github.com>
2022-11-27 22:36:39 -0300

    feat(backdrops): `pro-unlock` patch (#1121)

---
+++ src/main/kotlin/app/revanced/patches/backdrops/misc/pro/annotations/ProUnlockCompatibility.kt
@@ -0,0 +1,7 @@
+package app.revanced.patches.backdrops.misc.pro.annotations
+
+import app.revanced.patcher.annotation.Compatibility
+import app.revanced.patcher.annotation.Package
+
+@Compatibility([Package("com.backdrops.wallpapers")])
+internal annotation class ProUnlockCompatibility
