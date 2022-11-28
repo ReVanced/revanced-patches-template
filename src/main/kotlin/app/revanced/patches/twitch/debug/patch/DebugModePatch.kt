0cfad69a43ae6310a68bbee0ded8c18a8f0b7362
Tim Schneeberger <tim.schneeberger@outlook.de>
2022-11-28 23:57:58 +0100

    feat(twitch/debug-mode): add settings support

--- src/main/kotlin/app/revanced/patches/twitch/debug/patch/DebugModePatch.kt
+++ src/main/kotlin/app/revanced/patches/twitch/debug/patch/DebugModePatch.kt
@@ -8,13 +8,19 @@ import app.revanced.patcher.extensions.addInstructions
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patches.shared.settings.preference.impl.StringResource
+import app.revanced.patches.shared.settings.preference.impl.SwitchPreference
 import app.revanced.patches.twitch.debug.annotations.DebugModeCompatibility
 import app.revanced.patches.twitch.debug.fingerprints.IsDebugConfigEnabledFingerprint
 import app.revanced.patches.twitch.debug.fingerprints.IsOmVerificationEnabledFingerprint
 import app.revanced.patches.twitch.debug.fingerprints.ShouldShowDebugOptionsFingerprint
+import app.revanced.patches.twitch.misc.integrations.patch.IntegrationsPatch
+import app.revanced.patches.twitch.misc.settings.bytecode.patch.SettingsPatch
 
 @Patch(false)
+@DependsOn([IntegrationsPatch::class, SettingsPatch::class])
 @Name("debug-mode")
 @Description("Enables Twitch's internal debugging mode.")
 @DebugModeCompatibility
@@ -37,13 +43,34 @@ class DebugModePatch : BytecodePatch(
                     addInstructions(
                         0,
                         """
-                             const/4 v0, 0x1
+                             invoke-static {}, Lapp/revanced/twitch/patches/DebugModePatch;->isDebugModeEnabled()Z
+                             move-result v0
                              return v0
                           """
                     )
                 }
             }
         }
+
+        SettingsPatch.PreferenceScreen.MISC.OTHER.addPreferences(
+            SwitchPreference(
+                "revanced_debug_mode",
+                StringResource(
+                    "revanced_debug_mode_enable",
+                    "Enable debug mode"
+                ),
+                false,
+                StringResource(
+                    "revanced_debug_mode_on",
+                    "Debug mode is enabled (not recommended)"
+                ),
+                StringResource(
+                    "revanced_debug_mode_off",
+                    "Debug mode is disabled"
+                ),
+            )
+        )
+
         return PatchResultSuccess()
     }
 }
