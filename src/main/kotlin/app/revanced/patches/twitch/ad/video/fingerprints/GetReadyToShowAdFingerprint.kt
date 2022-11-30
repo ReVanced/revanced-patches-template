75fba85ddf38c0d87a635748f22a92c51747d166
Tim Schneeberger <tim.schneeberger@outlook.de>
2022-11-30 15:10:07 +0100

    feat(twitch/block-embedded-ads): `block-embedded-ads` patch support (#1160)

--- src/main/kotlin/app/revanced/patches/twitch/ad/video/fingerprints/GetReadyToShowAdFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/twitch/ad/video/fingerprints/GetReadyToShowAdFingerprint.kt
@@ -1,10 +1,9 @@
 package app.revanced.patches.twitch.ad.video.fingerprints
 
-
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 
-object AdsManagerFingerprint : MethodFingerprint(
+object GetReadyToShowAdFingerprint : MethodFingerprint(
     customFingerprint = { method ->
-        method.definingClass.endsWith("AdsManagerImpl;") && method.name == "playAds"
+        method.definingClass.endsWith("/StreamDisplayAdsPresenter;") && method.name == "getReadyToShowAdOrAbort"
     }
 )
