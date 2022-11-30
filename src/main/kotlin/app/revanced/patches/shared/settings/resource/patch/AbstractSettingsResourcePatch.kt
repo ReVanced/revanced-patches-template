90cf7230d2d26c6b72dee3987579c51d559ec389
Ushie <github@ushie.dev>
2022-12-01 00:05:54 +0300

    Revert "feat(twitch/block-embedded-ads): `block-embedded-ads` patch support (#1160)"
    This reverts commit 641d33fc7e213031124491bb468fb382267769aa.

--- src/main/kotlin/app/revanced/patches/shared/settings/resource/patch/AbstractSettingsResourcePatch.kt
+++ src/main/kotlin/app/revanced/patches/shared/settings/resource/patch/AbstractSettingsResourcePatch.kt
@@ -92,7 +92,7 @@ abstract class AbstractSettingsResourcePatch(
          * @param arrayResource The array resource to add.
          */
         fun addArray(arrayResource: ArrayResource) =
-            arraysNode!!.addResource(arrayResource) { it.include() }
+            arraysNode!!.addResource(arrayResource)
 
         /**
          * Add a preference to the settings.
