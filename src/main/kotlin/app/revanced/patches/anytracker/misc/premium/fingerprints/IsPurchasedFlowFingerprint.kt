46ad786b1488814494080ca48cff27c361c47ca1
oSumAtrIX <johan.melkonyan1@web.de>
2022-12-02 02:19:04 +0100

    fix(anytracker): syntax error in fingerprint (#1165)

--- src/main/kotlin/app/revanced/patches/anytracker/misc/premium/fingerprints/IsPurchasedFlowFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/anytracker/misc/premium/fingerprints/IsPurchasedFlowFingerprint.kt
@@ -1,11 +1,8 @@
 package app.revanced.patches.anytracker.misc.premium.fingerprints
 
-import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-import org.jf.dexlib2.AccessFlags
-import org.jf.dexlib2.Opcode
 
 object IsPurchasedFlowFingerprint : MethodFingerprint(
-    "Landroidx/lifecycle/LiveData"
+    "Landroidx/lifecycle/LiveData",
     strings = listOf("premium_user", "sku"),
 )
