18092163961c326e339d2406a5c2bf15df0815b7
Bennett <110846042+bennett-sh@users.noreply.github.com>
2022-12-10 18:18:55 +0100

    Add predictive back gesture patch for Twitter

---
+++ src/main/kotlin/app/revanced/patches/twitter/misc/predictiveback/patch/PredictiveBackPatch.kt
@@ -0,0 +1,31 @@
+package app.revanced.patches.twitter.misc.predictiveback.patch
+
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.ResourcePatch
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patches.twitter.misc.predictiveback.annotations.PredictiveBackCompatibility
+
+@Patch
+@Name("predictive-back")
+@Description("Enables the new predictive back feature")
+@PredictiveBackCompatibility
+@Version("0.0.1")
+class PredictiveBackPatch : ResourcePatch {
+    override fun execute(context: ResourceContext): PatchResult {
+        context.xmlEditor["AndroidManifest.xml"].use { editor ->
+            val document = editor.file
+            val application = document.getElementsByTagName("application").item(0)
+
+            val attr = document.createAttribute("android:enableOnBackInvokedCallback")
+            attr.value = "true"
+            application.attributes.setNamedItem(attr)
+        }
+
+        return PatchResultSuccess()
+    }
+}
