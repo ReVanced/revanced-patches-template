fbdfa3b5987501baf940389572242ab423b0608a
Jiří Richter <richtej2@gmail.com>
2022-12-12 14:27:10 +0100

    feat(sleepasandroid): remove `unlock-premium` patch (#1253)

--- src/main/kotlin/app/revanced/patches/sleepasandroid/annotations/UnlockPremiumCompatibility.kt
+++
@@ -1,9 +0,0 @@
-package app.revanced.patches.sleepasandroid.annotations
-
-import app.revanced.patcher.annotation.Compatibility
-import app.revanced.patcher.annotation.Package
-
-@Compatibility([Package("com.urbandroid.sleep")])
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-internal annotation class UnlockPremiumCompatibility
