fa6204a2bd3a0f467f5c13630519e6750d2b0898
oSumAtrIX <johan.melkonyan1@web.de>
2022-12-18 22:19:13 +0100

    chore(youtube): remove compatibility for v17.45.36 in favour of v17.49.37

--- src/main/kotlin/app/revanced/patches/youtube/misc/video/information/annotation/VideoInformationCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/video/information/annotation/VideoInformationCompatibility.kt
@@ -5,7 +5,7 @@ import app.revanced.patcher.annotation.Package
 
 @Compatibility(
     [Package(
-        "com.google.android.youtube", arrayOf("17.45.36")
+        "com.google.android.youtube", arrayOf("17.49.37")
     )]
 )
 @Target(AnnotationTarget.CLASS)
