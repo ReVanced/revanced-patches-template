e3b963d3c9ebac9bfc83c0af895780e6b6753b8d
KAZI MMT <82371061+kazimmt@users.noreply.github.com>
2022-12-19 14:27:45 +0600

    fix(remini): mistyped class names

--- src/main/kotlin/app/revanced/patches/remini/misc/premium/annotations/UnlockPremiumCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/remini/misc/premium/annotations/UnlockPremiumCompatibility.kt
@@ -10,4 +10,4 @@ import app.revanced.patcher.annotation.Package
 )
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-internal annotation class TimelineAdsCompatibility
+internal annotation class UnlockPremiumCompatibility
