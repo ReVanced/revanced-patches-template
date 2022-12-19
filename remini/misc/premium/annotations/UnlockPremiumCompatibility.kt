dcf0f942aa2266885814843ae02f344c75b9adfe
KAZI MMT <82371061+kazimmt@users.noreply.github.com>
2022-12-19 14:20:48 +0600

    remove: wrong directory

--- remini/misc/premium/annotations/UnlockPremiumCompatibility.kt
+++
@@ -1,7 +0,0 @@
-package app.revanced.patches.remini.misc.premium.annotations
-
-import app.revanced.patcher.annotation.Compatibility
-import app.revanced.patcher.annotation.Package
-
-@Compatibility([Package("com.bigwinepot.nwdn.international", ["3.7.46.202160002"])])
-internal annotation class UnlockPremiumCompatibility
