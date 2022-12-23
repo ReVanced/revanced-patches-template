e43d692c7299b1b3faa0f54577f7495bd4413aca
OxrxL <120989892+0xrxL@users.noreply.github.com>
2022-12-23 22:34:25 +0100

    refactor(youtube/vertical-scroll-patch): patch canScrollVertically() only

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/verticalscroll/fingerprints/CanScrollVerticallyFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/verticalscroll/fingerprints/CanScrollVerticallyFingerprint.kt
@@ -7,6 +7,17 @@ import org.jf.dexlib2.Opcode
 object CanScrollVerticallyFingerprint : MethodFingerprint(
     "Z",
     parameters = emptyList(),
-    opcodes = listOf(Opcode.INSTANCE_OF),
+    opcodes = listOf(
+        Opcode.IGET_OBJECT,
+        Opcode.INSTANCE_OF,
+        Opcode.CONST_4,
+        Opcode.IF_EQZ,
+        Opcode.CHECK_CAST,
+        Opcode.INVOKE_STATIC,
+        Opcode.MOVE_RESULT,
+        Opcode.RETURN,
+        Opcode.INVOKE_VIRTUAL,
+        Opcode.MOVE_RESULT,
+    ),
     customFingerprint = { methodDef -> methodDef.definingClass.endsWith("SwipeRefreshLayout;") }
 )
