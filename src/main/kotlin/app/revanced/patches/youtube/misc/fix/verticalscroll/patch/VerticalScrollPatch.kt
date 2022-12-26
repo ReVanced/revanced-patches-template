003a400ce41ff543fb5484c576f5ec2df0a87273
0xrxL <120989892+0xrxL@users.noreply.github.com>
2022-12-26 18:33:18 +0100

    fix(youtube/general-ads): don't early return when not necessary (#1353)

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/verticalscroll/patch/VerticalScrollPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/verticalscroll/patch/VerticalScrollPatch.kt
@@ -4,12 +4,14 @@ import app.revanced.extensions.toErrorResult
 import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.data.BytecodeContext
-import app.revanced.patcher.extensions.addInstructions
+import app.revanced.patcher.extensions.addInstruction
+import app.revanced.patcher.extensions.instruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patches.youtube.misc.fix.verticalscroll.annotations.VerticalScrollCompatibility
 import app.revanced.patches.youtube.misc.fix.verticalscroll.fingerprints.CanScrollVerticallyFingerprint
+import org.jf.dexlib2.iface.instruction.OneRegisterInstruction
 
 @Description("Fixes issues with scrolling on the home screen when the first component is of type EmptyComponent.")
 @VerticalScrollCompatibility
@@ -20,13 +22,17 @@ class VerticalScrollPatch : BytecodePatch(
     override fun execute(context: BytecodeContext): PatchResult {
         val result = CanScrollVerticallyFingerprint.result ?: return CanScrollVerticallyFingerprint.toErrorResult()
 
-        result.mutableMethod.addInstructions(
-            0,
-            """
-                const/4 v0, 0x0
-                return v0
-            """
-        )
+        with(result) {
+            val method = mutableMethod
+
+            val moveResultIndex = scanResult.patternScanResult!!.endIndex
+            val moveResultRegister = (method.instruction(moveResultIndex) as OneRegisterInstruction).registerA
+
+            method.addInstruction(
+                moveResultIndex + 1,
+                "const/4 v$moveResultRegister, 0x0"
+            )
+        }
 
         return PatchResultSuccess()
     }
