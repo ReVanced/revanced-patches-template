177e908dba260f184a2835b73b834563ca9c29fd
oSumAtrIX <johan.melkonyan1@web.de>
2023-01-07 09:42:40 +0100

    feat(youtube): `remember-playback-rate` patch

---
+++ src/main/kotlin/app/revanced/patches/youtube/misc/video/speed/remember/fingerprint/InitializePlaybackRateValuesFingerprint.kt
@@ -0,0 +1,7 @@
+package app.revanced.patches.youtube.misc.video.speed.remember.fingerprint
+
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+
+object InitializePlaybackRateValuesFingerprint : MethodFingerprint(
+    parameters = listOf("[L", "I")
+)
---
+++ src/main/kotlin/app/revanced/patches/youtube/misc/video/speed/remember/fingerprint/OnPlaybackRateItemClickFingerprint.kt
@@ -0,0 +1,12 @@
+package app.revanced.patches.youtube.misc.video.speed.remember.fingerprint
+
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import org.jf.dexlib2.Opcode
+
+object OnPlaybackRateItemClickFingerprint : MethodFingerprint(
+    customFingerprint = { it.name == "onItemClick" },
+    opcodes = listOf(
+        Opcode.IGET_OBJECT,
+        Opcode.INVOKE_VIRTUAL
+    )
+)
