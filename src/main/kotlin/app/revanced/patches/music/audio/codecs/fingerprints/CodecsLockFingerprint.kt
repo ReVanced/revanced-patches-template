1a530910c97500302f682f8181e537dcfa242096
FineFindus <63370021+FineFindus@users.noreply.github.com>
2023-01-08 15:51:10 +0100

    fix(ytm/codecs-unlock): change INVOKE_DIRECT to INVOKE_VIRTUAL

--- src/main/kotlin/app/revanced/patches/music/audio/codecs/fingerprints/CodecsLockFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/music/audio/codecs/fingerprints/CodecsLockFingerprint.kt
@@ -7,7 +7,7 @@ import org.jf.dexlib2.AccessFlags
 import org.jf.dexlib2.Opcode
 
 
-@FuzzyPatternScanMethod(3) // FIXME: Test this threshold and find the best value.
+@FuzzyPatternScanMethod(2) // FIXME: Test this threshold and find the best value.
 object CodecsLockFingerprint : MethodFingerprint(
     "L", AccessFlags.PUBLIC or AccessFlags.STATIC, opcodes = listOf(
         Opcode.INVOKE_DIRECT,
@@ -22,7 +22,7 @@ object CodecsLockFingerprint : MethodFingerprint(
         Opcode.INVOKE_STATIC,
         Opcode.MOVE_RESULT_OBJECT,
         Opcode.INVOKE_INTERFACE,
-        Opcode.INVOKE_DIRECT,
+        Opcode.INVOKE_VIRTUAL,
         Opcode.RETURN_OBJECT
     ),
     strings = listOf("eac3_supported")
