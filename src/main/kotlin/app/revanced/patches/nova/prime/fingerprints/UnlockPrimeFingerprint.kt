4402079c26050555aec15c483d4d3274a3f38361
illerokcob <bockorelli@protonmail.com>
2023-01-11 00:28:18 +0100

    Rewrite patch to find index dynamically

--- src/main/kotlin/app/revanced/patches/nova/prime/fingerprints/UnlockPrimeFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/nova/prime/fingerprints/UnlockPrimeFingerprint.kt
@@ -1,9 +1,10 @@
 package app.revanced.patches.nova.prime.fingerprints
 
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import org.jf.dexlib2.Opcode
 
 object UnlockPrimeFingerprint : MethodFingerprint(
     "V",
-    parameters = listOf("L"),
-    strings = listOf("ro.razer.internal.api")
+    opcodes = listOf(Opcode.IPUT_OBJECT, Opcode.CONST_STRING, Opcode.CONST_4, Opcode.INVOKE_INTERFACE, Opcode.MOVE_RESULT),
+    strings = listOf("1")
 )
--- src/main/kotlin/app/revanced/patches/nova/prime/patch/UnlockPrimePatch.kt
+++ src/main/kotlin/app/revanced/patches/nova/prime/patch/UnlockPrimePatch.kt
@@ -4,17 +4,19 @@ import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.data.BytecodeContext
-import app.revanced.patcher.extensions.addInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patches.nova.prime.annotations.UnlockPrimeCompatibility
 import app.revanced.patches.nova.prime.fingerprints.UnlockPrimeFingerprint
+import org.jf.dexlib2.Opcode
+import org.jf.dexlib2.builder.instruction.BuilderInstruction11x
+import org.jf.dexlib2.builder.instruction.BuilderInstruction21s
 
 @Patch
 @Name("unlock-prime")
-@Description("Unlock Nova Prime.")
+@Description("Unlocks Nova Prime and all functions of the app.")
 @UnlockPrimeCompatibility
 @Version("0.0.1")
 class UnlockPrimePatch : BytecodePatch(
@@ -22,14 +24,21 @@ class UnlockPrimePatch : BytecodePatch(
         UnlockPrimeFingerprint
     )
 ) {
-    override fun execute(context: BytecodeContext): PatchResult {
-        val method = UnlockPrimeFingerprint.result!!.mutableMethod
 
-        method.addInstruction(
-            8,
-            """
-                const/16 p1, 0x512
-            """
+    //Insert the hex value 0x512 right after the pattern which checks the version of the app (paid or free).
+    //Free version is '0', paid version is '0x512'.
+
+    override fun execute(context: BytecodeContext): PatchResult {
+        val result = UnlockPrimeFingerprint.result!!
+        val methodImplementation = result.mutableMethod.implementation
+        val startIndex = result.scanResult.patternScanResult?.startIndex!!
+
+        val replaceIndex = startIndex + 5
+
+        methodImplementation!!.addInstruction(
+            replaceIndex,
+            BuilderInstruction21s(
+                Opcode.CONST_16, (methodImplementation.instructions[replaceIndex - 1] as BuilderInstruction11x).registerA, 1298)    // 0x512
         )
 
         return PatchResultSuccess()
