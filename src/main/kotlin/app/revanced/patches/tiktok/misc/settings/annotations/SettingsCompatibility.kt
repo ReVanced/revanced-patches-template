e72b3bd4e22f5ade7193ba6a3cdf61fcd91fba55
d4rkk3y <43563783+d4rkk3y@users.noreply.github.com>
2023-01-13 22:00:45 +0700

    feat(tiktok): update patches compatibility to v27.8.3 (#1483)

--- src/main/kotlin/app/revanced/patches/tiktok/misc/settings/annotations/SettingsCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/tiktok/misc/settings/annotations/SettingsCompatibility.kt
@@ -5,8 +5,8 @@ import app.revanced.patcher.annotation.Package
 
 @Compatibility(
     [
-        Package("com.ss.android.ugc.trill"),
-        Package("com.zhiliaoapp.musically")
+        Package("com.ss.android.ugc.trill", arrayOf("27.8.3")),
+        Package("com.zhiliaoapp.musically", arrayOf("27.8.3"))
     ]
 )
 @Target(AnnotationTarget.CLASS)
