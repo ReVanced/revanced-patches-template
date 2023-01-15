b0a926f57902bf9f054d54b9eb1edf14444a5296
LisousEinaiKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-01-15 17:00:50 +0400

    until the patch is updated, specify the last version the patch actually works with

--- src/main/kotlin/app/revanced/patches/reddit/ad/general/annotations/GeneralAdsCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/reddit/ad/general/annotations/GeneralAdsCompatibility.kt
@@ -3,7 +3,11 @@ package app.revanced.patches.reddit.ad.general.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility([Package("com.reddit.frontpage")])
+@Compatibility(
+    [Package(
+        "com.reddit.frontpage", arrayOf("2022.43.0")
+    )]
+)
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 internal annotation class GeneralAdsCompatibility
