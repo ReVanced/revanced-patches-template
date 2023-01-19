bf45817677fd058f9b255dbef5c1ca9aaec95531
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-01-19 23:53:10 +0400

    fix(twitter/hide-views-stats): constrain to last working version (#1522)

--- src/main/kotlin/app/revanced/patches/twitter/layout/hideviews/annotations/HideViewsCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/twitter/layout/hideviews/annotations/HideViewsCompatibility.kt
@@ -3,7 +3,11 @@ package app.revanced.patches.twitter.layout.hideviews.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility([Package("com.twitter.android")])
+@Compatibility(
+    [Package(
+        "com.twitter.android", arrayOf("9.69.1-release.0")
+    )]
+)
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 internal annotation class HideViewsCompatibility
