2ba1ddcf4bd3fcd09039a104bc7558a8e2aab5cb
1fexd <PGFvy85nC@protonmail.com>
2023-01-19 19:42:03 +0100

    fix(finanzonline): Add DetectionCompatibility

--- src/main/kotlin/app/revanced/patches/finanzonline/detection/bootloader/patch/BootloaderDetectionPatch.kt
+++ src/main/kotlin/app/revanced/patches/finanzonline/detection/bootloader/patch/BootloaderDetectionPatch.kt
@@ -1,9 +1,7 @@
 package app.revanced.patches.finanzonline.detection.bootloader.patch
 
-import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
-import app.revanced.patcher.annotation.Package
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.addInstructions
@@ -13,12 +11,13 @@ import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patches.finanzonline.detection.bootloader.fingerprints.BootStateFingerprint
 import app.revanced.patches.finanzonline.detection.bootloader.fingerprints.BootloaderDetectionFingerprint
+import app.revanced.patches.finanzonline.detection.shared.annotations.DetectionCompatibility
 
 
 @Patch
 @Name("remove-bootloader-detection")
 @Description("Removes the check for an unlocked bootloader")
-@Compatibility([Package("at.gv.bmf.bmf2go", arrayOf("2.2.0"))])
+@DetectionCompatibility
 @Version("0.0.1")
 class BootloaderDetectionPatch : BytecodePatch(
     listOf(BootloaderDetectionFingerprint, BootStateFingerprint)
--- src/main/kotlin/app/revanced/patches/finanzonline/detection/root/patch/RootDetectionPatch.kt
+++ src/main/kotlin/app/revanced/patches/finanzonline/detection/root/patch/RootDetectionPatch.kt
@@ -1,9 +1,7 @@
 package app.revanced.patches.finanzonline.detection.root.patch
 
-import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
-import app.revanced.patcher.annotation.Package
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.addInstructions
@@ -12,11 +10,12 @@ import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patches.finanzonline.detection.root.fingerprints.RootDetectionFingerprint
+import app.revanced.patches.finanzonline.detection.shared.annotations.DetectionCompatibility
 
 @Patch
 @Name("remove-root-detection")
 @Description("Removes the check for root permissions")
-@Compatibility([Package("at.gv.bmf.bmf2go", arrayOf("2.2.0"))])
+@DetectionCompatibility
 @Version("0.0.1")
 class RootDetectionPatch : BytecodePatch(
     listOf(RootDetectionFingerprint)
