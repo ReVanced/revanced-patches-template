66cd88f4d8a9161a4c51b70f2384dcee92fe2aea
oSumAtrIX <johan.melkonyan1@web.de>
2023-01-22 15:38:12 +0100

    fix: parse any kind of patch version
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>

---
+++ src/main/kotlin/app/revanced/meta/JsonGenerator.kt
@@ -0,0 +1,70 @@
+package app.revanced.meta
+
+import app.revanced.patcher.extensions.PatchExtensions.compatiblePackages
+import app.revanced.patcher.extensions.PatchExtensions.dependencies
+import app.revanced.patcher.extensions.PatchExtensions.description
+import app.revanced.patcher.extensions.PatchExtensions.include
+import app.revanced.patcher.extensions.PatchExtensions.options
+import app.revanced.patcher.extensions.PatchExtensions.patchName
+import app.revanced.patcher.extensions.PatchExtensions.version
+import app.revanced.patcher.patch.PatchOption
+import com.google.gson.GsonBuilder
+import java.io.File
+
+internal class JsonGenerator : PatchesFileGenerator {
+    override fun generate(bundle: PatchBundlePatches) {
+        val patches = bundle.map {
+            JsonPatch(
+                it.patchName,
+                it.description ?: "This patch has no description.",
+                it.version ?: "0.0.0",
+                !it.include,
+                it.options?.map { option ->
+                    Option(
+                        option.key,
+                        option.title,
+                        option.description,
+                        option.required,
+                        option.let { lo ->
+                            if (lo is PatchOption.ListOption<*>) {
+                                lo.options.toMutableList().toTypedArray()
+                            } else null
+                        }
+                    )
+                }?.toTypedArray() ?: emptyArray(),
+                it.dependencies?.map { dep ->
+                    dep.java.patchName
+                }?.toTypedArray() ?: emptyArray(),
+                it.compatiblePackages?.map { pkg ->
+                    CompatiblePackage(pkg.name, pkg.versions)
+                }?.toTypedArray() ?: emptyArray()
+            )
+        }
+
+        val json = File("patches.json")
+        json.writeText(GsonBuilder().serializeNulls().create().toJson(patches))
+    }
+
+    data class JsonPatch(
+        val name: String,
+        val description: String,
+        val version: String,
+        val excluded: Boolean,
+        val options: Array<Option>,
+        val dependencies: Array<String>,
+        val compatiblePackages: Array<CompatiblePackage>,
+    )
+
+    data class CompatiblePackage(
+        val name: String,
+        val versions: Array<String>,
+    )
+
+    data class Option(
+        val key: String,
+        val title: String,
+        val description: String,
+        val required: Boolean,
+        val choices: Array<*>?,
+    )
+}
---
+++ src/main/kotlin/app/revanced/meta/PatchesFileGenerator.kt
@@ -0,0 +1,25 @@
+package app.revanced.meta
+
+import app.revanced.patcher.data.Context
+import app.revanced.patcher.patch.Patch
+import app.revanced.patcher.util.patch.PatchBundle
+import java.io.File
+
+typealias PatchBundlePatches = List<Class<out Patch<Context>>>
+
+internal interface PatchesFileGenerator {
+    fun generate(bundle: PatchBundlePatches)
+
+    private companion object {
+        @JvmStatic
+        fun main(args: Array<String>) = PatchBundle.Jar(
+            File("build/libs/").listFiles()!!.first {
+                it.name.startsWith("revanced-patches-") && it.name.endsWith(".jar")
+            }.absolutePath
+        ).loadPatches().also {
+            if (it.isEmpty()) throw IllegalStateException("No patches found")
+        }.let { bundle ->
+            arrayOf(JsonGenerator(), ReadmeGenerator()).forEach { it.generate(bundle) }
+        }
+    }
+}
