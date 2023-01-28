661f143f0c1fed5ddff2a3f2ef5fbf724761ebc6
oSumAtrIX <johan.melkonyan1@web.de>
2023-01-28 08:00:30 +0100

    chore(youtube): bump patches compatibility to v18.03.36
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patches/youtube/interaction/copyvideourl/annotation/CopyVideoUrlCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/youtube/interaction/copyvideourl/annotation/CopyVideoUrlCompatibility.kt
@@ -5,7 +5,7 @@ import app.revanced.patcher.annotation.Package
 
 @Compatibility(
     [
-        Package("com.google.android.youtube", arrayOf("17.49.37"))
+        Package("com.google.android.youtube", arrayOf("17.49.37", "18.03.36"))
     ]
 )
 @Target(AnnotationTarget.CLASS)
