6793e831567de75ca9af064119bfd265f8c50577
oSumAtrIX <johan.melkonyan1@web.de>
2023-02-10 06:00:40 +0100

    Apply suggestions from code review [skip ci]

--- src/main/kotlin/app/revanced/patches/youtube/layout/hidesubtitlespopup/annotation/HideSubtitlesPopupPatchCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hidesubtitlespopup/annotation/HideSubtitlesPopupPatchCompatibility.kt
@@ -3,7 +3,7 @@ package app.revanced.patches.youtube.layout.hidesubtitlespopup.annotation
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility([Package("com.google.android.youtube", arrayOf("17.49.37"))])
+@Compatibility([Package("com.google.android.youtube", arrayOf("18.03.36"))])
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 internal annotation class HideSubtitlesPopupPatchCompatibility
