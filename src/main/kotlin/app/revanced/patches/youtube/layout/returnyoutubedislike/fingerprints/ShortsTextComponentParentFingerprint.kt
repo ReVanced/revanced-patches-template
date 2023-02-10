9f1e900a6c27718697e4e093123213a08be30d9f
oSumAtrIX <johan.melkonyan1@web.de>
2023-02-11 00:29:17 +0100

    Apply suggestions from code review

--- src/main/kotlin/app/revanced/patches/youtube/layout/returnyoutubedislike/fingerprints/ShortsTextComponentParentFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/returnyoutubedislike/fingerprints/ShortsTextComponentParentFingerprint.kt
@@ -10,21 +10,16 @@ object ShortsTextComponentParentFingerprint : MethodFingerprint(
     access = AccessFlags.PROTECTED or AccessFlags.FINAL,
     parameters = listOf("L", "L"),
     opcodes = listOf(
-        // instructions not interested in.  Only here to better ensure a correct match
         Opcode.IF_EQZ,
         Opcode.CONST_4,
         Opcode.IF_EQ,
         Opcode.CONST_4,
         Opcode.IF_EQ,
         Opcode.RETURN_VOID,
-
-        // actual instructions interested in
         Opcode.IGET_OBJECT,
         Opcode.CHECK_CAST,
         Opcode.IGET_BOOLEAN,
         Opcode.IF_EQZ,
         Opcode.INVOKE_STATIC
     )
-
-    // class also contains unique string "Failed to set parent screen", but string is inside a different method
 )
