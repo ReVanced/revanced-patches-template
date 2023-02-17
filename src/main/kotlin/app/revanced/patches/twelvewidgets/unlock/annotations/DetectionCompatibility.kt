744b1f38bf65d0ae98ab23f966624b1ecf65fc21
1fexd <PGFvy85nC@protonmail.com>
2023-02-17 22:48:56 +0100

    chore(twelvewidgets): Update version

--- src/main/kotlin/app/revanced/patches/twelvewidgets/unlock/annotations/DetectionCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/twelvewidgets/unlock/annotations/DetectionCompatibility.kt
@@ -3,7 +3,7 @@ package app.revanced.patches.twelvewidgets.unlock.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility([Package("com.dci.dev.androidtwelvewidgets", arrayOf("1.4.1"))])
+@Compatibility([Package("com.dci.dev.androidtwelvewidgets", arrayOf("1.4.5"))])
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 internal annotation class DetectionCompatibility
