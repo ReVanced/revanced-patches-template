28642d1f93832c733a34ad77890f70f5a6fe5d51
fe <58902674+1fexd@users.noreply.github.com>
2023-02-18 00:32:49 +0000

    feat(androidtwelvewidgets): `unlock-paid-widgets` patch (#1641)

---
+++ src/main/kotlin/app/revanced/patches/twelvewidgets/unlock/fingerprints/AgendaDaysWidgetUnlockFingerprint.kt
@@ -0,0 +1,5 @@
+package app.revanced.patches.twelvewidgets.unlock.fingerprints
+
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+
+object AgendaDaysWidgetUnlockFingerprint : MethodUnlockFingerprint("AgendaDaysWidgetConfigureActivity")
---
+++ src/main/kotlin/app/revanced/patches/twelvewidgets/unlock/fingerprints/CalendarWideTimelineWidgetUnlockFingerprint.kt
@@ -0,0 +1,6 @@
+package app.revanced.patches.twelvewidgets.unlock.fingerprints
+
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+
+object CalendarWideTimelineWidgetUnlockFingerprint :
+    MethodUnlockFingerprint("CalendarWideTimelineWidgetConfigureActivity")
