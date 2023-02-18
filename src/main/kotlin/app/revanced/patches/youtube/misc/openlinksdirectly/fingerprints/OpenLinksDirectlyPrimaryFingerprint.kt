6a2fd88d4000b4c0a7c6e15331c5a6d43550c319
oSumAtrIX <johan.melkonyan1@web.de>
2023-02-18 20:50:38 +0100

    refactor(youtube/open-links-directly): remove unused annotation from fingerprints
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patches/youtube/misc/openlinksdirectly/fingerprints/OpenLinksDirectlyPrimaryFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/openlinksdirectly/fingerprints/OpenLinksDirectlyPrimaryFingerprint.kt
@@ -1,17 +1,13 @@
 package app.revanced.patches.youtube.misc.openlinksdirectly.fingerprints
 
-import app.revanced.patcher.annotation.Name
-import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import app.revanced.patches.youtube.misc.openlinksdirectly.annotations.OpenLinksDirectlyCompatibility
 import org.jf.dexlib2.AccessFlags
 import org.jf.dexlib2.Opcode
 
-@Name("open-links-directly-primary-fingerprint")
 @OpenLinksDirectlyCompatibility
-@Version("0.0.1")
-object OpenLinksDirectlyFingerprintPrimary : MethodFingerprint(
+object OpenLinksDirectlyPrimaryFingerprint : MethodFingerprint(
     "L", AccessFlags.PUBLIC or AccessFlags.FINAL, listOf("L"), listOf(
         Opcode.CHECK_CAST,
         Opcode.INVOKE_STATIC,
