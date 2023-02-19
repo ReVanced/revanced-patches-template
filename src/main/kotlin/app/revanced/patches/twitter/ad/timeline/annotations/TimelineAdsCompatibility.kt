d2faf441555c8d275fae936cb50a88bf0b11d482
LisousEinaiKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-02-20 00:25:15 +0400

    specify most recent version that works 100% correct

--- src/main/kotlin/app/revanced/patches/twitter/ad/timeline/annotations/TimelineAdsCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/twitter/ad/timeline/annotations/TimelineAdsCompatibility.kt
@@ -3,7 +3,11 @@ package app.revanced.patches.twitter.ad.timeline.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility([Package("com.twitter.android")])
+@Compatibility(
+    [Package(
+        "com.twitter.android", arrayOf("9.65.3-release.0")
+    )]
+)
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 internal annotation class TimelineAdsCompatibility
