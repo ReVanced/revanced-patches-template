71506625aca42aa4901878c0f706793e17ca5607
oSumAtrIX <johan.melkonyan1@web.de>
2023-02-25 00:00:58 +0100

    feat(youtube/disable-player-popup-panels): use better patch description
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patches/youtube/layout/panels/popup/fingerprints/EngagementPanelControllerFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/panels/popup/fingerprints/EngagementPanelControllerFingerprint.kt
@@ -1,4 +1,4 @@
-package app.revanced.patches.youtube.layout.panels.fullscreen.popup.fingerprints
+package app.revanced.patches.youtube.layout.panels.popup.fingerprints
 
 import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.method.annotation.FuzzyPatternScanMethod
