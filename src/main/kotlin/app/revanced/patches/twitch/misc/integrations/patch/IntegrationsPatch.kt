e56cd5dcc6ce0b8152dbfdc220330be4daecf3d3
oSumAtrIX <johan.melkonyan1@web.de>
2023-02-27 00:48:28 +0100

    build: update dependencies
    Signed-off-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patches/twitch/misc/integrations/patch/IntegrationsPatch.kt
+++ src/main/kotlin/app/revanced/patches/twitch/misc/integrations/patch/IntegrationsPatch.kt
@@ -1,12 +1,14 @@
 package app.revanced.patches.twitch.misc.integrations.patch
 
 import app.revanced.patcher.annotation.Name
-import app.revanced.patches.twitch.misc.integrations.fingerprints.InitFingerprint
-import app.revanced.patches.twitch.misc.integrations.annotations.IntegrationsCompatibility
+import app.revanced.patcher.patch.annotations.RequiresIntegrations
 import app.revanced.patches.shared.integrations.patch.AbstractIntegrationsPatch
+import app.revanced.patches.twitch.misc.integrations.annotations.IntegrationsCompatibility
+import app.revanced.patches.twitch.misc.integrations.fingerprints.InitFingerprint
 
 @Name("integrations")
 @IntegrationsCompatibility
+@RequiresIntegrations
 class IntegrationsPatch : AbstractIntegrationsPatch(
     "Lapp/revanced/twitch/utils/ReVancedUtils;",
     listOf(InitFingerprint)
