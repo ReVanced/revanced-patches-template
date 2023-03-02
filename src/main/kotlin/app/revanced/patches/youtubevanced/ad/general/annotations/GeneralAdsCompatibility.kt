029b2af4e8ac4e4df78762db38426eba022c0814
Rizwan <connerjohn2214@gmail.com>
2023-03-02 20:05:56 +0500

    refactor

--- src/main/kotlin/app/revanced/patches/youtubevanced/ad/general/annotations/GeneralAdsCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/youtubevanced/ad/general/annotations/GeneralAdsCompatibility.kt
@@ -3,11 +3,6 @@ package app.revanced.patches.youtubevanced.ad.general.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility(
-    [
-        Package("com.vanced.android.youtube")
-    ]
-)
+@Compatibility([Package("com.vanced.android.youtube")])
 @Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
 internal annotation class GeneralAdsCompatibility
