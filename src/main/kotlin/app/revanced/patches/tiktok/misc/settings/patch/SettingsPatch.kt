3304f15fe4f4ba53fff6f45ca6c7b5b6b88d5a4e
KAZI MMT <82371061+kazimmt@users.noreply.github.com>
2023-03-06 18:33:08 +0600

    bump patches compatibly to `28.5.4`

--- src/main/kotlin/app/revanced/patches/tiktok/misc/settings/patch/SettingsPatch.kt
+++ src/main/kotlin/app/revanced/patches/tiktok/misc/settings/patch/SettingsPatch.kt
@@ -8,104 +8,110 @@ import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.addInstructions
 import app.revanced.patcher.extensions.instruction
 import app.revanced.patcher.extensions.replaceInstruction
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint.Companion.resolve
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultError
 import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
 import app.revanced.patches.tiktok.misc.integrations.patch.IntegrationsPatch
 import app.revanced.patches.tiktok.misc.settings.annotations.SettingsCompatibility
-import app.revanced.patches.tiktok.misc.settings.fingerprints.AboutViewFingerprint
+import app.revanced.patches.tiktok.misc.settings.fingerprints.AboutPageFingerprint
 import app.revanced.patches.tiktok.misc.settings.fingerprints.AdPersonalizationActivityOnCreateFingerprint
 import app.revanced.patches.tiktok.misc.settings.fingerprints.SettingsOnViewCreatedFingerprint
 import org.jf.dexlib2.Opcode
+import org.jf.dexlib2.builder.instruction.BuilderInstruction21c
 import org.jf.dexlib2.iface.instruction.OneRegisterInstruction
 import org.jf.dexlib2.iface.instruction.ReferenceInstruction
+import org.jf.dexlib2.iface.instruction.WideLiteralInstruction
 import org.jf.dexlib2.iface.instruction.formats.Instruction35c
-import org.jf.dexlib2.iface.reference.StringReference
 import org.jf.dexlib2.iface.reference.TypeReference
 
 @Patch
 @DependsOn([IntegrationsPatch::class])
-@Name("settings")
+@Name("tiktok-settings")
 @Description("Adds ReVanced settings to TikTok.")
 @SettingsCompatibility
 @Version("0.0.1")
 class SettingsPatch : BytecodePatch(
     listOf(
+        AboutPageFingerprint,
         AdPersonalizationActivityOnCreateFingerprint,
         SettingsOnViewCreatedFingerprint,
     )
 ) {
     override fun execute(context: BytecodeContext): PatchResult {
-        SettingsOnViewCreatedFingerprint.result?.let {
-            AboutViewFingerprint.resolve(context, it.method, it.classDef)
-        }
-        // Patch Settings UI to add 'Revanced Settings'.
-        val targetIndexes = findOptionsOnClickIndex()
-        with(SettingsOnViewCreatedFingerprint.result!!.mutableMethod) {
-            for (index in targetIndexes) {
-                if (
-                    instruction(index).opcode != Opcode.NEW_INSTANCE ||
-                    instruction(index - 4).opcode != Opcode.MOVE_RESULT_OBJECT
-                )
-                    return PatchResultError("Hardcode offset changed.")
-                patchOptionNameAndOnClickEvent(index, context)
+
+        AboutPageFingerprint.result?.let {
+            val startIndex = it.scanResult.patternScanResult!!.startIndex
+
+            with(it.mutableMethod.implementation!!.instructions) {
+                copyrightPolicyLabelId = (this[startIndex] as WideLiteralInstruction).wideLiteral
             }
-        }
-        // Implement settings screen in `AdPersonalizationActivity`
-        with(AdPersonalizationActivityOnCreateFingerprint.result!!.mutableMethod) {
-            for ((index, instruction) in implementation!!.instructions.withIndex()) {
+
+        } ?: return AboutPageFingerprint.toErrorResult()
+
+        SettingsOnViewCreatedFingerprint.result?.mutableMethod?.let { method ->
+            val copyrightInstructions = method.implementation!!.instructions
+
+            val copyrightIndex = copyrightInstructions.indexOfFirst {
+                it.opcode == Opcode.CONST_STRING &&
+                        (it as BuilderInstruction21c).reference.toString() == "copyright_policy"
+            } - 6
+
+            val copyrightPolicyIndex = copyrightInstructions.indexOfFirst {
+                it.opcode == Opcode.CONST &&
+                        (it as WideLiteralInstruction).wideLiteral == copyrightPolicyLabelId
+            } + 2
+
+            arrayOf(
+                copyrightIndex,
+                copyrightPolicyIndex
+            ).forEach {
+                if (method.instruction(it).opcode != Opcode.MOVE_RESULT_OBJECT)
+                    return PatchResultError("Hardcode offset changed.")
+                val register = (method.instruction(it) as OneRegisterInstruction).registerA
+
+                method.insertSettings(context, it, register)
+            }
+
+        } ?: return SettingsOnViewCreatedFingerprint.toErrorResult()
+
+        AdPersonalizationActivityOnCreateFingerprint.result?.mutableMethod?.let {
+            for ((index, instruction) in it.implementation!!.instructions.withIndex()) {
                 if (instruction.opcode != Opcode.INVOKE_SUPER) continue
                 val thisRegister = (instruction as Instruction35c).registerC
-                addInstructions(
+                it.addInstructions(
                     index + 1,
                     """
                         invoke-static {v$thisRegister}, Lapp/revanced/tiktok/settingsmenu/SettingsMenu;->initializeSettings(Lcom/bytedance/ies/ugc/aweme/commercialize/compliance/personalization/AdPersonalizationActivity;)V
                         return-void
                     """
                 )
-                break
+                return PatchResultSuccess()
             }
-        }
-        return PatchResultSuccess()
+        } ?: return AdPersonalizationActivityOnCreateFingerprint.toErrorResult()
+
+        return PatchResultError("Could not find the method to hook.")
     }
 
-    private fun findOptionsOnClickIndex(): IntArray {
-        val results = IntArray(2)
-        SettingsOnViewCreatedFingerprint.result?.apply {
-            for ((index, instruction) in mutableMethod.implementation!!.instructions.withIndex()) {
-                // Old UI settings option to replace to 'Revanced Settings'
-                if (instruction.opcode == Opcode.CONST_STRING) {
-                    val string = ((instruction as ReferenceInstruction).reference as StringReference).string
-                    if (string == "copyright_policy") {
-                        results[0] = index - 2
-                        break
-                    }
-                }
-            }
+    private companion object {
+        var copyrightPolicyLabelId: Long = -1
 
-            // New UI settings option to replace to 'Revanced Settings'
-            results[1] = AboutViewFingerprint.result!!.scanResult.patternScanResult!!.startIndex
-        } ?: throw SettingsOnViewCreatedFingerprint.toErrorResult()
-        return results
-    }
-
-    private fun patchOptionNameAndOnClickEvent(index: Int, context: BytecodeContext) {
-        with(SettingsOnViewCreatedFingerprint.result!!.mutableMethod) {
-            // Patch option name
-            val overrideRegister = (instruction(index - 4) as OneRegisterInstruction).registerA
+        fun MutableMethod.insertSettings(
+            context: BytecodeContext,
+            index: Int,
+            overrideRegister: Int
+        ) {
             replaceInstruction(
-                index - 4,
+                index,
                 """
                     const-string v$overrideRegister, "Revanced Settings"
                 """
             )
 
-            // Patch option OnClick Event
-            with(((instruction(index) as ReferenceInstruction).reference as TypeReference).type) {
+            with(((instruction(index + 4) as ReferenceInstruction).reference as TypeReference).type) {
                 context.findClass(this)!!.mutableClass.methods.first { it.name == "onClick" }
                     .addInstructions(
                         0,
--- src/main/kotlin/app/revanced/patches/tiktok/misc/spoof/sim/annotations/SpoofSimCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/tiktok/misc/spoof/sim/annotations/SpoofSimCompatibility.kt
@@ -5,8 +5,8 @@ import app.revanced.patcher.annotation.Package
 
 @Compatibility(
     [
-        Package("com.ss.android.ugc.trill", arrayOf("27.8.3")),
-        Package("com.zhiliaoapp.musically", arrayOf("27.8.3"))
+        Package("com.ss.android.ugc.trill", arrayOf("27.8.3", "28.5.4")),
+        Package("com.zhiliaoapp.musically", arrayOf("27.8.3", "28.5.4"))
     ]
 )
 @Target(AnnotationTarget.CLASS)
