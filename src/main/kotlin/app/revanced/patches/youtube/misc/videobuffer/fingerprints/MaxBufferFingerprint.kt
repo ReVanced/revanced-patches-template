29f952d73e07f8f666d4744d6d97374bebfdc3c6
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-03-13 03:03:46 +0400

    feat(youtube/custom-video-buffer): replace patch with removal notice (#1718)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patches/youtube/misc/videobuffer/fingerprints/MaxBufferFingerprint.kt
+++
@@ -1,8 +0,0 @@
-package app.revanced.patches.youtube.misc.videobuffer.fingerprints
-
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-import org.jf.dexlib2.Opcode
-
-object MaxBufferFingerprint : MethodFingerprint(
-    opcodes = listOf(Opcode.SGET_OBJECT, Opcode.IGET, Opcode.IF_EQZ, Opcode.RETURN),
-)
