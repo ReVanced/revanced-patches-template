3e7d8dc84502796bdc67542a0ba100bf9eed5dd7
EvadeMaster <93124920+EvadeMaster@users.noreply.github.com>
2023-03-12 17:31:34 +0700

    feat(twitch): constraint compatibility to `14.6.1`

--- src/main/kotlin/app/revanced/patches/twitch/misc/settings/annotations/SettingsCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/twitch/misc/settings/annotations/SettingsCompatibility.kt
@@ -3,6 +3,19 @@ package app.revanced.patches.twitch.misc.settings.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility([Package("tv.twitch.android.app")])
+@Compatibility(
+    [
+        Package(
+            "tv.twitch.android.app", arrayOf(
+                "14.3.3",
+                "14.4.0",
+                "14.5.0", 
+                "14.5.2",
+                "14.6.0",
+                "14.6.1"
+            )
+        )
+    ]
+)
 @Target(AnnotationTarget.CLASS)
 internal annotation class SettingsCompatibility
