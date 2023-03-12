7d6fe421ff2f3e0a9a25c362e53d14033c03aa1e
EvadeMaster <93124920+EvadeMaster@users.noreply.github.com>
2023-03-12 19:43:08 +0700

    fix(twitch): misspelling annotations

--- src/main/kotlin/app/revanced/patches/twitch/misc/settings/annotations/SettingsCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/twitch/misc/settings/annotations/SettingsCompatibility.kt
@@ -3,6 +3,6 @@ package app.revanced.patches.twitch.misc.settings.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-mpatibility([Package("tv.twitch.android.app")])
+@Compatibility([Package("tv.twitch.android.app")])
 @Target(AnnotationTarget.CLASS)
 internal annotation class SettingsCompatibility
