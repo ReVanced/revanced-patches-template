1973350fe3fb94584a310f5e85110f68111608e5
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-03-15 23:43:36 +0400

    automatic spoofing, based on network connection status

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/patch/SpoofSignatureVerificationPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/patch/SpoofSignatureVerificationPatch.kt
@@ -7,21 +7,34 @@ import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.data.toMethodWalker
 import app.revanced.patcher.extensions.addInstructions
+import app.revanced.patcher.extensions.instruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
+import app.revanced.patches.shared.settings.preference.impl.StringResource
+import app.revanced.patches.shared.settings.preference.impl.SwitchPreference
 import app.revanced.patches.youtube.misc.fix.playback.annotation.ProtobufSpoofCompatibility
+import app.revanced.patches.youtube.misc.fix.playback.fingerprints.ConnectionResultFingerprint
 import app.revanced.patches.youtube.misc.fix.playback.fingerprints.ProtobufParameterBuilderFingerprint
+import app.revanced.patches.youtube.misc.integrations.patch.IntegrationsPatch
+import app.revanced.patches.youtube.misc.settings.bytecode.patch.SettingsPatch
+import app.revanced.patches.youtube.misc.video.information.patch.VideoInformationPatch
+import org.jf.dexlib2.iface.instruction.OneRegisterInstruction
 
 @Patch
 @Name("spoof-signature-verification")
 @Description("Spoofs the client to prevent playback issues.")
 @ProtobufSpoofCompatibility
+@DependsOn([IntegrationsPatch::class, SettingsPatch::class, VideoInformationPatch::class])
 @Version("0.0.1")
 class SpoofSignatureVerificationPatch : BytecodePatch(
-    listOf(ProtobufParameterBuilderFingerprint)
+    listOf(
+        ProtobufParameterBuilderFingerprint,
+        ConnectionResultFingerprint,
+    )
 ) {
     override fun execute(context: BytecodeContext): PatchResult {
         ProtobufParameterBuilderFingerprint.result?.let {
@@ -35,12 +48,36 @@ class SpoofSignatureVerificationPatch : BytecodePatch(
                 addInstructions(
                     0,
                     """
-                        invoke-static {p$protobufParameterRegister}, $INTEGRATIONS_CLASS_DESCRIPTOR->getVerificationSpoofOverride(Ljava/lang/String;)Ljava/lang/String;
+                        invoke-static {p$protobufParameterRegister}, $INTEGRATIONS_CLASS_DESCRIPTOR->getProtoBufParameterOverride(Ljava/lang/String;)Ljava/lang/String;
                         move-result-object p$protobufParameterRegister
                     """
                 )
             }
         } ?: return ProtobufParameterBuilderFingerprint.toErrorResult()
+
+        ConnectionResultFingerprint.result?.let {
+            val method = it.mutableMethod
+            val endIndex = it.scanResult.patternScanResult!!.endIndex
+            val statusCodeRegister = (method.instruction(endIndex - 2) as OneRegisterInstruction).registerA
+            val urlHeadersRegister = (method.instruction(endIndex) as OneRegisterInstruction).registerA
+
+            method.addInstructions(
+                endIndex + 1,
+                """
+                    invoke-static {v$statusCodeRegister, v$urlHeadersRegister}, $INTEGRATIONS_CLASS_DESCRIPTOR->connectionCompleted(ILjava/util/Map;)V
+                """
+            )
+        } ?: return ConnectionResultFingerprint.toErrorResult()
+
+        SettingsPatch.PreferenceScreen.MISC.addPreferences(
+            SwitchPreference(
+                "revanced_force_signature_spoofing",
+                StringResource("revanced_force_signature_spoofing_title", "Force app signature spoofing"),
+                false,
+                StringResource("revanced_force_signature_spoofing_summary_on", "Signature always spoofed"),
+                StringResource("revanced_force_signature_spoofing_summary_off", "Signature automatically spoofed when needed")
+            )
+        )
 
         return PatchResultSuccess()
     }
