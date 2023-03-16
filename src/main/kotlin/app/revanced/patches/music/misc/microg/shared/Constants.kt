cf4cd05f6b45f4424d072057453dd72c9c4b8330
AMSAMSAMSAMS <110509194+AMSAMSAMSAMS@users.noreply.github.com>
2023-03-16 10:30:08 +0700

    Update Constants.kt

--- src/main/kotlin/app/revanced/patches/music/misc/microg/shared/Constants.kt
+++ src/main/kotlin/app/revanced/patches/music/misc/microg/shared/Constants.kt
@@ -1,7 +1,7 @@
 package app.revanced.patches.music.misc.microg.shared
 
 object Constants {
-    internal const val REVANCED_MUSIC_APP_NAME = "YT Music ReVanced"
+    internal const val REVANCED_MUSIC_APP_NAME = "YouTube Music"
     internal const val REVANCED_MUSIC_PACKAGE_NAME = "app.revanced.android.apps.youtube.music"
     internal const val MUSIC_PACKAGE_NAME = "com.google.android.apps.youtube.music"
     internal const val SPOOFED_PACKAGE_NAME = MUSIC_PACKAGE_NAME
