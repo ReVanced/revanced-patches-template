997260c42a61b4d44076a6cc1a9465123d8d1faf
oSumAtrIX <johan.melkonyan1@web.de>
2023-03-19 23:24:31 +0100

    feat(youtube): support version `18.08.37`

--- src/main/kotlin/app/revanced/patches/youtube/misc/video/speed/remember/fingerprint/ChangePlaybackSpeedFragmentStateFingerprint.kt
+++
@@ -1,8 +0,0 @@
-package app.revanced.patches.youtube.misc.video.speed.remember.fingerprint
-
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-
-object ChangePlaybackSpeedFragmentStateFingerprint : MethodFingerprint(
-    "V",
-    strings = listOf("PLAYBACK_RATE_MENU_BOTTOM_SHEET_FRAGMENT")
-)
--- src/main/kotlin/app/revanced/patches/youtube/misc/video/speed/remember/fingerprint/InitializePlaybackSpeedValuesFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/video/speed/remember/fingerprint/InitializePlaybackSpeedValuesFingerprint.kt
@@ -3,5 +3,6 @@ package app.revanced.patches.youtube.misc.video.speed.remember.fingerprint
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 
 object InitializePlaybackSpeedValuesFingerprint : MethodFingerprint(
-    parameters = listOf("[L", "I")
+    parameters = listOf("[L", "I"),
+    strings = listOf("menu_item_playback_speed")
 )
