24aba9fea3bff0e29127cced5c248d2792788e86
oSumAtrIX <johan.melkonyan1@web.de>
2023-03-19 23:24:41 +0100

    feat(youtube): bump compatibility to `18.08.37`

--- src/main/kotlin/app/revanced/patches/youtube/layout/tabletminiplayer/annotations/TabletMiniPlayerCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/tabletminiplayer/annotations/TabletMiniPlayerCompatibility.kt
@@ -3,19 +3,6 @@ package app.revanced.patches.youtube.layout.tabletminiplayer.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility(
-    [Package(
-        "com.google.android.youtube", arrayOf(
-            "17.49.37", 
-            "18.03.36",
-            "18.03.42",
-            "18.04.35",
-            "18.04.41",
-            "18.05.32",
-            "18.05.35",
-            "18.05.40"
-        )
-    )]
-)
+@Compatibility([Package("com.google.android.youtube", arrayOf("18.08.37"))])
 @Target(AnnotationTarget.CLASS)
 internal annotation class TabletMiniPlayerCompatibility
