918d42992081020004ab235f56d818f90e2a267a
oSumAtrIX <johan.melkonyan1@web.de>
2023-03-19 23:12:26 +0100

    chore: minor refactoring

--- src/main/kotlin/app/revanced/patches/youtube/misc/video/videoid/patch/VideoIdPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/video/videoid/patch/VideoIdPatch.kt
@@ -27,23 +27,25 @@ class VideoIdPatch : BytecodePatch(
     listOf(VideoIdFingerprint, VideoIdFingerprintBackgroundPlay)
 ) {
     override fun execute(context: BytecodeContext): PatchResult {
-        VideoIdFingerprint.result?.let {
-            val videoIdRegisterInstructionIndex = it.scanResult.patternScanResult!!.endIndex
+        VideoIdFingerprint.result?.let { result ->
+            val videoIdRegisterInstructionIndex = result.scanResult.patternScanResult!!.endIndex
 
-            with(it.mutableMethod) {
-                insertMethod = this
+            result.mutableMethod.also {
+                insertMethod = it
+            }.apply {
                 videoIdRegister = (instruction(videoIdRegisterInstructionIndex) as OneRegisterInstruction).registerA
                 insertIndex = videoIdRegisterInstructionIndex + 1
             }
         } ?: return VideoIdFingerprint.toErrorResult()
 
-        VideoIdFingerprintBackgroundPlay.result?.let {
-            val endIndex = it.scanResult.patternScanResult!!.endIndex
+        VideoIdFingerprintBackgroundPlay.result?.let { result ->
+            val endIndex = result.scanResult.patternScanResult!!.endIndex
 
-            with(it.mutableMethod) {
-                backgroundPlayInsertMethod = this
-                backgroundPlayVideoIdRegister = (instruction(endIndex + 1) as OneRegisterInstruction).registerA
-                backgroundPlayInsertIndex = endIndex + 2
+            result.mutableMethod.also {
+                backgroundPlaybackMethod = it
+            }.apply {
+                backgroundPlaybackVideoIdRegister = (instruction(endIndex + 1) as OneRegisterInstruction).registerA
+                backgroundPlaybackInsertIndex = endIndex + 2
             }
         } ?: return VideoIdFingerprintBackgroundPlay.toErrorResult()
 
@@ -57,11 +59,11 @@ class VideoIdPatch : BytecodePatch(
         private lateinit var insertMethod: MutableMethod
 
         /**
-         * Adds an invoke-static instruction, called with the new id when the video changes
+         * Adds an invoke-static instruction, called with the new id when the video changes.
          *
-         * Supports all videos (regular videos, Shorts and Stories)
+         * Supports all videos (regular videos, Shorts and Stories).
          *
-         * _Does not function if playing in the background with no video visible_
+         * _Does not function if playing in the background with no video visible_.
          *
          * Be aware, this can be called multiple times for the same video id.
          *
@@ -70,22 +72,21 @@ class VideoIdPatch : BytecodePatch(
         fun injectCall(
             methodDescriptor: String
         ) = insertMethod.addInstructions(
-                // TODO: The order has been proven to not be required, so remove the logic for keeping order.
                 // Keep injection calls in the order they're added:
                 // Increment index. So if additional injection calls are added, those calls run after this injection call.
                 insertIndex++,
                 "invoke-static {v$videoIdRegister}, $methodDescriptor"
             )
 
-        private var backgroundPlayVideoIdRegister = 0
-        private var backgroundPlayInsertIndex = 0
-        private lateinit var backgroundPlayInsertMethod: MutableMethod
+        private var backgroundPlaybackVideoIdRegister = 0
+        private var backgroundPlaybackInsertIndex = 0
+        private lateinit var backgroundPlaybackMethod: MutableMethod
 
         /**
          * Alternate hook that supports only regular videos, but hook supports changing to new video
-         * during background play when no video is visible
+         * during background play when no video is visible.
          *
-         * _Does not support Shorts or Stories_
+         * _Does not support Shorts or Stories_.
          *
          * Be aware, the hook can be called multiple times for the same video id.
          *
@@ -93,9 +94,9 @@ class VideoIdPatch : BytecodePatch(
          */
         fun injectCallBackgroundPlay(
             methodDescriptor: String
-        ) = backgroundPlayInsertMethod.addInstructions(
-                backgroundPlayInsertIndex++, // move-result-object offset
-                "invoke-static {v$backgroundPlayVideoIdRegister}, $methodDescriptor"
+        ) = backgroundPlaybackMethod.addInstructions(
+            backgroundPlaybackInsertIndex++, // move-result-object offset
+                "invoke-static {v$backgroundPlaybackVideoIdRegister}, $methodDescriptor"
             )
     }
 }
