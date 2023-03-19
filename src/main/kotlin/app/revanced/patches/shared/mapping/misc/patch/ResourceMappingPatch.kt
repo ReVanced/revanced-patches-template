17bea9ee161fefaae69bde5f7dd1f4115f237f73
Ax333l <main@axelen.xyz>
2023-03-19 11:36:46 +0100

    i also forgot to push this

--- src/main/kotlin/app/revanced/patches/shared/mapping/misc/patch/ResourceMappingPatch.kt
+++ src/main/kotlin/app/revanced/patches/shared/mapping/misc/patch/ResourceMappingPatch.kt
@@ -4,26 +4,25 @@ import app.revanced.patcher.ResourceContext
 import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
-import app.revanced.patcher.apk.Apk
+import app.revanced.patcher.apk.arsc.ResourceElement
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.ResourcePatch
-import org.w3c.dom.Element
 import java.util.*
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
 
+
+// TODO: remove this patch and put resourceMappings in the context instead.
 
 @Name("resource-mapping")
 @Description("Creates a map of public resources.")
 @Version("0.0.1")
 class ResourceMappingPatch : ResourcePatch {
     companion object {
-        internal lateinit var resourceMappings: List<Apk.ResourceElement>
+        internal lateinit var resourceMappings: List<ResourceElement>
             private set
     }
 
     override fun execute(context: ResourceContext): PatchResult {
-        resourceMappings = context.apkBundle.base.resourceMap!!
+        resourceMappings = context.apkBundle.base.resourceMappings
 
         return PatchResult.Success
     }
