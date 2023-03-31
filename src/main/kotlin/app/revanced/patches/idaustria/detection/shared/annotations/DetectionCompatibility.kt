fe5dd1e1bdad71267bf8e028bde53dd12a038793
Your Name <you@example.com>
2023-04-01 00:51:55 +0200

    feat: Add version 2.6.0

--- src/main/kotlin/app/revanced/patches/idaustria/detection/shared/annotations/DetectionCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/idaustria/detection/shared/annotations/DetectionCompatibility.kt
@@ -3,6 +3,6 @@ package app.revanced.patches.idaustria.detection.shared.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility([Package("at.gv.oe.app", arrayOf("2.5.2"))])
+@Compatibility([Package("at.gv.oe.app", arrayOf("2.5.2", "2.6.0"))])
 @Target(AnnotationTarget.CLASS)
 internal annotation class DetectionCompatibility
