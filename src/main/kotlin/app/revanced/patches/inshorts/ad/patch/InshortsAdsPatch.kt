e909185492e72fca9ef02ff2c6e85bffbdee26e2
Aunali321 <48486084+Aunali321@users.noreply.github.com>
2023-04-01 18:22:18 +0000

    fix: failing build

--- src/main/kotlin/app/revanced/patches/inshorts/ad/patch/InshortsAdsPatch.kt
+++ src/main/kotlin/app/revanced/patches/inshorts/ad/patch/InshortsAdsPatch.kt
@@ -1,20 +1,21 @@
 package app.revanced.patches.inshorts.ad.patch
 
-import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.extensions.toErrorResult
+import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
-import app.revanced.patcher.BytecodeContext
-import app.revanced.extensions.toErrorResult
 import app.revanced.patcher.extensions.addInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patches.inshorts.ad.annotations.HideAdsCompatibility
 import app.revanced.patches.inshorts.ad.fingerprints.InshortsAdsFingerprint
 
 @Patch
 @Name("hide-ads")
-@Description("Removes ads from Inshorts app.")
+@Description("Removes ads from Inshorts.")
 @HideAdsCompatibility
 @Version("0.0.1")
 class HideAdsPatch : BytecodePatch(
@@ -32,6 +33,6 @@ class HideAdsPatch : BytecodePatch(
             }
         } ?: return InshortsAdsFingerprint.toErrorResult()
 
-        return PatchResult.Success
+        return PatchResultSuccess()
     }
 }
