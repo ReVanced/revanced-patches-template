587802c15e367cca8c5ffddc94f74ecdacdcdda9
Aunali321 <48486084+Aunali321@users.noreply.github.com>
2023-04-01 15:31:57 +0000

    feat: better fingerprint

--- src/main/kotlin/app/revanced/patches/inshorts/ad/fingerprints/InshortsAdsFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/inshorts/ad/fingerprints/InshortsAdsFingerprint.kt
@@ -1,8 +1,17 @@
 package app.revanced.patches.inshorts.ad.fingerprints
 
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import org.jf.dexlib2.Opcode
 
 object InshortsAdsFingerprint : MethodFingerprint(
     "V",
-    strings = listOf("GoogleAdLoader","exception in requestAd")
+    strings = listOf("GoogleAdLoader","exception in requestAd"),
+    opcodes = listOf(
+        Opcode.INVOKE_VIRTUAL_RANGE,
+        Opcode.GOTO,
+        Opcode.MOVE_EXCEPTION,
+        Opcode.CONST_STRING,
+        Opcode.CONST_STRING,
+        Opcode.INVOKE_STATIC,
+     ),
 )
