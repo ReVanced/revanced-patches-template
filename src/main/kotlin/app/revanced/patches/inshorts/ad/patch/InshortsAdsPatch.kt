d53ec6343abc40f73814461fbe84454dd119bd04
Aunali321 <48486084+Aunali321@users.noreply.github.com>
2023-04-01 17:58:36 +0000

    refactor: code refactor

--- src/main/kotlin/app/revanced/patches/inshorts/ad/patch/InshortsAdsPatch.kt
+++ src/main/kotlin/app/revanced/patches/inshorts/ad/patch/InshortsAdsPatch.kt
@@ -1,41 +1,37 @@
 package app.revanced.patches.inshorts.ad.patch
 
+import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
-import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.BytecodeContext
+import app.revanced.extensions.toErrorResult
 import app.revanced.patcher.extensions.addInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
-import app.revanced.patcher.patch.PatchResultError
-import app.revanced.patcher.patch.PatchResultSuccess
-import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patches.inshorts.ad.annotations.HideAdsCompatibility
 import app.revanced.patches.inshorts.ad.fingerprints.InshortsAdsFingerprint
-import org.jf.dexlib2.Opcode
-import org.jf.dexlib2.iface.instruction.ReferenceInstruction
-import org.jf.dexlib2.iface.instruction.TwoRegisterInstruction
-import org.jf.dexlib2.iface.reference.FieldReference
 
 @Patch
 @Name("hide-ads")
-@Description("Removes ads from Inshorts.")
+@Description("Removes ads from Inshorts app.")
 @HideAdsCompatibility
 @Version("0.0.1")
 class HideAdsPatch : BytecodePatch(
     listOf(InshortsAdsFingerprint)
 ) {
     override fun execute(context: BytecodeContext): PatchResult {
+        InshortsAdsFingerprint.result?.let { result ->
+            result.apply {
+                mutableMethod.addInstruction(
+                    0,
+                    """
+                        return-void
+                    """
+                )
+            }
+        } ?: return InshortsAdsFingerprint.toErrorResult()
 
-        val requestAdMethod = InshortsAdsFingerprint.result!!.mutableMethod
-        requestAdMethod.addInstruction(
-            0,
-            """
-                return-void
-            """
-        )
-
-
-        return PatchResultSuccess()
+        return PatchResult.Success
     }
 }
