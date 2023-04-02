08036d397b09dcf97311543cbc8a851b2ed67d67
Aunali321 <48486084+Aunali321@users.noreply.github.com>
2023-04-02 00:31:12 +0000

    refactor: removes opcodes

--- src/main/kotlin/app/revanced/patches/inshorts/ad/fingerprints/InshortsAdsFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/inshorts/ad/fingerprints/InshortsAdsFingerprint.kt
@@ -5,13 +5,5 @@ import org.jf.dexlib2.Opcode
 
 object InshortsAdsFingerprint : MethodFingerprint(
     "V",
-    strings = listOf("GoogleAdLoader","exception in requestAd"),
-    opcodes = listOf(
-        Opcode.INVOKE_VIRTUAL_RANGE,
-        Opcode.GOTO,
-        Opcode.MOVE_EXCEPTION,
-        Opcode.CONST_STRING,
-        Opcode.CONST_STRING,
-        Opcode.INVOKE_STATIC,
-     ),
+    strings = listOf("GoogleAdLoader","exception in requestAd")
 )
