ee1a7bdf238d57919d433142b0bb1f05c7c0c917
Andris <sebokandris2009@gmail.com>
2023-04-06 14:20:25 +0200

    feat: add shared patch to change the package name

---
+++ src/main/kotlin/app/revanced/patches/all/misc/packagename/patch/ChangePackageNamePatch.kt
@@ -0,0 +1,44 @@
+package app.revanced.patches.all.misc.packagename.patch
+
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.patch.*
+import app.revanced.patcher.patch.annotations.Patch
+import org.w3c.dom.Element
+
+@Patch(false)
+@Name("change-package-name")
+@Description("Changes the package name.")
+@Version("0.0.1")
+class ChangePackageNamePatch : ResourcePatch {
+    override fun execute(context: ResourceContext): PatchResult {
+        var originalPackageName = ""
+        packageName?.let { packageName ->
+            val packageNameRegex = Regex("^[a-z]\\w*(\\.[a-z]\\w*)+\$")
+            if (!packageName.matches(packageNameRegex)) return PatchResultError("Invalid package name")
+
+            context.xmlEditor["AndroidManifest.xml"].use { editor ->
+                val manifest = editor.file.getElementsByTagName("manifest").item(0) as Element
+                originalPackageName = manifest.getAttribute("package")
+            }
+            if (!originalPackageName.matches(packageNameRegex)) return PatchResultError("Failed to get the original package name")
+            context["AndroidManifest.xml"].writeText(context["AndroidManifest.xml"].readText().replace(originalPackageName, packageName))
+
+        } ?: return PatchResultError("No package name provided")
+
+        return PatchResultSuccess()
+    }
+
+    companion object : OptionsContainer() {
+        var packageName: String? by option(
+            PatchOption.StringOption(
+                key = "packageName",
+                default = "",
+                title = "Package name",
+                description = "The name of the package to rename of the app.",
+            )
+        )
+    }
+}
