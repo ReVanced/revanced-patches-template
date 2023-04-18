3daf875a2e64a5a32bcdffb5a9b15ee2a66d2741
badawoll <129878899+badawoll@users.noreply.github.com>
2023-04-18 23:47:17 +0000

    feat(memegenerator): `unlock-pro` patch (#1902)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

---
+++ src/main/kotlin/app/revanced/patches/memegenerator/detection/signature/patch/SignatureVerificationPatch.kt
@@ -0,0 +1,31 @@
+package app.revanced.patches.memegenerator.detection.signature.patch
+
+import app.revanced.extensions.toErrorResult
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.replaceInstructions
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patches.memegenerator.detection.signature.fingerprint.VerifySignatureFingerprint
+
+@Description("Disables detection of incorrect signature.")
+@Version("0.0.1")
+class SignatureVerificationPatch : BytecodePatch(
+    listOf(VerifySignatureFingerprint)
+) {
+    override fun execute(context: BytecodeContext): PatchResult {
+        VerifySignatureFingerprint.result?.apply {
+            mutableMethod.replaceInstructions(
+                0,
+                """
+                    const/4 p0, 0x1
+                    return  p0
+                """
+            )
+        } ?: throw VerifySignatureFingerprint.toErrorResult()
+
+        return PatchResultSuccess()
+    }
+}
---
+++ src/main/kotlin/app/revanced/patches/memegenerator/misc/pro/patch/UnlockProVersionPatch.kt
@@ -0,0 +1,45 @@
+package app.revanced.patches.memegenerator.misc.pro.patch
+
+import app.revanced.extensions.toErrorResult
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.replaceInstructions
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.DependsOn
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patches.memegenerator.detection.license.patch.LicenseValidationPatch
+import app.revanced.patches.memegenerator.detection.signature.patch.SignatureVerificationPatch
+import app.revanced.patches.memegenerator.misc.pro.annotations.UnlockProCompatibility
+import app.revanced.patches.memegenerator.misc.pro.fingerprint.IsFreeVersionFingerprint
+
+@Patch
+@Name("unlock-pro")
+@Description("Unlocks pro features.")
+@DependsOn([
+    SignatureVerificationPatch::class,
+    LicenseValidationPatch::class
+])
+@UnlockProCompatibility
+@Version("0.0.1")
+class UnlockProVersionPatch : BytecodePatch(
+    listOf(
+        IsFreeVersionFingerprint
+    )
+) {
+    override fun execute(context: BytecodeContext): PatchResult {
+        IsFreeVersionFingerprint.result?.apply {
+            mutableMethod.replaceInstructions(0,
+                """
+                    sget-object p0, Ljava/lang/Boolean;->FALSE:Ljava/lang/Boolean;
+                    return-object p0
+                """
+            )
+        } ?: throw IsFreeVersionFingerprint.toErrorResult()
+
+        return PatchResultSuccess()
+    }
+}
