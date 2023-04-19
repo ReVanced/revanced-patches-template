6f851e425337ff78437330624a3cd7167cc4f920
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-04-19 10:35:44 +0400

    fix(youtube/return-youtube-dislike): render dislikes when scrolling into the screen (#1873)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/returnyoutubedislike/fingerprints/TextComponentAtomicReferenceFingerprint.kt
@@ -0,0 +1,34 @@
+package app.revanced.patches.youtube.layout.returnyoutubedislike.fingerprints
+
+import app.revanced.patcher.extensions.or
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import org.jf.dexlib2.AccessFlags
+import org.jf.dexlib2.Opcode
+
+/**
+ * Resolves against the same method that [TextComponentContextFingerprint] resolves to.
+ */
+object TextComponentAtomicReferenceFingerprint : MethodFingerprint(
+    returnType = "L",
+    access = AccessFlags.PROTECTED or AccessFlags.FINAL,
+    parameters = listOf("L"),
+    opcodes = listOf(
+        Opcode.MOVE_OBJECT, // available unused register
+        Opcode.MOVE_OBJECT_FROM16,
+        Opcode.MOVE_OBJECT_FROM16,
+        Opcode.MOVE_FROM16,
+        Opcode.INVOKE_VIRTUAL, // CharSequence atomic reference
+        Opcode.MOVE_RESULT_OBJECT,
+        Opcode.CHECK_CAST,
+        Opcode.MOVE_OBJECT, // CharSequence reference, and control flow label. Insert code here.
+        Opcode.INVOKE_VIRTUAL_RANGE,
+        Opcode.MOVE_RESULT,
+        Opcode.IF_EQZ,
+        Opcode.INVOKE_VIRTUAL_RANGE,
+        Opcode.MOVE_RESULT_OBJECT,
+        Opcode.GOTO,
+        Opcode.CONST_4,
+        Opcode.INVOKE_VIRTUAL_RANGE,
+        Opcode.MOVE_RESULT_OBJECT,
+    )
+)
