091a25d46145b1c27791245fca0933e9c8a68e9a
oSumAtrIX <johan.melkonyan1@web.de>
2023-04-20 03:20:35 +0200

    fix(youtube/microg-support): rename patch correctly

--- src/main/kotlin/app/revanced/patches/youtube/misc/microg/patch/bytecode/MicroGBytecodePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/microg/patch/bytecode/MicroGBytecodePatch.kt
@@ -27,7 +27,7 @@ import app.revanced.util.microg.MicroGBytecodeHelper
         HideCastButtonPatch::class,
     ]
 )
-@Name("microg-support")
+@Name("vanced-microg-support")
 @Description("Allows YouTube ReVanced to run without root and under a different package name with Vanced MicroG.")
 @MicroGPatchCompatibility
 @Version("0.0.1")
