3ab5842ebbee3381604efcaa454ea5c49d3a5363
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-04-23 06:16:10 +0400

    fix(youtube/hide-video-action-buttons): fix hide action buttons not working for some users (#1959)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patches/youtube/layout/buttons/action/patch/HideButtonsPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/buttons/action/patch/HideButtonsPatch.kt
@@ -52,11 +52,20 @@ class HideButtonsPatch : ResourcePatch {
                         StringResource("revanced_hide_playlist_button_summary_off", "Playlist button is shown")
                     ),
                     SwitchPreference(
+                        "revanced_hide_clip_button",
+                        StringResource("revanced_hide_clip_button_title", "Hide clip button"),
+                        false,
+                        StringResource("revanced_hide_clip_button_summary_on", "Clip button is hidden"),
+                        StringResource("revanced_hide_clip_button_summary_off", "Clip button is shown"),
+                        StringResource("revanced_hide_clip_button_user_dialog_message",
+                            "Hiding the clip button might not work reliably. In the case it does not work, it can only be hidden by enabling \\'Hide all other action buttons\\'")
+                    ),
+                    SwitchPreference(
                         "revanced_hide_action_buttons",
                         StringResource("revanced_hide_action_buttons_title", "Hide all other action buttons"),
                         false,
-                        StringResource("revanced_hide_action_buttons_summary_on", "Share, remix, clip, thanks, shop, live chat buttons are hidden"),
-                        StringResource("revanced_hide_action_buttons_summary_off", "Share, remix, clip, thanks, shop, live chat buttons are shown")
+                        StringResource("revanced_hide_action_buttons_summary_on", "Share, remix, thanks, shop, live chat buttons are hidden"),
+                        StringResource("revanced_hide_action_buttons_summary_off", "Share, remix, thanks, shop, live chat buttons are shown")
                     )
                 ),
                 StringResource("revanced_hide_buttons_summary", "Hide or show buttons under videos")
