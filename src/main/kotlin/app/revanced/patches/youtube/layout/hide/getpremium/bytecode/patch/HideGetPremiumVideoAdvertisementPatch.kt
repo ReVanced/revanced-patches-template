3dc57d42457633fb27dfbfdbfcf2f3520c51bbb7
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-04-30 00:37:59 +0400

    cleanup, comments

--- src/main/kotlin/app/revanced/patches/youtube/layout/hide/getpremium/bytecode/patch/HideGetPremiumVideoAdvertisementPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hide/getpremium/bytecode/patch/HideGetPremiumVideoAdvertisementPatch.kt
@@ -12,14 +12,16 @@ import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patches.shared.settings.preference.impl.StringResource
+import app.revanced.patches.shared.settings.preference.impl.SwitchPreference
 import app.revanced.patches.youtube.layout.hide.getpremium.annotations.HideGetPremiumVideoAdvertisementCompatibility
 import app.revanced.patches.youtube.layout.hide.getpremium.bytecode.fingerprints.GetPremiumVideoAdvertisementViewFingerprint
-import app.revanced.patches.youtube.layout.hide.getpremium.resource.patch.HideGetPremiumVideoAdvertisementResourcePatch
 import app.revanced.patches.youtube.misc.integrations.patch.IntegrationsPatch
+import app.revanced.patches.youtube.misc.settings.bytecode.patch.SettingsPatch
 import org.jf.dexlib2.iface.instruction.TwoRegisterInstruction
 
 @Patch
-@DependsOn([IntegrationsPatch::class, HideGetPremiumVideoAdvertisementResourcePatch::class])
+@DependsOn([IntegrationsPatch::class, SettingsPatch::class])
 @Name("hide-get-premium")
 @Description("Hides the advertisement for YouTube Premium under the video player.")
 @HideGetPremiumVideoAdvertisementCompatibility
@@ -30,6 +32,16 @@ class HideGetPremiumPatch : BytecodePatch(
     )
 ) {
     override fun execute(context: BytecodeContext): PatchResult {
+        SettingsPatch.PreferenceScreen.LAYOUT.addPreferences(
+            SwitchPreference(
+                "revanced_hide_get_premium",
+                StringResource("revanced_hide_get_premium_title", "Hide YouTube Premium advertisement"),
+                true,
+                StringResource("revanced_hide_get_premium_summary_on", "YouTube Premium advertisement are hidden"),
+                StringResource("revanced_hide_get_premium_summary_off", "YouTube Premium advertisement are shown")
+            )
+        )
+
         GetPremiumVideoAdvertisementViewFingerprint.result?.let {
             it.mutableMethod.apply {
                 val startIndex = it.scanResult.patternScanResult!!.startIndex
@@ -41,6 +53,7 @@ class HideGetPremiumPatch : BytecodePatch(
                 addInstructions(
                     startIndex + 2,
                     """
+                        # Override the internal measurement of the layout with zero values.
                         invoke-static {}, $INTEGRATIONS_CLASS_DESCRIPTOR->hideGetPremiumView()Z
                         move-result v$tempRegister
                         if-eqz v$tempRegister, :allow
@@ -48,6 +61,7 @@ class HideGetPremiumPatch : BytecodePatch(
                         const/4 v$measuredHeightRegister, 0x0
                         :allow
                         nop
+                        # Layout width/height is then passed to a protected class method.
                     """
                 )
             }
