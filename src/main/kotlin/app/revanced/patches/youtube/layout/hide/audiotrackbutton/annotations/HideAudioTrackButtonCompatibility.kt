8da724bf628e9587157282ebcc8a1e1d0385d247
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-04-30 00:52:50 +0400

    moving to general ads patch

--- src/main/kotlin/app/revanced/patches/youtube/layout/hide/audiotrackbutton/annotations/HideAudioTrackButtonCompatibility.kt
+++
@@ -1,8 +0,0 @@
-package app.revanced.patches.youtube.layout.hide.audiotrackbutton.annotations
-
-import app.revanced.patcher.annotation.Compatibility
-import app.revanced.patcher.annotation.Package
-
-@Compatibility([Package("com.google.android.youtube", arrayOf("18.15.40"))])
-@Target(AnnotationTarget.CLASS)
-internal annotation class HideAudioTrackButtonCompatibility
