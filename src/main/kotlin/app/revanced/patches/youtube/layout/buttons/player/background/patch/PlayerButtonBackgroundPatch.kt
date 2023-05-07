f693d55caf1e0b72bb1f4c39b1eeb59436191e02
johnconner122 <107796137+johnconner122@users.noreply.github.com>
2023-05-08 04:57:44 +0500

    feat(youtube/hide-player-overlay): make it toggleable in settings (#2044)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patches/youtube/layout/buttons/player/background/patch/PlayerButtonBackgroundPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/buttons/player/background/patch/PlayerButtonBackgroundPatch.kt
@@ -12,16 +12,12 @@ import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patches.youtube.layout.buttons.player.background.annotations.PlayerButtonBackgroundCompatibility
 import org.w3c.dom.Element
 
-@Patch
+@Patch(false)
 @Name("remove-player-button-background")
 @Description("Removes the background from the video player buttons.")
 @PlayerButtonBackgroundCompatibility
 @Version("0.0.1")
 class PlayerButtonBackgroundPatch : ResourcePatch {
-    private companion object {
-        const val RESOURCE_FILE_PATH = "res/drawable/player_button_circle_background.xml"
-    }
-    
     override fun execute(context: ResourceContext): PatchResult {
         context.xmlEditor[RESOURCE_FILE_PATH].use { editor ->
             editor.file.doRecursively node@{ node ->
@@ -34,5 +30,9 @@ class PlayerButtonBackgroundPatch : ResourcePatch {
         }
 
         return PatchResultSuccess()
+    }
+
+    private companion object {
+        const val RESOURCE_FILE_PATH = "res/drawable/player_button_circle_background.xml"
     }
 }
