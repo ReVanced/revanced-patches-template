87d2f4c2ebd711556b89a4a9e5d1b3551798d3b6
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-05-13 16:36:05 +0400

    fixing merge

--- src/main/kotlin/app/revanced/patches/youtube/layout/theme/bytecode/patch/ThemeBytecodePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/theme/bytecode/patch/ThemeBytecodePatch.kt
@@ -8,8 +8,6 @@ import app.revanced.patcher.patch.*
 import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patches.youtube.layout.seekbar.bytecode.patch.SeekbarColorBytecodePatch
-import app.revanced.patches.youtube.layout.seekbar.bytecode.fingerprints.CreateDarkThemeSeekbarFingerprint
-import app.revanced.patches.youtube.layout.seekbar.bytecode.fingerprints.SetSeekbarClickedColorFingerprint
 import app.revanced.patches.youtube.layout.theme.resource.ThemeResourcePatch
 
 @Patch
@@ -17,9 +15,7 @@ import app.revanced.patches.youtube.layout.theme.resource.ThemeResourcePatch
 @Description("Applies a custom theme.")
 @DependsOn([LithoColorHookPatch::class, SeekbarColorBytecodePatch::class, ThemeResourcePatch::class])
 @Version("0.0.1")
-class ThemeBytecodePatch : BytecodePatch(
-    listOf(CreateDarkThemeSeekbarFingerprint, SetSeekbarClickedColorFingerprint)
-) {
+class ThemeBytecodePatch : BytecodePatch() {
 
     override fun execute(context: BytecodeContext): PatchResult {
         LithoColorHookPatch.lithoColorOverrideHook(INTEGRATIONS_CLASS_DESCRIPTOR, "getValue")
