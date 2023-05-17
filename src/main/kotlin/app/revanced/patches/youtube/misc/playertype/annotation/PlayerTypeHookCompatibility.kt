034215ab07535031071a2c9d19c05137c3bfb06a
oSumAtrIX <johan.melkonyan1@web.de>
2023-05-18 01:18:56 +0200

    feat(youtube): constrain patches to `18.16.37`

--- src/main/kotlin/app/revanced/patches/youtube/misc/playertype/annotation/PlayerTypeHookCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/playertype/annotation/PlayerTypeHookCompatibility.kt
@@ -3,13 +3,6 @@ package app.revanced.patches.youtube.misc.playertype.annotation
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility(
-    [Package(
-        "com.google.android.youtube", arrayOf(
-            "18.15.40",
-            "18.16.37"
-        )
-    )]
-)
+@Compatibility([Package("com.google.android.youtube", arrayOf("18.16.37"))])
 @Target(AnnotationTarget.CLASS)
 internal annotation class PlayerTypeHookCompatibility
--- src/main/kotlin/app/revanced/patches/youtube/video/information/annotation/VideoInformationCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/youtube/video/information/annotation/VideoInformationCompatibility.kt
@@ -3,8 +3,6 @@ package app.revanced.patches.youtube.video.information.annotation
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility(
-    [Package("com.google.android.youtube", arrayOf("18.08.37", "18.15.40", "18.16.37"))]
-)
+@Compatibility([Package("com.google.android.youtube", arrayOf("18.16.37"))])
 @Target(AnnotationTarget.CLASS)
 internal annotation class VideoInformationCompatibility
