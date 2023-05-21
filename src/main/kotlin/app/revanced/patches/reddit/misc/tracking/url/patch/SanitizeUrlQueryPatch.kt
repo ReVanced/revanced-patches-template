9593e4b5db604957545b4ab6747c82fb815ac08b
KAZI MMT <82371061+kazimmt@users.noreply.github.com>
2023-05-21 21:55:10 +0600

    feat(reddit): add `sanitize-sharing-links` patch (#2192)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

---
+++ src/main/kotlin/app/revanced/patches/reddit/misc/tracking/url/patch/SanitizeUrlQueryPatch.kt
@@ -0,0 +1,52 @@
+package app.revanced.patches.reddit.misc.tracking.url.patch
+
+import app.revanced.extensions.toErrorResult
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.addInstructions
+import app.revanced.patcher.extensions.instruction
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.patch.annotations.RequiresIntegrations
+import app.revanced.patches.reddit.misc.tracking.url.annotations.SanitizeUrlQueryCompatibility
+import app.revanced.patches.reddit.misc.tracking.url.fingerprints.ShareLinkFactoryFingerprint
+import org.jf.dexlib2.iface.instruction.OneRegisterInstruction
+
+@Patch
+@Name("sanitize-sharing-links")
+@Description("Removes (tracking) query parameters from the URLs when sharing links.")
+@SanitizeUrlQueryCompatibility
+@Version("0.0.1")
+@RequiresIntegrations
+class SanitizeUrlQueryPatch : BytecodePatch(
+    listOf(ShareLinkFactoryFingerprint)
+) {
+    override fun execute(context: BytecodeContext): PatchResult {
+        ShareLinkFactoryFingerprint.result?.let { result ->
+            result.mutableMethod.apply {
+                val insertIndex = result.scanResult.patternScanResult!!.endIndex + 1
+                val urlRegister = instruction<OneRegisterInstruction>(insertIndex - 1).registerA
+
+                addInstructions(
+                    insertIndex,
+                    """
+                        invoke-static {v$urlRegister}, $SANITIZE_METHOD_DESCRIPTOR
+                        move-result-object v$urlRegister
+                   """
+                )
+            }
+        } ?: return ShareLinkFactoryFingerprint.toErrorResult()
+
+        return PatchResultSuccess()
+    }
+
+    private companion object {
+        private const val SANITIZE_METHOD_DESCRIPTOR =
+            "Lapp/revanced/reddit/patches/SanitizeUrlQueryPatch;" +
+                    "->stripQueryParameters(Ljava/lang/String;)Ljava/lang/String;"
+    }
+}
