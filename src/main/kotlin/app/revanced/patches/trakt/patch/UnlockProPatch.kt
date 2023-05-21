1e8dcae6f540455b8698703bbded5f52fd0c6300
johnconner122 <107796137+johnconner122@users.noreply.github.com>
2023-05-21 22:27:50 +0500

    feat(trakt): add `unlock-pro` patch (#2210)
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

---
+++ src/main/kotlin/app/revanced/patches/trakt/patch/UnlockProPatch.kt
@@ -0,0 +1,52 @@
+package app.revanced.patches.trakt.patch
+
+import app.revanced.extensions.toErrorResult
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.addInstructions
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint.Companion.resolve
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patches.trakt.annotations.UnlockProCompatibility
+import app.revanced.patches.trakt.fingerprints.IsVIPEPFingerprint
+import app.revanced.patches.trakt.fingerprints.IsVIPFingerprint
+import app.revanced.patches.trakt.fingerprints.RealmUserSettingsFingerprint
+
+@Patch
+@Name("unlock-pro")
+@Description("Unlocks pro features.")
+@UnlockProCompatibility
+@Version("0.0.1")
+class UnlockProPatch : BytecodePatch(
+    listOf(RealmUserSettingsFingerprint)
+) {
+    override fun execute(context: BytecodeContext): PatchResult {
+        RealmUserSettingsFingerprint.result?.classDef?.let { realUserSettingsClass ->
+            arrayOf(IsVIPFingerprint, IsVIPEPFingerprint).onEach { fingerprint ->
+                // Resolve both fingerprints on the same class.
+                if (!fingerprint.resolve(context, realUserSettingsClass))
+                    throw fingerprint.toErrorResult()
+            }.forEach { fingerprint ->
+                // Return true for both VIP check methods.
+                fingerprint.result?.mutableMethod?.addInstructions(0, RETURN_TRUE_INSTRUCTIONS)
+                    ?: return fingerprint.toErrorResult()
+            }
+        } ?: return RealmUserSettingsFingerprint.toErrorResult()
+
+        return PatchResultSuccess()
+    }
+
+    private companion object {
+        const val RETURN_TRUE_INSTRUCTIONS =
+            """
+                const/4 v0, 0x1
+                invoke-static {v0}, Ljava/lang/Boolean;->valueOf(Z)Ljava/lang/Boolean;
+                move-result-object v1
+                return-object v1
+            """
+    }
+}
