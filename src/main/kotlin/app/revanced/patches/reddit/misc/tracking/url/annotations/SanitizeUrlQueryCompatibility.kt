ab6038e3798d4093cd3161bb0fb4f01056e236fd
KAZI MMT <82371061+kazimmt@users.noreply.github.com>
2023-05-23 00:19:45 +0600

    feat(reddit):bump patches compatibility

--- src/main/kotlin/app/revanced/patches/reddit/misc/tracking/url/annotations/SanitizeUrlQueryCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/reddit/misc/tracking/url/annotations/SanitizeUrlQueryCompatibility.kt
@@ -3,6 +3,13 @@ package app.revanced.patches.reddit.misc.tracking.url.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility([Package("com.reddit.frontpage", arrayOf("2023.12.0", "2023.17.1"))])
+@Compatibility(
+    [Package(
+        "com.reddit.frontpage", arrayOf(
+            "2023.12.0",
+            "2023.17.1",
+            "2023.18.0",
+            "2023.19.0"
+        ))])
 @Target(AnnotationTarget.CLASS)
 internal annotation class SanitizeUrlQueryCompatibility
