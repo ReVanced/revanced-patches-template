f1288e4bb8fb1b9f394d73fd814d97db8704b8e0
KAZI MMT <82371061+kazimmt@users.noreply.github.com>
2023-05-23 01:28:51 +0600

    feat(reddit): remove compatibility version constraints (#2226)

--- src/main/kotlin/app/revanced/patches/reddit/ad/general/annotations/HideAdsCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/reddit/ad/general/annotations/HideAdsCompatibility.kt
@@ -5,20 +5,7 @@ import app.revanced.patcher.annotation.Package
 
 @Compatibility(
     [Package(
-        "com.reddit.frontpage", arrayOf(
-            "2021.45.0", 
-            "2022.43.0", 
-            "2023.05.0",
-            "2023.06.0",
-            "2023.07.0", 
-            "2023.07.1",
-            "2023.08.0",
-            "2023.09.0",
-            "2023.09.1",
-            "2023.10.0",
-            "2023.11.0",
-            "2023.12.0"
-        )
+        "com.reddit.frontpage"
     )]
 )
 @Target(AnnotationTarget.CLASS)
--- src/main/kotlin/app/revanced/patches/reddit/misc/tracking/url/annotations/SanitizeUrlQueryCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/reddit/misc/tracking/url/annotations/SanitizeUrlQueryCompatibility.kt
@@ -3,6 +3,10 @@ package app.revanced.patches.reddit.misc.tracking.url.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility([Package("com.reddit.frontpage", arrayOf("2023.12.0", "2023.17.1"))])
+@Compatibility(
+    [Package(
+        "com.reddit.frontpage"
+    )]
+)
 @Target(AnnotationTarget.CLASS)
 internal annotation class SanitizeUrlQueryCompatibility
