35b51008ffef95be1a0ca2bbbf7e925cf3b6d3fd
KAZI MMT <82371061+kazimmt@users.noreply.github.com>
2023-05-23 01:13:16 +0600

    remove version constraint from patches

--- src/main/kotlin/app/revanced/patches/reddit/misc/tracking/url/annotations/SanitizeUrlQueryCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/reddit/misc/tracking/url/annotations/SanitizeUrlQueryCompatibility.kt
@@ -5,11 +5,8 @@ import app.revanced.patcher.annotation.Package
 
 @Compatibility(
     [Package(
-        "com.reddit.frontpage", arrayOf(
-            "2023.12.0",
-            "2023.17.1",
-            "2023.18.0",
-            "2023.19.0"
-        ))])
+        "com.reddit.frontpage"
+    )]
+)
 @Target(AnnotationTarget.CLASS)
 internal annotation class SanitizeUrlQueryCompatibility
