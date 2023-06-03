9d24589aeca4aebc952f9b7cdb00df4689ad235d
Your Name <you@example.com>
2023-06-03 13:36:56 +0200

    feat: Support 3.0.1, make patch more robust

--- src/main/kotlin/app/revanced/patches/finanzonline/detection/shared/annotations/DetectionCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/finanzonline/detection/shared/annotations/DetectionCompatibility.kt
@@ -3,6 +3,6 @@ package app.revanced.patches.finanzonline.detection.shared.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility([Package("at.gv.bmf.bmf2go", arrayOf("2.2.0"))])
+@Compatibility([Package("at.gv.bmf.bmf2go")])
 @Target(AnnotationTarget.CLASS)
 internal annotation class DetectionCompatibility
