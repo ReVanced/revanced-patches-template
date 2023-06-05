5f28ace9ccb3a8101c4c932a4cd181d29b19b4b1
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-06-06 03:58:32 +0400

    cleanup

--- src/main/kotlin/app/revanced/patches/youtube/misc/translations/patch/TranslationResourcePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/translations/patch/TranslationResourcePatch.kt
@@ -5,6 +5,7 @@ import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.data.ResourceContext
 import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultError
 import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.ResourcePatch
 import app.revanced.patcher.patch.annotations.DependsOn
@@ -24,27 +25,34 @@ import java.util.regex.Pattern
 class TranslationResourcePatch : ResourcePatch {
 
     override fun execute(context: ResourceContext): PatchResult {
-        // Look in the jar file and find the path of the translation string files.
-        val pattern = Pattern.compile("youtube\\/translation\\/([-a-zA-Z_0-9]+)\\/strings.xml")
+        // Look in the jar file and find the paths of the translation string files.
+        val pattern = Pattern.compile("$TRANSLATION_RESOURCE_DIRECTORY/([-_a-zA-Z0-9]+)/strings\\.xml")
 
         var jf: JarFile? = null
         try {
             jf = JarFile(this.javaClass.protectionDomain.codeSource.location.toURI().path)
             val entries = jf.entries()
+            var foundElements = false
             while (entries.hasMoreElements()) {
                 val match = pattern.matcher(entries.nextElement().name)
                 if (match.find()) {
                     val languageDirectory = match.group(1)
-                    context.copyResources("youtube/translation",
+                    context.copyResources(TRANSLATION_RESOURCE_DIRECTORY,
                         ResourceUtils.ResourceGroup(languageDirectory, "strings.xml")
                     )
+                    foundElements = true
                 }
             }
+            if (!foundElements) return PatchResultError("could not find translated string files")
         } finally {
             jf?.close()
         }
 
         return PatchResultSuccess()
+    }
+
+    private companion object {
+        const val TRANSLATION_RESOURCE_DIRECTORY = "youtube/translation"
     }
 
 }
