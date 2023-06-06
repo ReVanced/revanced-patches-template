e77dbfc55fd2289948ec9dcd325e16200d564645
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-06-06 15:57:18 +0400

    twitch translations stubs

---
+++ src/main/kotlin/app/revanced/patches/twitch/misc/translations/patch/TwitchTranslationResourcePatch.kt
@@ -0,0 +1,36 @@
+package app.revanced.patches.twitch.misc.translations.patch
+
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.ResourcePatch
+import app.revanced.patcher.patch.annotations.DependsOn
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patches.shared.settings.preference.impl.*
+import app.revanced.patches.twitch.misc.settings.resource.patch.TwitchSettingsResourcePatch
+import app.revanced.patches.twitch.misc.translations.annotation.TwitchTranslationCompatibility
+import app.revanced.util.resources.ResourceUtils
+
+@Patch
+@Name("translations")
+@Description("Adds translations to Twitch.")
+@TwitchTranslationCompatibility
+@DependsOn([TwitchSettingsResourcePatch::class])
+@Version("0.0.1")
+class TwitchTranslationResourcePatch : ResourcePatch {
+
+    override fun execute(context: ResourceContext): PatchResult {
+        // Look in the jar file and find the paths of the translation string files.
+        ResourceUtils.copyLocalizedStringFiles(context, TRANSLATION_RESOURCE_DIRECTORY)
+
+        return PatchResultSuccess()
+    }
+
+    private companion object {
+        const val TRANSLATION_RESOURCE_DIRECTORY = "twitch/translation"
+    }
+
+}
