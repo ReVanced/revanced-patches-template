4bc2cb6b4db922c11615f5ef74378ae1cfd0dd22
1fexd <PGFvy85nC@protonmail.com>
2023-06-10 21:48:59 +0200

    chore(songpal): Make patches more resilient

--- src/main/kotlin/app/revanced/patches/songpal/badge/fingerprints/CreateTabsFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/songpal/badge/fingerprints/CreateTabsFingerprint.kt
@@ -1,31 +1,24 @@
 package app.revanced.patches.songpal.badge.fingerprints
 
-import app.revanced.patcher.fingerprint.method.annotation.FuzzyPatternScanMethod
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import app.revanced.patches.songpal.badge.patch.BadgeTabPatch
 import org.jf.dexlib2.AccessFlags
 import org.jf.dexlib2.Opcode
+import org.jf.dexlib2.dexbacked.reference.DexBackedMethodReference
+import org.jf.dexlib2.iface.instruction.ReferenceInstruction
 
 // Located @ ub.i0.h#p (9.5.0)
-@FuzzyPatternScanMethod(2)
 object CreateTabsFingerprint : MethodFingerprint(
-    "L",
+    "Ljava/util/List",
     accessFlags = AccessFlags.PRIVATE.value,
-    opcodes = listOf(
-        Opcode.INVOKE_STATIC,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.INVOKE_STATIC,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.SGET_OBJECT,
-        Opcode.INVOKE_INTERFACE,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.SGET_OBJECT,
-        Opcode.INVOKE_INTERFACE,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.INVOKE_STATIC,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.INVOKE_INTERFACE,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.CHECK_CAST,
-        Opcode.RETURN_OBJECT
-    )
+    customFingerprint = { methodDef, _ ->
+        methodDef.implementation?.instructions?.any { instruction ->
+            if (instruction.opcode != Opcode.INVOKE_STATIC) return@any false
+
+            val ref = ((instruction as ReferenceInstruction).reference as DexBackedMethodReference)
+            ref.definingClass == BadgeTabPatch.ACTIVITY_TAB_DESCRIPTOR
+                    && ref.parameterTypes.isEmpty()
+                    && ref.returnType == "[${BadgeTabPatch.ACTIVITY_TAB_DESCRIPTOR}"
+        } ?: false
+    }
 )
--- src/main/kotlin/app/revanced/patches/songpal/badge/fingerprints/ShowNotificationFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/songpal/badge/fingerprints/ShowNotificationFingerprint.kt
@@ -1,31 +1,25 @@
 package app.revanced.patches.songpal.badge.fingerprints
 
-import app.revanced.patcher.fingerprint.method.annotation.FuzzyPatternScanMethod
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import org.jf.dexlib2.AccessFlags
 import org.jf.dexlib2.Opcode
+import org.jf.dexlib2.dexbacked.reference.DexBackedMethodReference
+import org.jf.dexlib2.iface.instruction.ReferenceInstruction
 
 // Located @ com.sony.songpal.mdr.vim.activity.MdrRemoteBaseActivity.e#run (9.5.0)
-@FuzzyPatternScanMethod(2)
 object ShowNotificationFingerprint : MethodFingerprint(
     "V",
     accessFlags = AccessFlags.PUBLIC.value,
-    opcodes = listOf(
-        Opcode.IGET_OBJECT,
-        Opcode.IGET,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.IGET_OBJECT,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.CONST,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.MOVE_RESULT,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.IGET_BOOLEAN,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.CONST,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.RETURN_VOID
-    )
+    customFingerprint = { methodDef, _ ->
+        methodDef.implementation?.instructions?.any { instruction ->
+            if (instruction.opcode != Opcode.INVOKE_VIRTUAL) return@any false
+            if (instruction !is ReferenceInstruction) return@any false
+            if (instruction.reference !is DexBackedMethodReference) return@any false
+
+            val ref = (instruction.reference as DexBackedMethodReference)
+            ref.definingClass == "Lcom/google/android/material/bottomnavigation/BottomNavigationView;"
+                    && ref.parameterTypes.size == 1 && ref.parameterTypes[0] == "I"
+                    && ref.returnType == "Lcom/google/android/material/badge/BadgeDrawable;"
+        } ?: false
+    }
 )
