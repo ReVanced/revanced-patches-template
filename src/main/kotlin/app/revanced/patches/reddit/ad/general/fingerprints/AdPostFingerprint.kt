cab012ca664f823e1dd3809af4147ab15515a5ee
LagradOst <46196380+Blatzar@users.noreply.github.com>
2023-06-11 16:10:21 +0000

    remove unnessecary string for fingerprint

--- src/main/kotlin/app/revanced/patches/reddit/ad/general/fingerprints/AdPostFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/reddit/ad/general/fingerprints/AdPostFingerprint.kt
@@ -5,8 +5,6 @@ import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 object AdPostFingerprint : MethodFingerprint(
     "V",
     // "children" are present throughout multiple versions
-    // "uxExperiences" might not work on all versions
-    // Not sure how to refine the search better.
-    strings = listOf("children", "uxExperiences"),
+    strings = listOf("children"),
     customFingerprint = { methodDef, _ -> methodDef.definingClass.endsWith("Listing;") },
 )
