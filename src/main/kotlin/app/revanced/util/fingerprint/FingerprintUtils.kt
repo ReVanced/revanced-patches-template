2964d00950ef32fdaf1232a555e7b6eafed7e3c3
1fexd <PGFvy85nC@protonmail.com>
2023-06-11 17:27:58 +0200

    feat: Add hasInstruction util method

---
+++ src/main/kotlin/app/revanced/util/fingerprint/FingerprintUtils.kt
@@ -0,0 +1,21 @@
+package app.revanced.util.fingerprint
+
+import app.revanced.patcher.fingerprint.method.impl.CustomFingerprint
+import org.jf.dexlib2.Opcode
+import org.jf.dexlib2.iface.instruction.Instruction
+
+object FingerprintUtils {
+    inline fun <reified T : Instruction> hasInstruction(
+        opcode: Opcode,
+        crossinline predicate: (T) -> Boolean
+    ): CustomFingerprint {
+        return { methodDef, _ ->
+            methodDef.implementation?.instructions?.any {
+                if (it.opcode != opcode) return@any false
+                if (it !is T) return@any false
+
+                predicate(it)
+            } ?: false
+        }
+    }
+}
