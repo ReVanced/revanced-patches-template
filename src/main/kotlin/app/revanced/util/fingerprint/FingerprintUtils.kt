ba03e2db9dc68b849168e7182d20fbeb0ced2397
1fexd <PGFvy85nC@protonmail.com>
2023-06-11 18:16:46 +0200

    chore: Don't use CustomFingerprint typealias yet

--- src/main/kotlin/app/revanced/util/fingerprint/FingerprintUtils.kt
+++ src/main/kotlin/app/revanced/util/fingerprint/FingerprintUtils.kt
@@ -1,14 +1,16 @@
 package app.revanced.util.fingerprint
 
-import app.revanced.patcher.fingerprint.method.impl.CustomFingerprint
 import org.jf.dexlib2.Opcode
+import org.jf.dexlib2.iface.ClassDef
+import org.jf.dexlib2.iface.Method
 import org.jf.dexlib2.iface.instruction.Instruction
 
 object FingerprintUtils {
+    // TODO: Use CustomFingerprint typealias once https://github.com/revanced/revanced-patcher/pull/189 is merged
     inline fun <reified T : Instruction> hasInstruction(
         opcode: Opcode,
         crossinline predicate: (T) -> Boolean
-    ): CustomFingerprint {
+    ): ((methodDef: Method, classDef: ClassDef) -> Boolean) {
         return { methodDef, _ ->
             methodDef.implementation?.instructions?.any {
                 if (it.opcode != opcode) return@any false
