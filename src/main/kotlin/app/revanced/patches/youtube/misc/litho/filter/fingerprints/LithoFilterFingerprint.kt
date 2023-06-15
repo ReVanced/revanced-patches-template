c044b54b12935db3f76e85ef05348cbe6254f27b
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-06-15 11:24:25 +0400

    performance tweak: check access flags instead of method name

--- src/main/kotlin/app/revanced/patches/youtube/misc/litho/filter/fingerprints/LithoFilterFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/litho/filter/fingerprints/LithoFilterFingerprint.kt
@@ -1,11 +1,13 @@
 package app.revanced.patches.youtube.misc.litho.filter.fingerprints
 
+import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import org.jf.dexlib2.AccessFlags
 
 object LithoFilterFingerprint : MethodFingerprint(
-    customFingerprint = custom@{ method, classDef ->
-        if (method.name != "<clinit>") return@custom false
-
+    accessFlags = AccessFlags.PUBLIC or AccessFlags.STATIC or AccessFlags.CONSTRUCTOR,
+    returnType = "V",
+    customFingerprint = { _, classDef ->
         classDef.type.endsWith("LithoFilterPatch;")
     }
 )
