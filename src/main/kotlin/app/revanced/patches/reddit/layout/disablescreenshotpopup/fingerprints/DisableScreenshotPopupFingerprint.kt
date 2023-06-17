e7e2d88cc2be3837132483eda575c03157d8110c
Aunali321 <aunvakil.aa@gmail.com>
2023-06-18 05:04:23 +0530

    feat(disablescreenshotpopup): update fingerprint

--- src/main/kotlin/app/revanced/patches/reddit/layout/disablescreenshotpopup/fingerprints/DisableScreenshotPopupFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/reddit/layout/disablescreenshotpopup/fingerprints/DisableScreenshotPopupFingerprint.kt
@@ -1,21 +1,12 @@
 package app.revanced.patches.reddit.layout.disablescreenshotpopup.fingerprints
 
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-import org.jf.dexlib2.Opcode
 
 object DisableScreenshotPopupFingerprint : MethodFingerprint(
     "V",
-    opcodes = listOf(
-        Opcode.AND_INT_LIT8,
-        Opcode.CONST_4,
-        Opcode.IF_NE,
-        Opcode.INVOKE_INTERFACE_RANGE,
-        Opcode.MOVE_RESULT,
-        Opcode.IF_NEZ,
-        Opcode.GOTO,
-        Opcode.INVOKE_INTERFACE_RANGE,
-    ),
-    customFingerprint = { methodDef, _ ->
-        methodDef.name == "invoke"
+    parameters = listOf("Landroidx/compose/runtime/e;", "I"),
+    customFingerprint = { methodDef, classDef ->
+        classDef.type.endsWith("ScreenshotTakenBannerKt\$lambda-1\$1;") &&
+                methodDef.name == "invoke"
     }
 )
