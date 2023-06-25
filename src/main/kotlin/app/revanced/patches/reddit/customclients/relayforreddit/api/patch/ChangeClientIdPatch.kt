b7510e8a788f02f68f08f529430706430ddb745c
oSumAtrIX <johan.melkonyan1@web.de>
2023-06-25 05:02:07 +0200

    refactor: improve code

--- src/main/kotlin/app/revanced/patches/reddit/customclients/relayforreddit/api/patch/ChangeClientIdPatch.kt
+++ src/main/kotlin/app/revanced/patches/reddit/customclients/relayforreddit/api/patch/ChangeClientIdPatch.kt
@@ -1,34 +1,23 @@
 package app.revanced.patches.reddit.customclients.relayforreddit.api.patch
 
-import app.revanced.extensions.toErrorResult
-import app.revanced.patcher.annotation.*
-import app.revanced.patcher.data.BytecodeContext
-import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
+import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-import app.revanced.patcher.patch.OptionsContainer
-import app.revanced.patcher.patch.PatchOption
-import app.revanced.patcher.patch.PatchResultError
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprintResult
+import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
-import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patches.reddit.customclients.AbstractChangeOAuthClientIdPatch
+import app.revanced.patches.reddit.customclients.ChangeClientIdPatchAnnotation
 import app.revanced.patches.reddit.customclients.relayforreddit.api.fingerprints.GetClientIdFingerprint
 
-@Patch
-@Name("change-oauth-client-id")
-@Description("Changes the OAuth client ID.")
+@ChangeClientIdPatchAnnotation
 @Compatibility([Package("free.reddit.news"), Package("reddit.news")])
-@Version("0.0.1")
 class ChangeOAuthClientIdPatch : AbstractChangeOAuthClientIdPatch(
-    "dbrady://relay",
-    Options,
-    GetClientIdFingerprint
+    "dbrady://relay", Options, listOf(GetClientIdFingerprint)
 ) {
-    override fun MethodFingerprint.patch(context: BytecodeContext): PatchResult {
-        GetClientIdFingerprint.result?.mutableMethod?.replaceInstruction(0, "const-string v0, \"$clientId\"")
-            ?: return GetClientIdFingerprint.toErrorResult()
+    override fun List<MethodFingerprintResult>.patch(context: BytecodeContext): PatchResult {
+        first().mutableMethod.replaceInstruction(0, "const-string v0, \"$clientId\"")
 
         return PatchResultSuccess()
     }
