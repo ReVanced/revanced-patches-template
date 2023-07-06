1425e9187fd33a0345d1c2a91b08ae365cd1c2ba
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-07-06 18:19:25 +0400

    classname is present in 18.16, 18.19, and 18.23

--- src/main/kotlin/app/revanced/patches/youtube/layout/hide/player/overlay/bytecode/fingerprints/CreatePlayerOverviewFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hide/player/overlay/bytecode/fingerprints/CreatePlayerOverviewFingerprint.kt
@@ -1,12 +1,13 @@
 package app.revanced.patches.youtube.layout.hide.player.overlay.bytecode.fingerprints
 
+import app.revanced.extensions.containsConstantInstructionValue
 import app.revanced.patcher.extensions.or
-import app.revanced.util.patch.LiteralValueFingerprint
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import app.revanced.patches.youtube.layout.hide.player.overlay.resource.patch.HidePlayerOverlayResourcePatch
 import org.jf.dexlib2.AccessFlags
 import org.jf.dexlib2.Opcode
 
-object CreatePlayerOverviewFingerprint : LiteralValueFingerprint(
+object CreatePlayerOverviewFingerprint : MethodFingerprint(
     returnType = "V",
     accessFlags = AccessFlags.PRIVATE or AccessFlags.FINAL,
     opcodes = listOf(
@@ -15,5 +16,8 @@ object CreatePlayerOverviewFingerprint : LiteralValueFingerprint(
         Opcode.MOVE_RESULT_OBJECT,
         Opcode.CHECK_CAST
     ),
-    literal = HidePlayerOverlayResourcePatch.scrimOverlayId
+    customFingerprint = { methodDef, _ ->
+        methodDef.definingClass.endsWith("YouTubeControlsOverlay;")
+                &&  methodDef.containsConstantInstructionValue(HidePlayerOverlayResourcePatch.scrimOverlayId)
+    }
 )
