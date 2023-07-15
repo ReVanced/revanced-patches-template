77e8639b71048f2795f8f32fe18d052b335e3ce4
KAZI MMT <82371061+kazimmt@users.noreply.github.com>
2023-07-15 18:32:39 +0600

    feat(youtube): rename `video-speed` to `playback-speed` (#2642)

--- src/main/kotlin/app/revanced/patches/youtube/video/speed/custom/fingerprints/ShowOldVideoSpeedMenuFingerprint.kt
+++
@@ -1,7 +0,0 @@
-package app.revanced.patches.youtube.video.speed.custom.fingerprints
-
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-
-object ShowOldVideoSpeedMenuFingerprint : MethodFingerprint(
-    strings = listOf("PLAYBACK_RATE_MENU_BOTTOM_SHEET_FRAGMENT")
-)
