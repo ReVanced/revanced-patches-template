9546d126430870d1abd8f43bb687b31b9fcb6fb5
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-07-20 03:17:47 +0400

    fix(YouTube - Spoof client): show video time and chapters while using seekbar (#2607)

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/UserAgentHeaderBuilderFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/UserAgentHeaderBuilderFingerprint.kt
@@ -1,6 +1,5 @@
 package app.revanced.patches.youtube.misc.fix.playback.fingerprints
 
-
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import org.jf.dexlib2.Opcode
 
--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/patch/SpoofSignatureVerificationPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/patch/SpoofSignatureVerificationPatch.kt
@@ -7,47 +7,35 @@ import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.data.toMethodWalker
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint.Companion.resolve
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
-import app.revanced.patches.shared.settings.preference.impl.StringResource
-import app.revanced.patches.shared.settings.preference.impl.SwitchPreference
-import app.revanced.patches.youtube.misc.fix.playback.fingerprints.ProtobufParameterBuilderFingerprint
+import app.revanced.patches.youtube.misc.fix.playback.fingerprints.*
 import app.revanced.patches.youtube.misc.integrations.patch.IntegrationsPatch
 import app.revanced.patches.youtube.misc.playertype.patch.PlayerTypeHookPatch
-import app.revanced.patches.youtube.misc.settings.bytecode.patch.SettingsPatch
+import org.jf.dexlib2.iface.instruction.ReferenceInstruction
 
 @Name("Spoof signature verification")
 @Description("Spoofs a patched client to prevent playback issues.")
 @DependsOn([
+    SpoofSignatureVerificationResourcePatch::class,
     IntegrationsPatch::class,
-    SettingsPatch::class,
-    PlayerTypeHookPatch::class,
+    PlayerTypeHookPatch::class
 ])
 @Version("0.0.1")
 class SpoofSignatureVerificationPatch : BytecodePatch(
     listOf(
         ProtobufParameterBuilderFingerprint,
+        StoryboardThumbnailParentFingerprint,
+        ScrubbedPreviewLayoutFingerprint,
     )
 ) {
     override fun execute(context: BytecodeContext): PatchResult {
-        SettingsPatch.PreferenceScreen.MISC.addPreferences(
-            SwitchPreference(
-                "revanced_spoof_signature_verification",
-                StringResource("revanced_spoof_signature_verification_title", "Spoof app signature"),
-                StringResource("revanced_spoof_signature_verification_summary_on",
-                    "App signature spoofed\\n\\n"
-                        + "Side effects include:\\n"
-                        + "• Ambient mode may not work\\n"
-                        + "• Seekbar thumbnails are hidden\\n"
-                        + "• Downloading videos may not work"),
-                StringResource("revanced_spoof_signature_verification_summary_off", "App signature not spoofed"),
-                StringResource("revanced_spoof_signature_verification_user_dialog_message",
-                    "Turning off this setting may cause playback issues.")
-            )
-        )
 
         // hook parameter
         ProtobufParameterBuilderFingerprint.result?.let {
@@ -67,6 +55,53 @@ class SpoofSignatureVerificationPatch : BytecodePatch(
                 )
             }
         } ?: return ProtobufParameterBuilderFingerprint.toErrorResult()
+
+
+        // When signature spoofing is enabled, the seekbar when tapped does not show
+        // the video time, chapter names, or the video thumbnail.
+        // Changing the value returned of this method forces all of these to show up,
+        // except the thumbnails are blank, which is handled with the patch below.
+        StoryboardThumbnailParentFingerprint.result ?: return StoryboardThumbnailParentFingerprint.toErrorResult()
+        StoryboardThumbnailFingerprint.resolve(context, StoryboardThumbnailParentFingerprint.result!!.classDef)
+        StoryboardThumbnailFingerprint.result?.apply {
+            val endIndex = scanResult.patternScanResult!!.endIndex
+            // Replace existing instruction to preserve control flow label.
+            // The replaced return instruction always returns false
+            // (it is the 'no thumbnails found' control path),
+            // so there is no need to pass the existing return value to integrations.
+            mutableMethod.replaceInstruction(
+                endIndex,
+                """
+                    invoke-static {}, $INTEGRATIONS_CLASS_DESCRIPTOR->getSeekbarThumbnailOverrideValue()Z
+                """
+            )
+            // Since this is end of the method must replace one line then add the rest.
+            mutableMethod.addInstructions(
+                endIndex + 1,
+                """
+                move-result v0
+                return v0
+            """
+            )
+        } ?: return StoryboardThumbnailFingerprint.toErrorResult()
+
+
+        // Seekbar thumbnail now show up but are always a blank image.
+        // Additional changes are needed to force the client to generate the thumbnails (assuming it's possible),
+        // but for now hide the empty thumbnail.
+        ScrubbedPreviewLayoutFingerprint.result?.apply {
+            val endIndex = scanResult.patternScanResult!!.endIndex
+            mutableMethod.apply {
+                val imageViewFieldName = getInstruction<ReferenceInstruction>(endIndex).reference
+                addInstructions(
+                    implementation!!.instructions.lastIndex,
+                    """
+                        iget-object v0, p0, $imageViewFieldName   # copy imageview field to a register
+                        invoke-static {v0}, $INTEGRATIONS_CLASS_DESCRIPTOR->seekbarImageViewCreated(Landroid/widget/ImageView;)V
+                """
+                )
+            }
+        } ?: return ScrubbedPreviewLayoutFingerprint.toErrorResult()
 
         return PatchResultSuccess()
     }
