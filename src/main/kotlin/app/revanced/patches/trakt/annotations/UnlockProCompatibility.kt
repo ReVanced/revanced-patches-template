324bbde92a851e855c11f266e92fa14c39d88160
johnconner122 <107796137+johnconner122@users.noreply.github.com>
2023-07-20 03:50:16 +0500

    fix(Trakt - Unlock pro): constraint to last known working version (#2662)

--- src/main/kotlin/app/revanced/patches/trakt/annotations/UnlockProCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/trakt/annotations/UnlockProCompatibility.kt
@@ -3,6 +3,6 @@ package app.revanced.patches.trakt.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility([Package("tv.trakt.trakt")])
+@Compatibility([Package("tv.trakt.trakt", arrayOf("1.1.1"))])
 @Target(AnnotationTarget.CLASS)
 internal annotation class UnlockProCompatibility
