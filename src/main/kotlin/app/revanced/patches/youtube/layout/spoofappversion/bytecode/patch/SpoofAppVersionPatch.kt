0135a410326ab6a037d108a10a5781adb3110429
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-07-20 22:41:20 +0400

    adding some kind of description

--- src/main/kotlin/app/revanced/patches/youtube/layout/spoofappversion/bytecode/patch/SpoofAppVersionPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/spoofappversion/bytecode/patch/SpoofAppVersionPatch.kt
@@ -54,7 +54,7 @@ class SpoofAppVersionPatch : BytecodePatch(
                     "revanced_spoof_app_version_target_entries",
                     listOf(
                         StringResource("revanced_spoof_app_version_target_entry_1", "17.30.35 - Restore old UI layout"),
-                        StringResource("revanced_spoof_app_version_target_entry_2", "17.01.35"),
+                        StringResource("revanced_spoof_app_version_target_entry_2", "17.01.35 - Restore old watch history"),
                         StringResource("revanced_spoof_app_version_target_entry_3", "16.08.35 - Restore explore tab"),
                         StringResource("revanced_spoof_app_version_target_entry_4", "16.01.35 - Restore old Shorts player"),
                     )
