055d55c9c110ad6163d086eb80095953cbef3ce1
oSumAtrIX <johan.melkonyan1@web.de>
2023-08-02 01:35:40 +0200

    feat(YouTube): Support version `18.29.38`

--- src/main/kotlin/app/revanced/patches/youtube/interaction/seekbar/patch/EnableSeekbarTappingPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/interaction/seekbar/patch/EnableSeekbarTappingPatch.kt
@@ -13,13 +13,15 @@ import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patcher.util.smali.ExternalLabel
 import app.revanced.patches.youtube.interaction.seekbar.annotation.SeekbarTappingCompatibility
-import app.revanced.patches.youtube.interaction.seekbar.fingerprints.AccessibilityPlayerProgressTimeFingerprint
+import app.revanced.patches.youtube.interaction.seekbar.fingerprints.OnTouchEventHandlerFingerprint
 import app.revanced.patches.youtube.interaction.seekbar.fingerprints.SeekbarTappingFingerprint
 import app.revanced.patches.youtube.misc.integrations.patch.IntegrationsPatch
 import com.android.tools.smali.dexlib2.Opcode
 import com.android.tools.smali.dexlib2.iface.Method
 import com.android.tools.smali.dexlib2.iface.instruction.NarrowLiteralInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.formats.Instruction35c
+import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
+import com.android.tools.smali.dexlib2.iface.reference.MethodReference
 
 @Patch
 @DependsOn([IntegrationsPatch::class, EnableSeekbarTappingResourcePatch::class])
@@ -27,37 +29,23 @@ import com.android.tools.smali.dexlib2.iface.instruction.formats.Instruction35c
 @Description("Enables tap-to-seek on the seekbar of the video player.")
 @SeekbarTappingCompatibility
 class EnableSeekbarTappingPatch : BytecodePatch(
-    listOf(AccessibilityPlayerProgressTimeFingerprint, SeekbarTappingFingerprint)
+    listOf(OnTouchEventHandlerFingerprint, SeekbarTappingFingerprint)
 ) {
     override fun execute(context: BytecodeContext): PatchResult {
         // Find the required methods to tap the seekbar.
-        val seekbarTappingMethods =
-            AccessibilityPlayerProgressTimeFingerprint.result?.classDef?.methods?.let { methods ->
-                buildMap {
-                    // find the methods which tap the seekbar
-                    methods.forEach { method ->
-                        if (method.implementation == null) return@forEach
+        val seekbarTappingMethods = OnTouchEventHandlerFingerprint.result?.let {
+            val patternScanResult = it.scanResult.patternScanResult!!
 
-                        val instructions = method.implementation!!.instructions
+            fun getReference(index: Int) = it.mutableMethod.getInstruction<ReferenceInstruction>(index)
+                .reference as MethodReference
 
-                        // The method has more than 7 instructions.
-                        if (instructions.count() < 7) return@forEach
-
-                        // The 7th instruction has the opcode CONST_4.
-                        val instruction = instructions.elementAt(6)
-                        if (instruction.opcode != Opcode.CONST_4) return@forEach
-
-                        // the literal for this instruction has to be either 1 or 2.
-                        val literal = (instruction as NarrowLiteralInstruction).narrowLiteral
-
-                        // Based on the literal, determine which method is which.
-                        if (literal == 1) this["P"] = method
-                        if (literal == 2) this["O"] = method
-                    }
-                }
+            buildMap {
+                put("O", getReference(patternScanResult.endIndex))
+                put("N", getReference(patternScanResult.startIndex))
             }
+        }
 
-        seekbarTappingMethods ?: return AccessibilityPlayerProgressTimeFingerprint.toErrorResult()
+        seekbarTappingMethods ?: return OnTouchEventHandlerFingerprint.toErrorResult()
 
         SeekbarTappingFingerprint.result?.let {
             val insertIndex = it.scanResult.patternScanResult!!.endIndex - 1
@@ -68,11 +56,11 @@ class EnableSeekbarTappingPatch : BytecodePatch(
                 val freeRegister = 0
                 val xAxisRegister = 2
 
-                val pMethod = seekbarTappingMethods["P"]!!
                 val oMethod = seekbarTappingMethods["O"]!!
+                val nMethod = seekbarTappingMethods["N"]!!
 
-                fun Method.toInvokeInstructionString() =
-                    "invoke-virtual { v$thisInstanceRegister, v$xAxisRegister }, $definingClass->$name(I)V"
+                fun MethodReference.toInvokeInstructionString() =
+                    "invoke-virtual { v$thisInstanceRegister, v$xAxisRegister }, $this"
 
                 addInstructionsWithLabels(
                     insertIndex,
@@ -81,7 +69,7 @@ class EnableSeekbarTappingPatch : BytecodePatch(
                         move-result v$freeRegister
                         if-eqz v$freeRegister, :disabled
                         ${oMethod.toInvokeInstructionString()}
-                        ${pMethod.toInvokeInstructionString()}
+                        ${nMethod.toInvokeInstructionString()}
                     """,
                     ExternalLabel("disabled", getInstruction(insertIndex))
                 )
--- src/main/kotlin/app/revanced/patches/youtube/layout/hide/loadmorebutton/bytecode/fingerprints/HideLoadMoreButtonFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hide/loadmorebutton/bytecode/fingerprints/HideLoadMoreButtonFingerprint.kt
@@ -1,15 +1,10 @@
 package app.revanced.patches.youtube.layout.hide.loadmorebutton.bytecode.fingerprints
 
-import app.revanced.patcher.extensions.or
-import app.revanced.util.patch.LiteralValueFingerprint
 import app.revanced.patches.youtube.layout.hide.loadmorebutton.resource.patch.HideLoadMoreButtonResourcePatch
-import com.android.tools.smali.dexlib2.AccessFlags
+import app.revanced.util.patch.LiteralValueFingerprint
 import com.android.tools.smali.dexlib2.Opcode
 
 object HideLoadMoreButtonFingerprint : LiteralValueFingerprint(
-    returnType = "V",
-    accessFlags = AccessFlags.PUBLIC or AccessFlags.CONSTRUCTOR,
-    parameters = listOf("L", "L", "L", "L"),
     opcodes = listOf(
         Opcode.CONST,
         Opcode.CONST_4,
