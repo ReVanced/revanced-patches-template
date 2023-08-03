f31e6fc87b9a058c0983854432304109ef73dd4c
oSumAtrIX <johan.melkonyan1@web.de>
2023-08-03 15:47:23 +0200

    refactor

--- src/main/kotlin/app/revanced/patches/lightroom/misc/login/annotations/BypassLoginCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/lightroom/misc/login/annotations/BypassLoginCompatibility.kt
@@ -3,10 +3,6 @@ package app.revanced.patches.lightroom.misc.login.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility(
-    [Package(
-        "com.adobe.lrmobile",
-    )]
-)
+@Compatibility([Package("com.adobe.lrmobile",)])
 @Target(AnnotationTarget.CLASS)
 internal annotation class BypassLoginCompatibility
