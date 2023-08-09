d8e8943e40866a12942753cb7b954673344e8db3
Aunali321 <aunvakil.aa@gmail.com>
2023-08-09 22:38:18 +0530

    refactor: code cleanup

--- src/main/kotlin/app/revanced/patches/all/misc/mitmproxy/patch/MitmProxyPatch.kt
+++ src/main/kotlin/app/revanced/patches/all/misc/mitmproxy/patch/MitmProxyPatch.kt
@@ -13,39 +13,16 @@ import java.io.File
 
 @Patch(false)
 @Name("Mitm proxy")
-@Description("Mitmproxy to inspect HTTPS traffic.")
+@Description("Mitm proxy to inspect HTTPS traffic.")
 @Version("0.0.1")
 @DependsOn([EnableAndroidDebuggingPatch::class])
 class MitmProxyPatch : ResourcePatch {
 
-//    steps required to achieve this:
-    // 1. check if "networkSecurityConfig" attribute exists in the application node
-    // 2. if it does, open the "network_security_config.xml" file and <certificates src="user" /> to the trust-anchors node
-    // 3. if it does not, Add android:networkSecurityConfig="@xml/network_security_config" to the <application> element in your application manifest.
-    // 4. create the "network_security_config.xml" file in the res/xml directory with the following contents:
-    //<?xml version="1.0" encoding="utf-8"?>
-    //<network-security-config>
-    //    <base-config cleartextTrafficPermitted="true">
-    //        <trust-anchors>
-    //            <certificates src="system" />
-    //            <certificates
-    //                src="user"
-    //                overridePins="true" />
-    //        </trust-anchors>
-    //    </base-config>
-    //    <debug-overrides>
-    //        <trust-anchors>
-    //            <certificates src="system" />
-    //            <certificates
-    //                src="user"
-    //                overridePins="true" />
-    //        </trust-anchors>
-    //    </debug-overrides>
-    //</network-security-config>
-
-
     override fun execute(context: ResourceContext): PatchResult {
         val resXmlDirectory = context["res/xml"]
+
+        // Check if "networkSecurityConfig" attribute exists in the application node
+        // If it does not, Add android:networkSecurityConfig="@xml/network_security_config" to the <application> element in your application manifest.
 
         context.xmlEditor["AndroidManifest.xml"].use { editor ->
             val document = editor.file
@@ -53,7 +30,6 @@ class MitmProxyPatch : ResourcePatch {
                 .getElementsByTagName("application")
                 .item(0) as Element
 
-            // Add `android:networkSecurityConfig` attribute to the application node with the value of "@xml/network_security_config"
             if (!applicationNode.hasAttribute("networkSecurityConfig")) {
                 document.createAttribute("android:networkSecurityConfig")
                     .apply { value = "@xml/network_security_config" }
@@ -62,7 +38,7 @@ class MitmProxyPatch : ResourcePatch {
 
         }
 
-        // Check and create the "network_security_config.xml" file in the res/xml directory with the following contents:
+        // Create the "network_security_config.xml" file in the res/xml directory with the following contents:
         val networkSecurityConfigFile = File(resXmlDirectory, "network_security_config.xml")
 
         if (!networkSecurityConfigFile.exists()) {
@@ -91,7 +67,8 @@ class MitmProxyPatch : ResourcePatch {
                 """.trimIndent()
             )
         } else {
-            // If the file already exists, open the "network_security_config.xml" file and <certificates src="system" /> and <certificates src="user" overridePins="true" /> to the trust-anchors node
+
+            // If the file already exists, open the "network_security_config.xml" file and add <certificates src="system" /> and <certificates src="user" overridePins="true" /> to the trust-anchors node
             networkSecurityConfigFile.readText().let { fileContents ->
                 if (!fileContents.contains("<certificates src=\"user\" />")) {
                     networkSecurityConfigFile.writeText(
