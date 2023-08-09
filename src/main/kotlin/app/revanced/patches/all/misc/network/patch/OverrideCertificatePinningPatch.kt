38de2cce752f63c2df6e282433d232d7cae22043
Aunali321 <aunvakil.aa@gmail.com>
2023-08-09 23:57:06 +0530

    refactor: apply changes from code review

--- src/main/kotlin/app/revanced/patches/all/misc/network/patch/OverrideCertificatePinningPatch.kt
+++ src/main/kotlin/app/revanced/patches/all/misc/network/patch/OverrideCertificatePinningPatch.kt
@@ -1,4 +1,4 @@
-package app.revanced.patches.all.misc.mitmproxy.patch
+package app.revanced.patches.all.misc.network.patch
 
 import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
@@ -12,18 +12,16 @@ import org.w3c.dom.Element
 import java.io.File
 
 @Patch(false)
-@Name("Mitm proxy")
+@Name("Override certificate pinning")
 @Description("Mitm proxy to inspect HTTPS traffic.")
 @Version("0.0.1")
 @DependsOn([EnableAndroidDebuggingPatch::class])
-class MitmProxyPatch : ResourcePatch {
+class OverrideCertificatePinningPatch : ResourcePatch {
 
     override fun execute(context: ResourceContext): PatchResult {
         val resXmlDirectory = context["res/xml"]
 
-        // Check if "networkSecurityConfig" attribute exists in the application node
-        // If it does not, Add android:networkSecurityConfig="@xml/network_security_config" to the <application> element in your application manifest.
-
+        // Add android:networkSecurityConfig="@xml/network_security_config" and the "networkSecurityConfig" attribute if it does not exist.
         context.xmlEditor["AndroidManifest.xml"].use { editor ->
             val document = editor.file
             val applicationNode = document
@@ -38,7 +36,7 @@ class MitmProxyPatch : ResourcePatch {
 
         }
 
-        // Create the "network_security_config.xml" file in the res/xml directory with the following contents:
+//        In case the file does not exist create the "network_security_config.xml" file.
         val networkSecurityConfigFile = File(resXmlDirectory, "network_security_config.xml")
 
         if (!networkSecurityConfigFile.exists()) {
@@ -64,11 +62,11 @@ class MitmProxyPatch : ResourcePatch {
                             </trust-anchors>
                         </debug-overrides>
                     </network-security-config>
-                """.trimIndent()
+                """
             )
         } else {
 
-            // If the file already exists, open the "network_security_config.xml" file and add <certificates src="system" /> and <certificates src="user" overridePins="true" /> to the trust-anchors node
+            // If the file already exists.
             networkSecurityConfigFile.readText().let { fileContents ->
                 if (!fileContents.contains("<certificates src=\"user\" />")) {
                     networkSecurityConfigFile.writeText(
