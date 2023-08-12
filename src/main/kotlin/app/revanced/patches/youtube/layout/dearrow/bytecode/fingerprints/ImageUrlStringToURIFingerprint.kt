cfcc309d07ecd1d83d572812470d3e940dd15838
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-08-13 01:21:34 +0400

    alternate patch point, works everywhere (including playlists), BUT if user turns on/off it continues to use the old cached thumbnails.

--- src/main/kotlin/app/revanced/patches/youtube/layout/dearrow/bytecode/fingerprints/ImageUrlStringToURIFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/dearrow/bytecode/fingerprints/ImageUrlStringToURIFingerprint.kt
@@ -6,16 +6,21 @@ import org.jf.dexlib2.AccessFlags
 import org.jf.dexlib2.Opcode
 
 object ImageUrlStringToURIFingerprint : MethodFingerprint(
-    returnType = "Landroid/net/Uri;",
-    accessFlags = AccessFlags.PUBLIC or AccessFlags.STATIC,
-    parameters =  listOf("Ljava/lang/String;"),
-    opcodes = listOf(
-        Opcode.CONST_STRING,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.RETURN_OBJECT,
-    ),
-    strings = listOf("https"),
+//    returnType = "Landroid/net/Uri;",
+    accessFlags = AccessFlags.PUBLIC or AccessFlags.CONSTRUCTOR,
+    parameters =  listOf("Lorg/chromium/net/impl/CronetUrlRequestContext;", "Ljava/lang/String;", "I",
+        "Lorg/chromium/net/UrlRequest\$Callback;", "Ljava/util/concurrent/Executor;", "Ljava/util/Collection;",
+        "Z", "Z", "Z", "Z", "I", "Z", "I", "Lorg/chromium/net/RequestFinishedInfo\$Listener;", "I", "J"),
+//    opcodes = listOf(
+//        Opcode.CONST_STRING,
+//        Opcode.INVOKE_VIRTUAL,
+//        Opcode.MOVE_RESULT_OBJECT,
+//        Opcode.INVOKE_VIRTUAL,
+//        Opcode.MOVE_RESULT_OBJECT,
+//        Opcode.RETURN_OBJECT,
+//    ),
+//    strings = listOf("https"),
+    customFingerprint = { methodDef, _ ->
+        methodDef.definingClass == "Lorg/chromium/net/impl/CronetUrlRequest;"
+    }
 )
