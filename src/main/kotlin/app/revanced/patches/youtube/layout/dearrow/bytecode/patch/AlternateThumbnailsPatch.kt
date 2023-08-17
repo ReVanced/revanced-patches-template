1c66167a917a6ac37ff4ca7f4eaaf1649c75902d
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-08-17 16:00:54 +0400

    Update src/main/kotlin/app/revanced/patches/youtube/layout/dearrow/bytecode/patch/AlternateThumbnailsPatch.kt
    Co-authored-by: oSumAtrIX <johan.melkonyan1@web.de>

--- src/main/kotlin/app/revanced/patches/youtube/layout/dearrow/bytecode/patch/AlternateThumbnailsPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/dearrow/bytecode/patch/AlternateThumbnailsPatch.kt
@@ -32,7 +32,7 @@ import app.revanced.patches.youtube.misc.settings.bytecode.patch.SettingsPatch
 @DependsOn([IntegrationsPatch::class, SettingsPatch::class])
 @Name("Alternate thumbnails")
 @AlternativeThumbnailsCompatibility
-@Description("Adds an option to replace video thumbnails with still image captures of the video")
+@Description("Adds an option to replace video thumbnails with still image captures of the video.")
 class AlternateThumbnailsPatch : BytecodePatch(
     listOf(MessageDigestImageUrlParentFingerprint, CronetURLRequestCallbackOnResponseStartedFingerprint)
 ) {
