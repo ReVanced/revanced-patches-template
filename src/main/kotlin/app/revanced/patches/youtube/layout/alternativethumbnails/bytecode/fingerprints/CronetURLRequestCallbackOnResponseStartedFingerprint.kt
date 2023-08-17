e5a1d74d02298b07795a93678697e95dbfe90479
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-08-17 19:23:01 +0400

    Fixing merge

--- src/main/kotlin/app/revanced/patches/youtube/layout/alternativethumbnails/bytecode/fingerprints/CronetURLRequestCallbackOnResponseStartedFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/alternativethumbnails/bytecode/fingerprints/CronetURLRequestCallbackOnResponseStartedFingerprint.kt
@@ -2,7 +2,7 @@ package app.revanced.patches.youtube.layout.alternativethumbnails.bytecode.finge
 
 import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-import org.jf.dexlib2.AccessFlags
+import com.android.tools.smali.dexlib2.AccessFlags
 
 // Acts as a parent fingerprint.
 object CronetURLRequestCallbackOnResponseStartedFingerprint : MethodFingerprint(
