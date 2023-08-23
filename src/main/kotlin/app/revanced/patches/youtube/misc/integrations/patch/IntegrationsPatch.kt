fab6e9fe85697bc845a8f3e0a1324ead00fc38ce
oSumAtrIX <johan.melkonyan1@web.de>
2023-08-23 15:23:55 +0200

    refactor: remove unnecessary annotations

--- src/main/kotlin/app/revanced/patches/youtube/misc/integrations/patch/IntegrationsPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/integrations/patch/IntegrationsPatch.kt
@@ -3,17 +3,9 @@ package app.revanced.patches.youtube.misc.integrations.patch
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.patch.annotations.RequiresIntegrations
 import app.revanced.patches.shared.integrations.patch.AbstractIntegrationsPatch
-import app.revanced.patches.youtube.misc.integrations.annotations.IntegrationsCompatibility
-import app.revanced.patches.youtube.misc.integrations.fingerprints.EmbeddedPlayerControlsOverlayFingerprint
-import app.revanced.patches.youtube.misc.integrations.fingerprints.EmbeddedPlayerFingerprint
-import app.revanced.patches.youtube.misc.integrations.fingerprints.ApplicationInitFingerprint
-import app.revanced.patches.youtube.misc.integrations.fingerprints.RemoteEmbedFragmentFingerprint
-import app.revanced.patches.youtube.misc.integrations.fingerprints.RemoteEmbeddedPlayerFingerprint
-import app.revanced.patches.youtube.misc.integrations.fingerprints.APIPlayerServiceFingerprint
-import app.revanced.patches.youtube.misc.integrations.fingerprints.StandalonePlayerActivityFingerprint
+import app.revanced.patches.youtube.misc.integrations.fingerprints.*
 
 @Name("Integrations")
-@IntegrationsCompatibility
 @RequiresIntegrations
 class IntegrationsPatch : AbstractIntegrationsPatch(
     "Lapp/revanced/integrations/utils/ReVancedUtils;",
