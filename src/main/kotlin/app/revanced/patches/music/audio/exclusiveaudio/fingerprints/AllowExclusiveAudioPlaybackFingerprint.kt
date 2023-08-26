0861991cfbdfa977713f1f1f4ee8a0a5fd3f939e
oSumAtrIX <johan.melkonyan1@web.de>
2023-08-26 16:21:58 +0200

    feat(YouTube Music - Exclusive audio playback): Support latest version

--- src/main/kotlin/app/revanced/patches/music/audio/exclusiveaudio/fingerprints/AllowExclusiveAudioPlaybackFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/music/audio/exclusiveaudio/fingerprints/AllowExclusiveAudioPlaybackFingerprint.kt
@@ -5,14 +5,11 @@ import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
-object  AudioOnlyEnablerFingerprint: MethodFingerprint(
-    "Z", AccessFlags.PUBLIC or AccessFlags.FINAL, listOf(), listOf(
-        Opcode.IGET_OBJECT,
-        Opcode.INVOKE_INTERFACE,
-        Opcode.MOVE_RESULT_OBJECT,
-        Opcode.IGET_OBJECT,
-        Opcode.INVOKE_INTERFACE,
-        Opcode.MOVE_RESULT_OBJECT,
+object AllowExclusiveAudioPlaybackFingerprint: MethodFingerprint(
+    "Z",
+    AccessFlags.PUBLIC or AccessFlags.FINAL,
+    listOf(),
+    listOf(
         Opcode.INVOKE_VIRTUAL,
         Opcode.MOVE_RESULT_OBJECT,
         Opcode.CHECK_CAST,
