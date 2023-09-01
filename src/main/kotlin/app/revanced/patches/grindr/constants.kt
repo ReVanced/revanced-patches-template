21a134fb8c0703b5d5b26fc09fe14f4137ac4fcd
Joar Heimonen <joarheimonen@live.no>
2023-09-01 12:23:41 +0200

    Removed unused constants

--- src/main/kotlin/app/revanced/patches/grindr/constants.kt
+++ src/main/kotlin/app/revanced/patches/grindr/constants.kt
@@ -1,11 +1,6 @@
 package app.revanced.patches.grindr
 
 internal object Constants {
-    const val REVANCED_APP_NAME = "grindrReVanced"
-    const val REVANCED_PACKAGE_NAME = "com.grindrrevanced.android"
     const val PACKAGE_NAME = "com.grindrapp.android"
-    const val SPOOFED_PACKAGE_NAME = PACKAGE_NAME
     const val SPOOFED_PACKAGE_SIGNATURE = "823F5A17C33B16B4775480B31607E7DF35D67AF8"
-    //old 92A99370F1ABE83DFDA3F36D4E564428B27738FC
-    //new 823F5A17C33B16B4775480B31607E7DF35D67AF8
 }
