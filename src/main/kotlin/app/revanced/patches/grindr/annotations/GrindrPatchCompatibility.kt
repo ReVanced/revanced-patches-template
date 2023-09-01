685e88c57a8a9f4dda7ef060c0fefbe107e9af0d
Joar Heimonen <joarheimonen@live.no>
2023-09-01 12:13:29 +0200

    added version

--- src/main/kotlin/app/revanced/patches/grindr/annotations/GrindrPatchCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/grindr/annotations/GrindrPatchCompatibility.kt
@@ -3,6 +3,6 @@ package app.revanced.patches.grindr.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility([Package("com.grindrapp.android")])
+@Compatibility([Package("com.grindrapp.android", arrayOf("9.15.0"))])
 @Target(AnnotationTarget.CLASS)
 internal annotation class GrindrPatchCompatibility
