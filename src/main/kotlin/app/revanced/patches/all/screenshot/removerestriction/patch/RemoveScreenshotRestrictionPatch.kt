a9bdbdde1f6c0c7b5a8da44af5ff634e7caaea4d
Linus789 <23507341+Linus789@users.noreply.github.com>
2023-09-05 09:22:17 +0200

    fix(Remove screenshot restriction): higher reliability

--- src/main/kotlin/app/revanced/patches/all/screenshot/removerestriction/patch/RemoveScreenshotRestrictionPatch.kt
+++ src/main/kotlin/app/revanced/patches/all/screenshot/removerestriction/patch/RemoveScreenshotRestrictionPatch.kt
@@ -2,6 +2,8 @@ package app.revanced.patches.all.screenshot.removerestriction.patch
 
 import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.annotations.Patch
 import app.revanced.patcher.patch.annotations.RequiresIntegrations
 import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
@@ -9,9 +11,12 @@ import app.revanced.util.patch.AbstractTransformInstructionsPatch
 import app.revanced.util.patch.IMethodCall
 import app.revanced.util.patch.Instruction35cInfo
 import app.revanced.util.patch.filterMapInstruction35c
+import com.android.tools.smali.dexlib2.Opcode
 import com.android.tools.smali.dexlib2.iface.ClassDef
 import com.android.tools.smali.dexlib2.iface.Method
 import com.android.tools.smali.dexlib2.iface.instruction.Instruction
+import com.android.tools.smali.dexlib2.iface.instruction.formats.Instruction22c
+import com.android.tools.smali.dexlib2.iface.reference.FieldReference
 
 @Patch(false)
 @Name("Remove screenshot restriction")
@@ -32,12 +37,23 @@ class RemoveScreenshotRestrictionPatch : AbstractTransformInstructionsPatch<Inst
         override val methodParams: Array<String>,
         override val returnType: String
     ): IMethodCall {
+        AddFlags(
+            "Landroid/view/Window;",
+            "addFlags",
+            arrayOf("I"),
+            "V",
+        ),
         SetFlags(
             "Landroid/view/Window;",
             "setFlags",
             arrayOf("I", "I"),
             "V",
         );
+    }
+
+    override fun execute(context: BytecodeContext) {
+        super.execute(context)
+        ModifyLayoutParamsFlags().execute(context)
     }
 
     override fun filterMap(
@@ -55,5 +71,39 @@ class RemoveScreenshotRestrictionPatch : AbstractTransformInstructionsPatch<Inst
     override fun transform(mutableMethod: MutableMethod, entry: Instruction35cInfo) {
         val (methodType, instruction, instructionIndex) = entry
         methodType.replaceInvokeVirtualWithIntegrations(INTEGRATIONS_CLASS_DESCRIPTOR, mutableMethod, instruction, instructionIndex)
+    }
+}
+
+private class ModifyLayoutParamsFlags : AbstractTransformInstructionsPatch<Pair<Instruction22c, Int>>() {
+    override fun filterMap(
+        classDef: ClassDef,
+        method: Method,
+        instruction: Instruction,
+        instructionIndex: Int
+    ): Pair<Instruction22c, Int>? {
+        if (instruction.opcode != Opcode.IPUT) {
+            return null
+        }
+
+        val instruction22c = instruction as Instruction22c
+        val fieldReference = instruction22c.reference as FieldReference
+
+        if (fieldReference.definingClass != "Landroid/view/WindowManager\$LayoutParams;"
+            || fieldReference.name != "flags"
+            || fieldReference.type != "I") {
+            return null
+        }
+
+        return Pair(instruction22c, instructionIndex)
+    }
+
+    override fun transform(mutableMethod: MutableMethod, entry: Pair<Instruction22c, Int>) {
+        val (instruction, index) = entry
+        val register = instruction.registerA
+
+        mutableMethod.addInstructions(
+            index,
+            "and-int/lit16 v$register, v$register, -0x2001"
+        )
     }
 }
