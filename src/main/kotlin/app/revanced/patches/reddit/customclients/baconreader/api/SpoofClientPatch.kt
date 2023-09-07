43cbba00bbbc8ae9a465eebda51a2839c7bf8652
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-07 23:10:34 +0200

    bacon client spoof

--- src/main/kotlin/app/revanced/patches/reddit/customclients/baconreader/api/SpoofClientPatch.kt
+++ src/main/kotlin/app/revanced/patches/reddit/customclients/baconreader/api/SpoofClientPatch.kt
@@ -1,32 +1,35 @@
-package app.revanced.patches.reddit.customclients.baconreader.api.patch
+package app.revanced.patches.reddit.customclients.baconreader.api
 
-import app.revanced.patcher.annotation.Compatibility
-import app.revanced.patcher.annotation.Description
-import app.revanced.patcher.annotation.Package
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprintResult
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.reddit.customclients.AbstractSpoofClientPatch
-import app.revanced.patches.reddit.customclients.SpoofClientAnnotation
 import app.revanced.patches.reddit.customclients.baconreader.api.fingerprints.GetAuthorizationUrlFingerprint
 import app.revanced.patches.reddit.customclients.baconreader.api.fingerprints.RequestTokenFingerprint
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 
 
-@SpoofClientAnnotation
-@Description("Spoofs the client in order to allow logging in. " +
-        "The OAuth application type has to be \"Installed app\" " +
-        "and the redirect URI has to be set to \"http://baconreader.com/auth\".")
-@Compatibility(
-    [
-        Package("com.onelouder.baconreader"),
-        Package("com.onelouder.baconreader.premium")
+@Patch(
+    name = "Spoof client",
+    description = "Spoofs the client in order to allow logging in. " +
+            "The OAuth application type has to be \"Installed app\" " +
+            "and the redirect URI has to be set to \"http://baconreader.com/auth\".",
+    compatiblePackages = [
+        CompatiblePackage("com.onelouder.baconreader"),
+        CompatiblePackage("com.onelouder.baconreader.premium")
     ]
 )
-class SpoofClientPatch : AbstractSpoofClientPatch(
-    "http://baconreader.com/auth", Options, listOf(GetAuthorizationUrlFingerprint, RequestTokenFingerprint)
+object SpoofClientPatch : AbstractSpoofClientPatch(
+    "http://baconreader.com/auth", listOf(GetAuthorizationUrlFingerprint, RequestTokenFingerprint)
 ) {
+    private val clientId by clientIdOption
+
+    init {
+        options.register(clientIdOption)
+    }
 
     override fun List<MethodFingerprintResult>.patchClientId(context: BytecodeContext) {
         fun MethodFingerprintResult.patch(replacementString: String) {
@@ -47,6 +50,4 @@ class SpoofClientPatch : AbstractSpoofClientPatch(
         // Patch client id for access token request.
         last().patch(clientId!!)
     }
-
-    companion object Options : AbstractSpoofClientPatch.Options.SpoofClientOptionsContainer()
 }
