07c72cd950d9dbaae2b361cd387d360cf7d489dc
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-09-07 12:41:40 +0400

    fixing compile error

--- src/main/kotlin/app/revanced/patches/youtube/layout/hide/suggestionsshelf/annotations/SuggestionsShelfCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hide/suggestionsshelf/annotations/SuggestionsShelfCompatibility.kt
@@ -1,4 +1,4 @@
-package app.revanced.patches.youtube.layout.hide.breakingnews.annotations
+package app.revanced.patches.youtube.layout.hide.suggestionsshelf.annotations
 
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
