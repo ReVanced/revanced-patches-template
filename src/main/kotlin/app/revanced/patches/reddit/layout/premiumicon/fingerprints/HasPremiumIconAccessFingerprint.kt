a072126da18a86edfd17ca48e6fa9b60e67e5473
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-07 22:34:40 +0200

    premium icon

--- src/main/kotlin/app/revanced/patches/reddit/layout/premiumicon/fingerprints/HasPremiumIconAccessFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/reddit/layout/premiumicon/fingerprints/HasPremiumIconAccessFingerprint.kt
@@ -2,7 +2,7 @@ package app.revanced.patches.reddit.layout.premiumicon.fingerprints
 
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 
-object PremiumIconFingerprint : MethodFingerprint(
+object HasPremiumIconAccessFingerprint : MethodFingerprint(
     "Z",
     customFingerprint = { methodDef, _ ->
         methodDef.definingClass.endsWith("MyAccount;") && methodDef.name == "isPremiumSubscriber"
