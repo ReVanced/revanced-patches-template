36497e7ee2d0f807353484b48eeaae89c76d8cee
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-07 22:20:43 +0200

    solidexplorer

--- src/main/kotlin/app/revanced/patches/solidexplorer2/functionality/filesize/RemoveFileSizeLimitPatch.kt
+++ src/main/kotlin/app/revanced/patches/solidexplorer2/functionality/filesize/RemoveFileSizeLimitPatch.kt
@@ -1,23 +1,22 @@
-package app.revanced.patches.solidexplorer2.functionality.filesize.patch
+package app.revanced.patches.solidexplorer2.functionality.filesize
 
 import app.revanced.extensions.exception
-import app.revanced.patcher.annotation.Compatibility
-import app.revanced.patcher.annotation.Description
-import app.revanced.patcher.annotation.Name
-import app.revanced.patcher.annotation.Package
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.solidexplorer2.functionality.filesize.fingerprints.OnReadyFingerprint
 import com.android.tools.smali.dexlib2.iface.instruction.ThreeRegisterInstruction
 
-@Patch
-@Name("Remove file size limit")
-@Description("Allows opening files larger than 2 MB in the text editor.")
-@Compatibility([Package("pl.solidexplorer2")])
-class RemoveFileSizeLimitPatch : BytecodePatch(listOf(OnReadyFingerprint)) {
+
+@Patch(
+    name = "Remove file size limit",
+    description = "Allows opening files larger than 2 MB in the text editor.",
+    compatiblePackages = [CompatiblePackage("pl.solidexplorer2")]
+)
+object RemoveFileSizeLimitPatch : BytecodePatch(setOf(OnReadyFingerprint)) {
     override fun execute(context: BytecodeContext) = OnReadyFingerprint.result?.let { result ->
         val cmpIndex = result.scanResult.patternScanResult!!.startIndex + 1
         val cmpResultRegister = result.mutableMethod.getInstruction<ThreeRegisterInstruction>(cmpIndex).registerA
