e2f248cdf3587aa988cba1212f2241c136493355
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-08 00:00:02 +0200

    relay client spoof

--- src/main/kotlin/app/revanced/patches/reddit/customclients/relayforreddit/api/SpoofClientPatch.kt
+++ src/main/kotlin/app/revanced/patches/reddit/customclients/relayforreddit/api/SpoofClientPatch.kt
@@ -1,28 +1,31 @@
-package app.revanced.patches.reddit.customclients.relayforreddit.api.patch
+package app.revanced.patches.reddit.customclients.relayforreddit.api
 
-import app.revanced.patcher.annotation.Compatibility
-import app.revanced.patcher.annotation.Description
-import app.revanced.patcher.annotation.Package
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprintResult
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.reddit.customclients.AbstractSpoofClientPatch
-import app.revanced.patches.reddit.customclients.SpoofClientAnnotation
 import app.revanced.patches.reddit.customclients.relayforreddit.api.fingerprints.GetLoggedInBearerTokenFingerprint
 import app.revanced.patches.reddit.customclients.relayforreddit.api.fingerprints.GetLoggedOutBearerTokenFingerprint
 import app.revanced.patches.reddit.customclients.relayforreddit.api.fingerprints.GetRefreshTokenFingerprint
 import app.revanced.patches.reddit.customclients.relayforreddit.api.fingerprints.LoginActivityClientIdFingerprint
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 
-@SpoofClientAnnotation
-@Description("Spoofs the client in order to allow logging in. " +
-        "The OAuth application type has to be \"Installed app\" " +
-        "and the redirect URI has to be set to \"dbrady://relay\".")
-@Compatibility([Package("free.reddit.news"), Package("reddit.news")])
-class SpoofClientPatch : AbstractSpoofClientPatch(
+
+@Patch(
+    name = "Spoof client",
+    description = "Spoofs the client in order to allow logging in. " +
+            "The OAuth application type has to be \"Installed app\" " +
+            "and the redirect URI has to be set to \"dbrady://relay\".",
+    compatiblePackages = [
+        CompatiblePackage("free.reddit.news"),
+        CompatiblePackage("reddit.news")
+    ]
+)
+object SpoofClientPatch : AbstractSpoofClientPatch(
     "dbrady://relay",
-    Options,
     listOf(
         LoginActivityClientIdFingerprint,
         GetLoggedInBearerTokenFingerprint,
@@ -30,6 +33,10 @@ class SpoofClientPatch : AbstractSpoofClientPatch(
         GetRefreshTokenFingerprint
     )
 ) {
+    private val clientId by clientIdOption
+
+    init { options.register(clientIdOption) }
+
     override fun List<MethodFingerprintResult>.patchClientId(context: BytecodeContext) {
         forEach {
             val clientIdIndex = it.scanResult.stringsScanResult!!.matches.first().index
@@ -43,6 +50,4 @@ class SpoofClientPatch : AbstractSpoofClientPatch(
             }
         }
     }
-
-    companion object Options : AbstractSpoofClientPatch.Options.SpoofClientOptionsContainer()
 }
