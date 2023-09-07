a8a3e2470fb77e61d306690092829ec6b64c0b05
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-07 23:06:11 +0200

    joey piracy

--- src/main/kotlin/app/revanced/patches/reddit/customclients/joeyforreddit/detection/piracy/DisablePiracyDetectionPatch.kt
+++ src/main/kotlin/app/revanced/patches/reddit/customclients/joeyforreddit/detection/piracy/DisablePiracyDetectionPatch.kt
@@ -1,4 +1,4 @@
-package app.revanced.patches.reddit.customclients.joeyforreddit.detection.piracy.patch
+package app.revanced.patches.reddit.customclients.joeyforreddit.detection.piracy
 
 import app.revanced.extensions.exception
 import app.revanced.patcher.data.BytecodeContext
@@ -6,7 +6,7 @@ import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patches.reddit.customclients.joeyforreddit.detection.piracy.fingerprints.PiracyDetectionFingerprint
 
-class DisablePiracyDetectionPatch : BytecodePatch(listOf(PiracyDetectionFingerprint)) {
+object DisablePiracyDetectionPatch : BytecodePatch(setOf(PiracyDetectionFingerprint)) {
     override fun execute(context: BytecodeContext) {
         PiracyDetectionFingerprint.result?.mutableMethod?.addInstruction(
             0,
