e5fe7c94654f667d62b80a5992f583e56c995225
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-08 00:19:10 +0200

    reddit comment  ads

--- src/main/kotlin/app/revanced/patches/reddit/ad/general/HideAdsPatch.kt
+++ src/main/kotlin/app/revanced/patches/reddit/ad/general/HideAdsPatch.kt
@@ -7,7 +7,7 @@ import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.reddit.ad.banner.patch.HideBannerPatch
-import app.revanced.patches.reddit.ad.comments.patch.HideCommentAdsPatch
+import app.revanced.patches.reddit.ad.comments.HideCommentAdsPatch
 import app.revanced.patches.reddit.ad.general.fingerprints.AdPostFingerprint
 import app.revanced.patches.reddit.ad.general.fingerprints.NewAdPostFingerprint
 import com.android.tools.smali.dexlib2.Opcode
