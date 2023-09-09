bc754f86e26f8bd6a12be5ca23d36f8a05de8bd6
CnC-Robert <CnC.Rob3rt@gmail.com>
2023-09-09 12:59:30 +0200

    strava unlock subscription

--- src/main/kotlin/app/revanced/patches/strava/subscription/UnlockSubscriptionPatch.kt
+++ src/main/kotlin/app/revanced/patches/strava/subscription/UnlockSubscriptionPatch.kt
@@ -1,21 +1,27 @@
-package app.revanced.patches.strava.subscription.patch
+package app.revanced.patches.strava.subscription
 
 import app.revanced.extensions.exception
-import app.revanced.patcher.annotation.Compatibility
-import app.revanced.patcher.annotation.Description
-import app.revanced.patcher.annotation.Name
-import app.revanced.patcher.annotation.Package
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.strava.subscription.fingerprints.GetSubscribedFingerprint
 
-@Patch
-@Name("Unlock subscription features")
-@Description("Unlocks \"Matched Runs\" and \"Segment Efforts\".")
-@Compatibility([Package("com.strava", ["320.12"])])
-class UnlockSubscriptionPatch : BytecodePatch(listOf(GetSubscribedFingerprint)) {
+@Patch(
+    name = "Unlock subscription features",
+    description = "Unlocks \"Matched Runs\" and \"Segment Efforts\".",
+    compatiblePackages = [
+        CompatiblePackage(
+            "com.strava",
+            arrayOf(
+                "320.12"
+            )
+        )
+    ]
+)
+@Suppress("unused")
+object UnlockSubscriptionPatch : BytecodePatch(setOf(GetSubscribedFingerprint)) {
     override fun execute(context: BytecodeContext) = GetSubscribedFingerprint.result?.let { result ->
         val isSubscribedIndex = result.scanResult.patternScanResult!!.startIndex
         result.mutableMethod.replaceInstruction(isSubscribedIndex, "const/4 v0, 0x1")
