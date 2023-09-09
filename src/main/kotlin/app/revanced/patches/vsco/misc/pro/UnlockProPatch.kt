0d051ca2b1a5506105c1107157a244756a205b30
CnC-Robert <CnC.Rob3rt@gmail.com>
2023-09-09 11:40:28 +0200

    vsco unlock pro

--- src/main/kotlin/app/revanced/patches/vsco/misc/pro/UnlockProPatch.kt
+++ src/main/kotlin/app/revanced/patches/vsco/misc/pro/UnlockProPatch.kt
@@ -1,20 +1,22 @@
-package app.revanced.patches.vsco.misc.pro.patch
+package app.revanced.patches.vsco.misc.pro
 
 import app.revanced.extensions.exception
-import app.revanced.patcher.annotation.*
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.vsco.misc.pro.fingerprints.RevCatSubscriptionFingerprint
 
-
-@Patch
-@Name("Unlock pro")
-@Description("Unlocks pro features.")
-@Compatibility([Package("com.vsco.cam")])
-class UnlockProPatch : BytecodePatch(
-    listOf(RevCatSubscriptionFingerprint)
+@Patch(
+    name = "Unlock pro",
+    description = "Unlocks pro features.",
+    compatiblePackages = [
+        CompatiblePackage("com.vsco.cam")
+    ]
+)
+object UnlockProPatch : BytecodePatch(
+    setOf(RevCatSubscriptionFingerprint)
 ) {
     override fun execute(context: BytecodeContext) {
         RevCatSubscriptionFingerprint.result?.mutableMethod?.apply {
