c57dfd73f7067aaf660b1a80a68e853d6f39961e
CnC-Robert <CnC.Rob3rt@gmail.com>
2023-09-09 13:07:17 +0200

    spotify lite enable on demand

--- src/main/kotlin/app/revanced/patches/spotify/lite/ondemand/OnDemandPatch.kt
+++ src/main/kotlin/app/revanced/patches/spotify/lite/ondemand/OnDemandPatch.kt
@@ -1,23 +1,23 @@
-package app.revanced.patches.spotify.lite.ondemand.patch
+package app.revanced.patches.spotify.lite.ondemand
 
 import app.revanced.extensions.exception
-import app.revanced.patcher.annotation.Description
-import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotations.Patch
-import app.revanced.patches.spotify.lite.ondemand.annotations.OnDemandCompatibility
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.spotify.lite.ondemand.fingerprints.OnDemandFingerprint
 
-@Patch
-@Name("Enable on demand")
-@Description("Enables listening to songs on-demand, allowing to play any song from playlists, albums or artists without limitations. This does not remove ads.")
-@OnDemandCompatibility
-class OnDemandPatch : BytecodePatch(
-    listOf(
-        OnDemandFingerprint
-    )
+@Patch(
+    name = "Enable on demand",
+    description = "Enables listening to songs on-demand, allowing to play any song from playlists, albums or artists without limitations. This does not remove ads.",
+    compatiblePackages = [
+        CompatiblePackage("com.spotify.lite")
+    ]
+)
+@Suppress("unused")
+object OnDemandPatch : BytecodePatch(
+    setOf(OnDemandFingerprint)
 ) {
     override fun execute(context: BytecodeContext) {
         OnDemandFingerprint.result?.apply {
