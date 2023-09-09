2b801d78d636eadeca63537eda4b970c5d7979c1
CnC-Robert <CnC.Rob3rt@gmail.com>
2023-09-09 11:49:28 +0200

    twitter hide recommended users

---
+++ src/main/kotlin/app/revanced/patches/twitter/misc/hook/patch/recommendation/HideRecommendedUsersPatch.kt
@@ -0,0 +1,21 @@
+package app.revanced.patches.twitter.misc.hook.patch.recommendation
+
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patches.twitter.misc.hook.json.patch.JsonHookPatch
+import app.revanced.patches.twitter.misc.hook.patch.BaseHookPatchPatch
+
+@Patch(
+    name = "Hide recommended users",
+    description = "Hides recommended users.",
+    dependencies = [
+        JsonHookPatch::class
+    ],
+    compatiblePackages = [
+        CompatiblePackage("com.twitter.android")
+    ]
+)
+@Suppress("unused")
+object HideRecommendedUsersPatch : BaseHookPatchPatch(
+    "Lapp/revanced/twitter/patches/hook/patch/recommendation/RecommendedUsersHook;"
+)
