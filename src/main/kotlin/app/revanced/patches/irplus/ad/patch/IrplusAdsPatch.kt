c38ef91ffbd98de42f965c7cff4dd9a2a3290fa1
Pun Butrach <pun.butrach@gmail.com>
2023-09-10 19:10:39 +0700

    refactor(Irplus): use new format set by patcher
    Squashie!
    
    Co-authored-by: CollisionBot <pun.experiment@gmail.com>

--- src/main/kotlin/app/revanced/patches/irplus/ad/patch/IrplusAdsPatch.kt
+++ src/main/kotlin/app/revanced/patches/irplus/ad/patch/IrplusAdsPatch.kt
@@ -1,21 +1,26 @@
-package app.revanced.patches.irplus.ad.patch
+package app.revanced.patches.irplus.ad
 
-import app.revanced.patcher.annotation.Description
-import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotations.Patch
-import app.revanced.patches.irplus.ad.annotations.IrplusAdsCompatibility
 import app.revanced.patches.irplus.ad.fingerprints.IrplusAdsFingerprint
 
 
-@Patch
-@Name("Remove ads")
-@Description("Removes all ads from the app.")
-@IrplusAdsCompatibility
-class IrplusAdsPatch : BytecodePatch(
-    listOf(IrplusAdsFingerprint)
+@Patch(
+    name = "Remove ads",
+    description = "Removes all ads from the app.",
+    compatiblePackages = [
+        CompatiblePackage(
+            "net.binarymode.android.irplus"
+        )
+    ]
+)
+object IrplusAdsPatch : BytecodePatch(
+    setOf(
+        IrplusAdsFingerprint
+    )
 ) {
     override fun execute(context: BytecodeContext) {
         val method = IrplusAdsFingerprint.result!!.mutableMethod
