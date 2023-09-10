b3b6f0885262592ce0fb154dcd8ce5b2f7cfa573
Pun Butrach <pun.butrach@gmail.com>
2023-09-10 19:08:29 +0700

    refactor(Money Manager): use new format set by patcher
    Squashie!
    
    Co-authored-by: CollisionBot <pun.experiment@gmail.com>

--- src/main/kotlin/app/revanced/patches/moneymanager/patch/UnlockProPatch.kt
+++ src/main/kotlin/app/revanced/patches/moneymanager/patch/UnlockProPatch.kt
@@ -1,20 +1,25 @@
-package app.revanced.patches.moneymanager.patch
+package app.revanced.patches.moneymanager
 
-import app.revanced.patcher.annotation.Description
-import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotations.Patch
-import app.revanced.patches.moneymanager.annotations.UnlockProCompatibility
 import app.revanced.patches.moneymanager.fingerprints.UnlockProFingerprint
 
-@Patch
-@Name("Unlock pro")
-@Description("Unlocks pro features.")
-@UnlockProCompatibility
-class UnlockProPatch : BytecodePatch(
-    listOf(UnlockProFingerprint)
+@Patch(
+    name = "Unlock pro",
+    description = "Unlocks pro features.",
+    compatiblePackages = [
+        CompatiblePackage(
+            "com.ithebk.expensemanager"
+        )
+    ]
+)
+object UnlockProPatch : BytecodePatch(
+    setOf(
+        UnlockProFingerprint
+    )
 ) {
     override fun execute(context: BytecodeContext) {
        UnlockProFingerprint.result!!.mutableMethod.addInstructions(
