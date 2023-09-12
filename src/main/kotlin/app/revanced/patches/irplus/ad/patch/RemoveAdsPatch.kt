0c96317501217848afb195cdd0f9bd39ad124291
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-12 23:46:41 +0200

    irplus ads

--- src/main/kotlin/app/revanced/patches/irplus/ad/patch/RemoveAdsPatch.kt
+++ src/main/kotlin/app/revanced/patches/irplus/ad/patch/RemoveAdsPatch.kt
@@ -1,21 +1,19 @@
 package app.revanced.patches.irplus.ad.patch
 
-import app.revanced.patcher.annotation.Description
-import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotations.Patch
-import app.revanced.patches.irplus.ad.annotations.IrplusAdsCompatibility
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.irplus.ad.fingerprints.IrplusAdsFingerprint
 
 
-@Patch
-@Name("Remove ads")
-@Description("Removes all ads from the app.")
-@IrplusAdsCompatibility
-class IrplusAdsPatch : BytecodePatch(
-    listOf(IrplusAdsFingerprint)
+@Patch(
+    name = "Remove ads",
+    compatiblePackages = [CompatiblePackage("net.binarymode.android.irplus")]
+)
+object RemoveAdsPatch : BytecodePatch(
+    setOf(IrplusAdsFingerprint)
 ) {
     override fun execute(context: BytecodeContext) {
         val method = IrplusAdsFingerprint.result!!.mutableMethod
