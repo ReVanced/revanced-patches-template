bcd356cb9fddcb400021978962264efe8a703b0b
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-12 20:43:48 +0200

    mandatory login

--- src/main/kotlin/app/revanced/patches/lightroom/misc/login/patch/DisableMandatoryLoginPatch.kt
+++ src/main/kotlin/app/revanced/patches/lightroom/misc/login/patch/DisableMandatoryLoginPatch.kt
@@ -1,18 +1,21 @@
 package app.revanced.patches.lightroom.misc.login.patch
 
 import app.revanced.extensions.exception
-import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotations.Patch
-import app.revanced.patches.lightroom.misc.login.annotations.DisableMandatoryLoginCompatibility
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.lightroom.misc.login.fingerprint.IsLoggedInFingerprint
 
-@Patch
-@Name("Disable mandatory login")
-@DisableMandatoryLoginCompatibility
-class DisableMandatoryLoginPatch : BytecodePatch(listOf(IsLoggedInFingerprint)) {
+@Patch(
+    name = "Disable mandatory login",
+    compatiblePackages = [CompatiblePackage("com.adobe.lrmobile")]
+)
+@Suppress("unused")
+object DisableMandatoryLoginPatch : BytecodePatch(
+    setOf(IsLoggedInFingerprint)
+) {
     override fun execute(context: BytecodeContext) {
         IsLoggedInFingerprint.result?.mutableMethod?.apply {
             val index = implementation!!.instructions.lastIndex - 1
