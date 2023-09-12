59d760bb2a38143014fe051f3c47712e7fd099d1
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-12 17:25:40 +0200

    client spoof

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/patch/ClientSpoofPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/patch/ClientSpoofPatch.kt
@@ -1,26 +1,39 @@
 package app.revanced.patches.youtube.misc.fix.playback.patch
 
 import app.revanced.extensions.exception
-import app.revanced.patcher.annotation.Description
-import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotations.DependsOn
-import app.revanced.patcher.patch.annotations.Patch
-import app.revanced.patches.youtube.misc.fix.playback.annotations.ClientSpoofCompatibility
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.youtube.misc.fix.playback.fingerprints.UserAgentHeaderBuilderFingerprint
 import com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction
 
-@Patch
-@Name("Client spoof")
-@Description("Spoofs the client to allow playback.")
-@ClientSpoofCompatibility
-@DependsOn([SpoofSignatureVerificationPatch::class])
-class ClientSpoofPatch : BytecodePatch(
-    listOf(UserAgentHeaderBuilderFingerprint)
+
+@Patch(
+    name = "Client spoof",
+    description = "Spoofs the client to allow playback.",
+    dependencies = [SpoofSignatureVerificationPatch::class],
+    compatiblePackages = [
+        CompatiblePackage(
+            "com.google.android.youtube",
+            [
+                "18.16.37",
+                "18.19.35",
+                "18.20.39",
+                "18.23.35",
+                "18.29.38",
+                "18.32.39"
+            ]
+        )
+    ]
+)
+object ClientSpoofPatch : BytecodePatch(
+    setOf(UserAgentHeaderBuilderFingerprint)
 ) {
+    private const val ORIGINAL_PACKAGE_NAME = "com.google.android.youtube"
+
     override fun execute(context: BytecodeContext) {
         UserAgentHeaderBuilderFingerprint.result?.let { result ->
             val insertIndex = result.scanResult.patternScanResult!!.endIndex
@@ -31,9 +44,5 @@ class ClientSpoofPatch : BytecodePatch(
            }
 
         } ?: throw UserAgentHeaderBuilderFingerprint.exception
-    }
-
-    private companion object {
-        private const val ORIGINAL_PACKAGE_NAME = "com.google.android.youtube"
     }
 }
