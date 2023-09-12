3b47a57b2edb894c5fd14d5a176e62428aeea5c9
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-12 23:57:50 +0200

    spoof device id photomath

--- src/main/kotlin/app/revanced/patches/photomath/detection/deviceid/patch/SpoofDeviceIdPatch.kt
+++ src/main/kotlin/app/revanced/patches/photomath/detection/deviceid/patch/SpoofDeviceIdPatch.kt
@@ -1,27 +1,25 @@
 package app.revanced.patches.photomath.detection.deviceid.patch
 
 import app.revanced.extensions.exception
-import app.revanced.patcher.annotation.Compatibility
-import app.revanced.patcher.annotation.Description
-import app.revanced.patcher.annotation.Name
-import app.revanced.patcher.annotation.Package
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.replaceInstructions
 import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotations.DependsOn
-import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.photomath.detection.deviceid.fingerprints.GetDeviceIdFingerprint
 import app.revanced.patches.photomath.detection.signature.patch.SignatureDetectionPatch
 import kotlin.random.Random
 
-@Patch
-@DependsOn([SignatureDetectionPatch::class])
-@Name("Spoof device ID")
-@Description("Spoofs device ID to mitigate manual bans by developers.")
-@Compatibility([Package("com.microblink.photomath")])
-class SpoofDeviceIdPatch : BytecodePatch(
-    listOf(GetDeviceIdFingerprint)
-) {
+@Patch(
+    name = "Spoof device ID",
+    description = "Spoofs device ID to mitigate manual bans by developers.",
+    dependencies = [SignatureDetectionPatch::class],
+    compatiblePackages = [CompatiblePackage("com.microblink.photomath")]
+)
+@Suppress("unused")
+object SpoofDeviceIdPatch : BytecodePatch(
+    setOf(GetDeviceIdFingerprint)
+){
     override fun execute(context: BytecodeContext) = GetDeviceIdFingerprint.result?.mutableMethod?.replaceInstructions(
         0,
         """
