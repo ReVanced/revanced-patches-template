4d6a9be4210a85238beb6bc22249510cc2be8f93
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-12 20:13:20 +0200

    money  manager unlock pro

--- src/main/kotlin/app/revanced/patches/moneymanager/patch/UnlockProPatch.kt
+++ src/main/kotlin/app/revanced/patches/moneymanager/patch/UnlockProPatch.kt
@@ -1,21 +1,19 @@
 package app.revanced.patches.moneymanager.patch
 
-import app.revanced.patcher.annotation.Description
-import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotations.Patch
-import app.revanced.patches.moneymanager.annotations.UnlockProCompatibility
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.moneymanager.fingerprints.UnlockProFingerprint
 
-@Patch
-@Name("Unlock pro")
-@Description("Unlocks pro features.")
-@UnlockProCompatibility
-class UnlockProPatch : BytecodePatch(
-    listOf(UnlockProFingerprint)
-) {
+@Patch(
+    name = "Unlock pro",
+    compatiblePackages = [CompatiblePackage("com.ithebk.expensemanager")]
+)
+object UnlockProPatch : BytecodePatch(
+    setOf(UnlockProFingerprint)
+){
     override fun execute(context: BytecodeContext) {
        UnlockProFingerprint.result!!.mutableMethod.addInstructions(
             0,
