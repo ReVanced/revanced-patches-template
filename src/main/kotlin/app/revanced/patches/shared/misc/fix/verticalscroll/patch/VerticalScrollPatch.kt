1bd90d7ba3fb446951ab7d634f141b7715ce83dd
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-12 20:37:48 +0200

    vertical scroll

--- src/main/kotlin/app/revanced/patches/shared/misc/fix/verticalscroll/patch/VerticalScrollPatch.kt
+++ src/main/kotlin/app/revanced/patches/shared/misc/fix/verticalscroll/patch/VerticalScrollPatch.kt
@@ -1,19 +1,17 @@
 package app.revanced.patches.shared.misc.fix.verticalscroll.patch
 
 import app.revanced.extensions.exception
-import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patches.shared.misc.fix.verticalscroll.annotations.VerticalScrollCompatibility
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.shared.misc.fix.verticalscroll.fingerprints.CanScrollVerticallyFingerprint
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 
-@Description("Fixes issues with refreshing the feed when the first component is of type EmptyComponent.")
-@VerticalScrollCompatibility
-class VerticalScrollPatch : BytecodePatch(
-    listOf(CanScrollVerticallyFingerprint)
+@Patch(description = "Fixes issues with refreshing the feed when the first component is of type EmptyComponent.")
+object VerticalScrollPatch : BytecodePatch(
+    setOf(CanScrollVerticallyFingerprint)
 ) {
     override fun execute(context: BytecodeContext) {
         CanScrollVerticallyFingerprint.result?.let {
