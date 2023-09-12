1804b29fb8f16f9469e5058e9a8e717a22790e24
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-12 23:39:44 +0200

    hide email

--- src/main/kotlin/app/revanced/patches/youtube/layout/hide/personalinformation/resource/patch/HideEmailAddressResourcePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hide/personalinformation/resource/patch/HideEmailAddressResourcePatch.kt
@@ -2,17 +2,15 @@ package app.revanced.patches.youtube.layout.hide.personalinformation.resource.pa
 
 import app.revanced.patcher.data.ResourceContext
 import app.revanced.patcher.patch.ResourcePatch
-import app.revanced.patcher.patch.annotations.DependsOn
-import app.revanced.patches.shared.mapping.misc.patch.ResourceMappingPatch
+import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patches.shared.mapping.misc.ResourceMappingPatch
 import app.revanced.patches.shared.settings.preference.impl.StringResource
 import app.revanced.patches.shared.settings.preference.impl.SwitchPreference
 import app.revanced.patches.youtube.misc.settings.SettingsPatch
 
-@DependsOn([SettingsPatch::class, ResourceMappingPatch::class])
-class HideEmailAddressResourcePatch : ResourcePatch {
-    companion object {
-        internal var accountSwitcherAccessibilityLabelId: Long = -1
-    }
+@Patch(dependencies = [SettingsPatch::class, ResourceMappingPatch::class])
+object HideEmailAddressResourcePatch : ResourcePatch() {
+    internal var accountSwitcherAccessibilityLabelId: Long = -1
 
     override fun execute(context: ResourceContext) {
         SettingsPatch.PreferenceScreen.LAYOUT.addPreferences(
