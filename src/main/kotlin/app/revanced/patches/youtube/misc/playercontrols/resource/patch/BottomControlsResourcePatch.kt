cf70a355f87c5584810d9f0c157f55c382768995
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-12 16:56:49 +0200

    bottom controls resource

--- src/main/kotlin/app/revanced/patches/youtube/misc/playercontrols/resource/patch/BottomControlsResourcePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/playercontrols/resource/patch/BottomControlsResourcePatch.kt
@@ -1,14 +1,26 @@
 package app.revanced.patches.youtube.misc.playercontrols.resource.patch
 
-import app.revanced.patcher.util.DomFileEditor
 import app.revanced.patcher.data.ResourceContext
 import app.revanced.patcher.patch.ResourcePatch
-import app.revanced.patcher.patch.annotations.DependsOn
-import app.revanced.patches.shared.mapping.misc.patch.ResourceMappingPatch
+import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patcher.util.DomFileEditor
+import app.revanced.patches.shared.mapping.misc.ResourceMappingPatch
 import java.io.Closeable
 
-@DependsOn([ResourceMappingPatch::class])
-class BottomControlsResourcePatch : ResourcePatch, Closeable {
+@Patch(dependencies = [ResourceMappingPatch::class])
+@Suppress("unused")
+object BottomControlsResourcePatch : ResourcePatch(), Closeable {
+    internal var bottomUiContainerResourceId: Long = -1
+
+    internal const val TARGET_RESOURCE_NAME = "youtube_controls_bottom_ui_container.xml"
+    private const val TARGET_RESOURCE = "res/layout/$TARGET_RESOURCE_NAME"
+
+    private lateinit var resourceContext: ResourceContext
+    private lateinit var targetXmlEditor: DomFileEditor
+
+    // The element to which to add the new elements to
+    private var lastLeftOf = "fullscreen_button"
+
     override fun execute(context: ResourceContext) {
         resourceContext = context
         targetXmlEditor = context.xmlEditor[TARGET_RESOURCE]
@@ -17,58 +29,45 @@ class BottomControlsResourcePatch : ResourcePatch, Closeable {
             .single { it.type == "id" && it.name == "bottom_ui_container_stub" }.id
     }
 
-    companion object {
-        internal var bottomUiContainerResourceId : Long = -1
+    /**
+     * Add new controls to the bottom of the YouTube player.
+     * @param hostYouTubeControlsBottomUiResourceName The hosting resource name containing the elements.
+     */
+    internal fun addControls(hostYouTubeControlsBottomUiResourceName: String) {
+        val sourceXmlEditor =
+            resourceContext.xmlEditor[this::class.java.classLoader.getResourceAsStream(
+                hostYouTubeControlsBottomUiResourceName
+            )!!]
 
-        internal const val TARGET_RESOURCE_NAME = "youtube_controls_bottom_ui_container.xml"
-        private const val TARGET_RESOURCE = "res/layout/$TARGET_RESOURCE_NAME"
+        val targetElement =
+            "android.support.constraint.ConstraintLayout"
 
-        private lateinit var resourceContext: ResourceContext
-        private lateinit var targetXmlEditor: DomFileEditor
+        val hostElements = sourceXmlEditor.file.getElementsByTagName(targetElement).item(0).childNodes
 
-        // The element to which to add the new elements to
-        private var lastLeftOf = "fullscreen_button"
+        val destinationResourceFile = this.targetXmlEditor.file
+        val destinationElement =
+            destinationResourceFile.getElementsByTagName(targetElement).item(0)
 
-        /**
-         * Add new controls to the bottom of the YouTube player.
-         * @param hostYouTubeControlsBottomUiResourceName The hosting resource name containing the elements.
-         */
-        internal fun addControls(hostYouTubeControlsBottomUiResourceName: String) {
-            val sourceXmlEditor =
-                resourceContext.xmlEditor[this::class.java.classLoader.getResourceAsStream(
-                    hostYouTubeControlsBottomUiResourceName
-                )!!]
+        for (index in 1 until hostElements.length) {
+            val element = hostElements.item(index).cloneNode(true)
 
-            val targetElement =
-                "android.support.constraint.ConstraintLayout"
+            // if the element has no attributes theres no point to adding it to the destination
+            if (!element.hasAttributes()) continue
 
-            val hostElements = sourceXmlEditor.file.getElementsByTagName(targetElement).item(0).childNodes
+            // set the elements lastLeftOf attribute to the lastLeftOf value
+            val namespace = "@+id"
+            element.attributes.getNamedItem("yt:layout_constraintRight_toLeftOf").nodeValue =
+                "$namespace/$lastLeftOf"
 
-            val destinationResourceFile = this.targetXmlEditor.file
-            val destinationElement =
-                destinationResourceFile.getElementsByTagName(targetElement).item(0)
+            // set lastLeftOf attribute to the current element
+            val nameSpaceLength = 5
+            lastLeftOf = element.attributes.getNamedItem("android:id").nodeValue.substring(nameSpaceLength)
 
-            for (index in 1 until hostElements.length) {
-                val element = hostElements.item(index).cloneNode(true)
-
-                // if the element has no attributes theres no point to adding it to the destination
-                if (!element.hasAttributes()) continue
-
-                // set the elements lastLeftOf attribute to the lastLeftOf value
-                val namespace = "@+id"
-                element.attributes.getNamedItem("yt:layout_constraintRight_toLeftOf").nodeValue =
-                    "$namespace/$lastLeftOf"
-
-                // set lastLeftOf attribute to the current element
-                val nameSpaceLength = 5
-                lastLeftOf = element.attributes.getNamedItem("android:id").nodeValue.substring(nameSpaceLength)
-
-                // copy the element
-                destinationResourceFile.adoptNode(element)
-                destinationElement.appendChild(element)
-            }
-            sourceXmlEditor.close()
+            // copy the element
+            destinationResourceFile.adoptNode(element)
+            destinationElement.appendChild(element)
         }
+        sourceXmlEditor.close()
     }
 
     override fun close() {
