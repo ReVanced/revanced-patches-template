0622f0467cde9d733fdc7ac2a7890b84d8015670
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-13 01:17:05 +0200

    disable dashboard ads

--- src/main/kotlin/app/revanced/patches/tumblr/ads/DisableDashboardAds.kt
+++ src/main/kotlin/app/revanced/patches/tumblr/ads/DisableDashboardAds.kt
@@ -1,24 +1,23 @@
-package app.revanced.patches.tumblr.ads.patch
+package app.revanced.patches.tumblr.ads
 
 import app.revanced.extensions.exception
-import app.revanced.patcher.annotation.Compatibility
-import app.revanced.patcher.annotation.Description
-import app.revanced.patcher.annotation.Name
-import app.revanced.patcher.annotation.Package
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.tumblr.ads.fingerprints.AdWaterfallFingerprint
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 
-@Patch
-@Name("Disable dashboard ads")
-@Description("Disables ads in the dashboard.")
-@Compatibility([Package("com.tumblr")])
-class DisableDashboardAds : BytecodePatch(
-    listOf(AdWaterfallFingerprint)
+@Patch(
+    name = "Disable dashboard ads",
+    description = "Disables ads in the dashboard.",
+    compatiblePackages = [CompatiblePackage("com.tumblr")]
+)
+@Suppress("unused")
+object DisableDashboardAds : BytecodePatch(
+    setOf(AdWaterfallFingerprint)
 ) {
     override fun execute(context: BytecodeContext) = AdWaterfallFingerprint.result?.let {
         it.scanResult.stringsScanResult!!.matches.forEach { match ->
