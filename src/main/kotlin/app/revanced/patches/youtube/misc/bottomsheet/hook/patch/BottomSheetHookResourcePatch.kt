b6808a161fa1597d8f9c799c4f575d0251b5d16c
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-12 16:58:23 +0200

    bottom sheet resource

--- src/main/kotlin/app/revanced/patches/youtube/misc/bottomsheet/hook/patch/BottomSheetHookResourcePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/bottomsheet/hook/patch/BottomSheetHookResourcePatch.kt
@@ -3,17 +3,20 @@ package app.revanced.patches.youtube.misc.bottomsheet.hook.patch
 import app.revanced.patcher.data.ResourceContext
 import app.revanced.patcher.patch.PatchException
 import app.revanced.patcher.patch.ResourcePatch
-import app.revanced.patcher.patch.annotations.DependsOn
-import app.revanced.patches.shared.mapping.misc.patch.ResourceMappingPatch
+import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patches.shared.mapping.misc.ResourceMappingPatch
 
-@DependsOn([ResourceMappingPatch::class])
-class BottomSheetHookResourcePatch : ResourcePatch {
+@Patch(
+    name = "Remove root detection",
+    description = "Removes the check for root permissions.",
+    dependencies = [ResourceMappingPatch::class]
+)
+@Suppress("unused")
+object BottomSheetHookResourcePatch : ResourcePatch() {
+    internal var bottomSheetMargins = -1L
+
     override fun execute(context: ResourceContext) {
         bottomSheetMargins =  ResourceMappingPatch.resourceMappings.find { it.name == "bottom_sheet_margins" }?.id
             ?: throw PatchException("Could not find resource")
-    }
-
-    internal companion object {
-        var bottomSheetMargins = -1L
     }
 }
