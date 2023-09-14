fd7464f012cbfd193b59b44b496bd6d541bd107e
oSumAtrIX <johan.melkonyan1@web.de>
2023-09-14 03:19:51 +0200

    migrate patches

--- src/main/kotlin/app/revanced/patches/tumblr/annoyances/popups/patch/DisableGiftMessagePopupPatch.kt
+++ src/main/kotlin/app/revanced/patches/tumblr/annoyances/popups/patch/DisableGiftMessagePopupPatch.kt
@@ -1,22 +1,21 @@
 package app.revanced.patches.tumblr.annoyances.popups.patch
 
 import app.revanced.extensions.exception
-import app.revanced.patcher.annotation.Compatibility
-import app.revanced.patcher.annotation.Description
-import app.revanced.patcher.annotation.Name
-import app.revanced.patcher.annotation.Package
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.tumblr.annoyances.popups.fingerprints.ShowGiftMessagePopupFingerprint
 
-@Patch
-@Name("Disable gift message popup")
-@Description("Disables the popup suggesting to buy TumblrMart items for other people.")
-@Compatibility([Package("com.tumblr")])
-class DisableGiftMessagePopupPatch : BytecodePatch(
-    listOf(ShowGiftMessagePopupFingerprint)
+@Patch(
+    name = "Disable gift message popup",
+    description = "Disables the popup suggesting to buy TumblrMart items for other people.",
+    compatiblePackages = [CompatiblePackage("com.tumblr")]
+)
+@Suppress("unused")
+object DisableGiftMessagePopupPatch : BytecodePatch(
+    setOf(ShowGiftMessagePopupFingerprint)
 ) {
     override fun execute(context: BytecodeContext) =
         ShowGiftMessagePopupFingerprint.result?.mutableMethod?.addInstructions(0, "return-void")
