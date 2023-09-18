0622d0e17161950196317771b36e97021795b938
johnconner122 <107796137+johnconner122@users.noreply.github.com>
2023-09-18 16:42:05 +0500

    fix: hide `Breaking news` in tablet layout

--- src/main/kotlin/app/revanced/patches/youtube/layout/hide/suggestionsshelf/patch/SuggestionsShelfPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hide/suggestionsshelf/patch/SuggestionsShelfPatch.kt
@@ -1,25 +1,35 @@
 package app.revanced.patches.youtube.layout.hide.suggestionsshelf.patch
 
+import app.revanced.extensions.exception
 import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
+import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.ResourcePatch
 import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patches.shared.mapping.misc.patch.ResourceMappingPatch
 import app.revanced.patches.shared.settings.preference.impl.StringResource
 import app.revanced.patches.shared.settings.preference.impl.SwitchPreference
 import app.revanced.patches.youtube.layout.hide.suggestionsshelf.annotations.SuggestionsShelfCompatibility
+import app.revanced.patches.youtube.layout.hide.suggestionsshelf.fingerprints.BreakingNewsFingerprint
 import app.revanced.patches.youtube.layout.utils.navbarindexhook.patch.NavBarIndexHookPatch
 import app.revanced.patches.youtube.misc.litho.filter.patch.LithoFilterPatch
 import app.revanced.patches.youtube.misc.settings.bytecode.patch.SettingsPatch
+import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 
 @Patch
-@DependsOn([LithoFilterPatch::class, NavBarIndexHookPatch::class])
+@DependsOn([LithoFilterPatch::class, NavBarIndexHookPatch::class, ResourceMappingPatch::class])
 @Name("Hide Suggestions shelf")
 @Description("Hides suggestions shelf on the homepage tab.")
 @SuggestionsShelfCompatibility
-class SuggestionsShelfPatch : ResourcePatch {
-    override fun execute(context: ResourceContext) {
+class SuggestionsShelfPatch : BytecodePatch(
+    listOf(BreakingNewsFingerprint)
+) {
+    override fun execute(context: BytecodeContext) {
         SettingsPatch.PreferenceScreen.LAYOUT.addPreferences(
             SwitchPreference(
                 "revanced_hide_suggestions_shelf",
@@ -29,10 +39,33 @@ class SuggestionsShelfPatch : ResourcePatch {
             )
         )
 
+        BreakingNewsFingerprint.result?.let {
+            val insertIndex = it.scanResult.patternScanResult!!.endIndex - 1
+            val moveResultIndex = insertIndex - 1
+
+            it.mutableMethod.apply {
+                val breakingNewsViewRegister = getInstruction<OneRegisterInstruction>(moveResultIndex).registerA
+
+                addInstruction(
+                    insertIndex,
+                    """
+                        invoke-static {v$breakingNewsViewRegister}, $FILTER_CLASS_DESCRIPTOR->hideBreakingNews(Landroid/view/View;)V
+                    """
+                )
+            }
+
+        } ?: throw BreakingNewsFingerprint.exception
+
+        horizontalCardListId = ResourceMappingPatch.resourceMappings.single {
+            it.type == "layout" && it.name == "horizontal_card_list"
+        }.id
+
         LithoFilterPatch.addFilter(FILTER_CLASS_DESCRIPTOR)
     }
 
-    private companion object {
-        const val FILTER_CLASS_DESCRIPTOR = "Lapp/revanced/integrations/patches/components/SuggestionsShelfFilter;"
+    companion object {
+        private const val FILTER_CLASS_DESCRIPTOR = "Lapp/revanced/integrations/patches/components/SuggestionsShelfFilter;"
+
+        internal var horizontalCardListId = -1L
     }
 }
