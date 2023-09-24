649e5f01a3088d0f97ebf919e851f7c35b917ba3
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-09-24 16:42:10 +0400

    merge specific changes

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/SpoofSignatureResourcePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/SpoofSignatureResourcePatch.kt
@@ -5,9 +5,9 @@ import app.revanced.patcher.patch.ResourcePatch
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.shared.mapping.misc.ResourceMappingPatch
 import app.revanced.patches.shared.settings.preference.impl.PreferenceScreen
-import app.revanced.patches.shared.settings.preference.impl.StringResource
 import app.revanced.patches.shared.settings.preference.impl.SwitchPreference
 import app.revanced.patches.youtube.misc.settings.SettingsPatch
+import app.revanced.patches.youtube.misc.settings.SettingsResourcePatch
 
 @Patch(dependencies = [SettingsPatch::class, ResourceMappingPatch::class])
 object SpoofSignatureResourcePatch : ResourcePatch() {
@@ -16,51 +16,25 @@ object SpoofSignatureResourcePatch : ResourcePatch() {
     override fun execute(context: ResourceContext) {
         SettingsPatch.PreferenceScreen.MISC.addPreferences(
             PreferenceScreen(
-                key = "revanced_spoof_signature_verification",
-                title = StringResource(
-                    "revanced_spoof_signature_verification_title",
-                    "Spoof app signature"
-                ),
-                preferences = listOf(
+                "revanced_spoof_signature_verification_screen",
+                "revanced_spoof_signature_verification_screen_title",
+                listOf(
                     SwitchPreference(
                         "revanced_spoof_signature_verification_enabled",
-                        StringResource("revanced_spoof_signature_verification_enabled_title", "Spoof app signature"),
-                        StringResource(
-                            "revanced_spoof_signature_verification_enabled_summary_on",
-                            "App signature spoofed\\n\\n"
-                                    + "Side effects include:\\n"
-                                    + "• No ambient mode\\n"
-                                    + "• Videos can't be downloaded\\n"
-                                    + "• Seekbar thumbnails are hidden"
-                        ),
-                        StringResource(
-                            "revanced_spoof_signature_verification_enabled_summary_off",
-                            "App signature not spoofed\\n\\nVideo playback may not work"
-                        ),
-                        StringResource(
-                            "revanced_spoof_signature_verification_enabled_user_dialog_message",
-                            "Turning off this setting will cause video playback issues."
-                        )
+                        "revanced_spoof_signature_verification_enabled_title",
+                        "revanced_spoof_signature_verification_enabled_summary_on",
+                        "revanced_spoof_signature_verification_enabled_summary_off"
                     ),
                     SwitchPreference(
                         "revanced_spoof_signature_in_feed_enabled",
-                        StringResource("revanced_spoof_signature_in_feed_enabled_title", "Spoof app signature in feed"),
-                        StringResource(
-                            "revanced_spoof_signature_in_feed_enabled_summary_on",
-                            "App signature spoofed\\n\\n"
-                                    + "Side effects include:\\n"
-                                    + "• Feed videos are missing subtitles\\n"
-                                    + "• Automatically played feed videos will show up in your watch history"
-                        ),
-                        StringResource(
-                            "revanced_spoof_signature_in_feed_enabled_summary_off",
-                            "App signature not spoofed for feed videos\n\n"
-                                    + "Feed videos will play for less than 1 minute before encountering playback issues"
-                        )
-                    )
+                        "revanced_spoof_signature_in_feed_enabled_title",
+                        "revanced_spoof_signature_in_feed_enabled_summary_on",
+                        "revanced_spoof_signature_in_feed_enabled_summary_off",
+                    ),
                 )
             )
         )
+        SettingsResourcePatch.mergePatchStrings("SpoofSignatureVerification")
 
         scrubbedPreviewThumbnailResourceId = ResourceMappingPatch.resourceMappings.single {
             it.type == "id" && it.name == "thumbnail"
