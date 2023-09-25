92f741fb61d0a3d96f5d377179b3bfd41edcc891
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-09-26 02:06:50 +0400

    Ambient mode works now.  YT Premium 180p enhanced does not.

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/SpoofSignaturePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/SpoofSignaturePatch.kt
@@ -54,7 +54,7 @@ object SpoofSignaturePatch : BytecodePatch(
                             "revanced_spoof_signature_verification_enabled_summary_on",
                             "App signature spoofed\\n\\n"
                                     + "Side effects include:\\n"
-                                    + "• No ambient mode\\n"
+                                    + "• Enhanced bitrate is not available\\n"
                                     + "• Videos cannot be downloaded"
                         ),
                         StringResource(
