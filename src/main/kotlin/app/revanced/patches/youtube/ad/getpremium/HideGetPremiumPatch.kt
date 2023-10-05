e1879eb6f6b229830512b1e9ee6eb8283d4dda97
KobeW50 <84587632+KobeW50@users.noreply.github.com>
2023-10-04 22:33:21 -0400

    fix patch name and add description

--- src/main/kotlin/app/revanced/patches/youtube/ad/getpremium/HideGetPremiumPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/ad/getpremium/HideGetPremiumPatch.kt
@@ -15,7 +15,8 @@ import app.revanced.patches.youtube.misc.settings.SettingsPatch
 import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
 
 @Patch(
-    name = "Hide get premium",
+    name = "Hide \"Get Premium\"",
+    description = "Hides YouTube Premium advertisements under video player.",
     dependencies = [IntegrationsPatch::class, SettingsPatch::class],
     compatiblePackages = [
         CompatiblePackage(
