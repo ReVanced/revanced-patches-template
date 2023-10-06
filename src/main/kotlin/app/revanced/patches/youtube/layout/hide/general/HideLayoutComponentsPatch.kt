40cc213144485d6f51906cb636fa3b57438be335
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2023-10-06 18:21:28 +0400

    fix(YouTube - Hide layout components): fix incorrect sorting of settings UI

--- src/main/kotlin/app/revanced/patches/youtube/layout/hide/general/HideLayoutComponentsPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/hide/general/HideLayoutComponentsPatch.kt
@@ -53,14 +53,14 @@ object HideLayoutComponentsPatch : BytecodePatch(
             ),
             SwitchPreference(
                 "revanced_hide_join_membership_button",
-                StringResource("revanced_hide_join_membership_button_title", "Hide \"Join\" button"),
+                StringResource("revanced_hide_join_membership_button_title", "Hide Join button"),
                 StringResource("revanced_hide_join_membership_button_summary_on", "Button is hidden"),
                 StringResource("revanced_hide_join_membership_button_summary_off", "Button is shown")
             ),
 
             SwitchPreference(
                 "revanced_hide_notify_me_button",
-                StringResource("revanced_hide_notify_me_button_title", "Hide \"Notify me\" button"),
+                StringResource("revanced_hide_notify_me_button_title", "Hide Notify me button"),
                 StringResource("revanced_hide_notify_me_button_summary_on", "Button is hidden"),
                 StringResource("revanced_hide_notify_me_button_summary_off", "Button is shown")
             ),
