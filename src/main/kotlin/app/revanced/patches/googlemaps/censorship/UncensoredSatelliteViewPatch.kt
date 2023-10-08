7ec1fa4767b9f4411a70c5d7b572375fbaa64fee
scria1000 <91804886+scria1000@users.noreply.github.com>
2023-10-08 23:50:13 +0300

    Extract instruction filter into a new function

--- src/main/kotlin/app/revanced/patches/googlemaps/censorship/UncensoredSatelliteViewPatch.kt
+++ src/main/kotlin/app/revanced/patches/googlemaps/censorship/UncensoredSatelliteViewPatch.kt
@@ -20,9 +20,23 @@ import com.android.tools.smali.dexlib2.iface.reference.MethodReference
     description = "Circumvent Google's intentional provision of low-resolution satellite imagery in select regions through spoofing the country information provided by the SIM card.",
     compatiblePackages = [CompatiblePackage("com.google.android.apps.maps")]
 )
+@Suppress("unused")
 object UncensoredSatelliteViewPatch : AbstractTransformInstructionsPatch<Int>() {
     override fun filterMap(
         classDef: ClassDef, method: Method, instruction: Instruction, instructionIndex: Int
+    ) = filterCountryCodeMethodCall(instruction, instructionIndex)
+
+    override fun transform(mutableMethod: MutableMethod, entry: Int) {
+        // Get register of the next MOVE_RESULT_OBJECT instruction that accompanies INVOKE_VIRTUAL
+        val register = mutableMethod.getInstruction<OneRegisterInstruction>(entry + 1).registerA
+
+        mutableMethod.removeInstruction(entry + 1)
+        // We return Iceland's ISO code, I don't see a reason to customize this for now
+        mutableMethod.replaceInstruction(entry, "const-string v$register, \"is\"")
+    }
+
+    private fun filterCountryCodeMethodCall(
+        instruction: Instruction, instructionIndex: Int
     ): Int? {
         if (instruction.opcode != Opcode.INVOKE_VIRTUAL) return null
         if (instruction !is ReferenceInstruction) return null
@@ -32,14 +46,5 @@ object UncensoredSatelliteViewPatch : AbstractTransformInstructionsPatch<Int>() 
         if (reference.name != "getSimCountryIso" && reference.name != "getNetworkCountryIso") return null
 
         return instructionIndex
-    }
-
-    override fun transform(mutableMethod: MutableMethod, entry: Int) {
-        // Get register of the next MOVE_RESULT_OBJECT instruction that accompanies INVOKE_VIRTUAL
-        val register = mutableMethod.getInstruction<OneRegisterInstruction>(entry + 1).registerA
-
-        mutableMethod.removeInstruction(entry + 1)
-        // We return Iceland's ISO code, I don't see a reason to customize this for now
-        mutableMethod.replaceInstruction(entry, "const-string v$register, \"is\"")
     }
 }
