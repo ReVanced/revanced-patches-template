5b0eec28af514c48a42004f6e4b230ed58e7d792
Alex Uskov <FrozenAlex@users.noreply.github.com>
2023-10-09 15:24:41 +0400

    Fix comment

--- src/main/kotlin/app/revanced/patches/twitter/misc/links/fingerprints/AddTelemetryToLinkFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/twitter/misc/links/fingerprints/AddTelemetryToLinkFingerprint.kt
@@ -4,7 +4,7 @@ import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
-// Returns a shareable link string based on a tweet ID and a username
+// Adds telemetry to the share links
 object AddTelemetryToLinkFingerprint : MethodFingerprint(
     accessFlags = AccessFlags.PUBLIC.value or AccessFlags.STATIC.value or AccessFlags.FINAL.value,
     opcodes = listOf(Opcode.RETURN_OBJECT),
