eee51a863a059c5073b2817e57059e072b72155d
oSumAtrIX <johan.melkonyan1@web.de>
2023-11-11 18:08:52 +0100

    feat(YouTube - Seekbar tapping): Support version `18.43.45`

--- src/main/kotlin/app/revanced/patches/youtube/interaction/seekbar/fingerprints/OnTouchEventHandlerFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/interaction/seekbar/fingerprints/OnTouchEventHandlerFingerprint.kt
@@ -1,8 +1,8 @@
 package app.revanced.patches.youtube.interaction.seekbar.fingerprints
 
 import app.revanced.patcher.extensions.or
-import app.revanced.patcher.fingerprint.annotation.FuzzyPatternScanMethod
 import app.revanced.patcher.fingerprint.MethodFingerprint
+import app.revanced.patcher.fingerprint.annotation.FuzzyPatternScanMethod
 import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
@@ -12,7 +12,7 @@ object OnTouchEventHandlerFingerprint : MethodFingerprint(
     accessFlags = AccessFlags.PUBLIC or AccessFlags.PUBLIC,
     parameters = listOf("L"),
     opcodes = listOf(
-        Opcode.INVOKE_VIRTUAL, // oMethodReference
+        Opcode.INVOKE_VIRTUAL, // nMethodReference
         Opcode.RETURN,
         Opcode.IGET_OBJECT,
         Opcode.IGET_BOOLEAN,
@@ -23,10 +23,9 @@ object OnTouchEventHandlerFingerprint : MethodFingerprint(
         Opcode.INT_TO_FLOAT,
         Opcode.INVOKE_VIRTUAL,
         Opcode.MOVE_RESULT,
-        Opcode.IF_NEZ,
-        Opcode.RETURN,
+        Opcode.IF_EQZ,
         Opcode.INVOKE_VIRTUAL,
-        Opcode.INVOKE_VIRTUAL, // pMethodReference
+        Opcode.INVOKE_VIRTUAL, // oMethodReference
     ),
     customFingerprint = { methodDef, _ -> methodDef.name == "onTouchEvent" }
 )
