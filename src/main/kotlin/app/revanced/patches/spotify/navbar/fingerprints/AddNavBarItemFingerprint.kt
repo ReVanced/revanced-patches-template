b87005de0c40293f85d3997f43b353a87a925156
oSumAtrIX <johan.melkonyan1@web.de>
2023-11-23 19:35:41 +0100

    fix(Spotify - Hide premium navbar): Support latest version

---
+++ src/main/kotlin/app/revanced/patches/spotify/navbar/fingerprints/AddNavBarItemFingerprint.kt
@@ -0,0 +1,12 @@
+package app.revanced.patches.spotify.navbar.fingerprints
+
+import app.revanced.patcher.extensions.or
+import app.revanced.patches.spotify.navbar.PremiumNavbarTabResourcePatch
+import app.revanced.util.patch.LiteralValueFingerprint
+import com.android.tools.smali.dexlib2.AccessFlags
+
+object AddNavBarItemFingerprint : LiteralValueFingerprint(
+     returnType = "V",
+     accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
+     literalSupplier = { PremiumNavbarTabResourcePatch.showBottomNavigationItemsTextId },
+)
