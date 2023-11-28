a69d91635b3d5722f85972df6898673656d1fafe
Alex Uskov <FrozenAlex@users.noreply.github.com>
2023-11-28 12:39:32 +0400

    Reduce size of fingerprints

--- src/main/kotlin/app/revanced/patches/twitter/misc/links/fingerprints/AddTelemetryToLinkFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/twitter/misc/links/fingerprints/AddTelemetryToLinkFingerprint.kt
@@ -6,9 +6,5 @@ import com.android.tools.smali.dexlib2.Opcode
 
 // Adds telemetry to the share links
 object AddTelemetryToLinkFingerprint : MethodFingerprint(
-    accessFlags = AccessFlags.PUBLIC.value or AccessFlags.STATIC.value or AccessFlags.FINAL.value,
-    opcodes = listOf(Opcode.RETURN_OBJECT),
-    returnType = "Ljava/lang/String;",
-    parameters = listOf("Ljava/lang/String;", "I", "Ljava/lang/String;"),
     strings = listOf("<this>", "shareParam", "sessionToken"),
 )
--- src/main/kotlin/app/revanced/patches/twitter/misc/links/fingerprints/LinkBuilderMethodFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/twitter/misc/links/fingerprints/LinkBuilderMethodFingerprint.kt
@@ -6,9 +6,5 @@ import com.android.tools.smali.dexlib2.Opcode
 
 // Returns a shareable link string based on a tweet ID and a username
 object LinkBuilderMethodFingerprint : MethodFingerprint(
-    accessFlags = AccessFlags.PUBLIC.value or AccessFlags.STATIC.value,
-    opcodes = listOf(Opcode.RETURN_OBJECT),
-    returnType = "Ljava/lang/String;",
-    parameters = listOf("J", "Ljava/lang/String;"),
     strings = listOf("/%1\$s/status/%2\$d"),
 )
