1227ea2d8a16dfcece6b8db41c6e461b2c62e66b
KobeW50 <84587632+KobeW50@users.noreply.github.com>
2023-11-30 17:03:08 -0500

    fix(YouTube - Custom player overlay opacity): Remove unneeded punctuation in patch description

--- src/main/kotlin/app/revanced/patches/youtube/layout/player/overlay/CustomPlayerOverlayOpacityPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/player/overlay/CustomPlayerOverlayOpacityPatch.kt
@@ -13,7 +13,7 @@ import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 
 @Patch(
     name = "Custom player overlay opacity",
-    description = "Change the opacity of the player background, when player controls are visible.",
+    description = "Change the opacity of the player background when player controls are visible.",
     dependencies = [CustomPlayerOverlayOpacityResourcePatch::class],
     compatiblePackages = [
         CompatiblePackage("com.google.android.youtube")
