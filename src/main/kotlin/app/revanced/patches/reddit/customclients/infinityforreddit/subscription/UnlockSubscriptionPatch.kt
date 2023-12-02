840b29e9891243a4ccabb6ade5ad6ac92984efc2
oSumAtrIX <johan.melkonyan1@web.de>
2023-12-02 17:27:12 +0100

    feat(Infinity for Reddit): Add `Unlock subscription` patch

---
+++ src/main/kotlin/app/revanced/patches/reddit/customclients/infinityforreddit/subscription/UnlockSubscriptionPatch.kt
@@ -0,0 +1,24 @@
+package app.revanced.patches.reddit.customclients.infinityforreddit.subscription
+
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patches.reddit.customclients.infinityforreddit.api.SpoofClientPatch
+import app.revanced.patches.reddit.customclients.infinityforreddit.subscription.fingerprints.StartSubscriptionActivityFingerprint
+import app.revanced.util.Utils.returnEarly
+
+@Patch(
+    name = "Unlock subscription",
+    description = "Unlocks the subscription feature but requires a custom client ID.",
+    dependencies = [SpoofClientPatch::class],
+    compatiblePackages = [
+        CompatiblePackage("ml.docilealligator.infinityforreddit")
+    ]
+)
+@Suppress("unused")
+object UnlockSubscriptionPatch : BytecodePatch(
+    setOf(StartSubscriptionActivityFingerprint)
+) {
+    override fun execute(context: BytecodeContext) = listOf(StartSubscriptionActivityFingerprint).returnEarly()
+}
