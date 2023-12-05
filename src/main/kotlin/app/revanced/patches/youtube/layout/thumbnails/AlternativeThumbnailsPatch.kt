7775c25e68e9f332917eac65cf48a8230e12c0b2
oSumAtrIX <johan.melkonyan1@web.de>
2023-12-05 19:01:06 +0100

    refactor & fix strings

--- src/main/kotlin/app/revanced/patches/youtube/layout/thumbnails/AlternativeThumbnailsPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/thumbnails/AlternativeThumbnailsPatch.kt
@@ -1,6 +1,5 @@
 package app.revanced.patches.youtube.layout.thumbnails
 
-import app.revanced.util.exception
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
@@ -12,6 +11,7 @@ import app.revanced.patches.shared.settings.preference.impl.*
 import app.revanced.patches.youtube.layout.thumbnails.fingerprints.*
 import app.revanced.patches.youtube.misc.integrations.IntegrationsPatch
 import app.revanced.patches.youtube.misc.settings.SettingsPatch
+import app.revanced.util.exception
 
 @Patch(
     name = "Alternative thumbnails",
@@ -90,14 +90,20 @@ object AlternativeThumbnailsPatch : BytecodePatch(
                 listOf(
                     ListPreference(
                         "revanced_alt_thumbnail_mode",
-                        StringResource("revanced_alt_thumbnail_mode_title", "Thumbnail Mode"),
+                        StringResource("revanced_alt_thumbnail_mode_title", "Thumbnail mode"),
                         ArrayResource(
                             "revanced_alt_thumbnail_mode_entries",
                             listOf(
-                                StringResource("revanced_alt_thumbnail_mode_entry_1", "Creator-Provided Thumbnails"),
-                                StringResource("revanced_alt_thumbnail_mode_entry_2", "YouTube Video Stills"),
-                                StringResource("revanced_alt_thumbnail_mode_entry_3", "DeArrow, fallback to Creator-Provided Thumbnails"),
-                                StringResource("revanced_alt_thumbnail_mode_entry_4", "DeArrow, fallback to YouTube Video Stills")
+                                StringResource("revanced_alt_thumbnail_mode_entry_1", "Original thumbnails"),
+                                StringResource("revanced_alt_thumbnail_mode_entry_2", "YouTube video stills"),
+                                StringResource(
+                                    "revanced_alt_thumbnail_mode_entry_3",
+                                    "DeArrow, fallback to original thumbnails"
+                                ),
+                                StringResource(
+                                    "revanced_alt_thumbnail_mode_entry_4",
+                                    "DeArrow, fallback to YouTube video stills"
+                                )
                             )
                         ),
                         ArrayResource(
@@ -135,27 +141,39 @@ object AlternativeThumbnailsPatch : BytecodePatch(
                         StringResource("revanced_alt_thumbnail_fast_quality_title", "Use fast alternative thumbnails"),
                         StringResource(
                             "revanced_alt_thumbnail_fast_quality_summary_on",
-                            "Using medium quality stills. Thumbnails will load faster, but live streams, unreleased, or very old videos may show blank thumbnails"
+                            "Using medium quality stills. " +
+                                    "Thumbnails will load faster, but live streams, unreleased, " +
+                                    "or very old videos may show blank thumbnails"
                         ),
                         StringResource("revanced_alt_thumbnail_fast_quality_summary_off", "Using high quality stills")
                     ),
                     TextPreference(
                         "revanced_alt_thumbnail_dearrow_api_url",
-                        StringResource("revanced_alt_thumbnail_dearrow_api_url_title", "DeArrow Thumbnail Cache Endpoint"),
-                        StringResource("revanced_alt_thumbnail_dearrow_api_url_summary", "The URL of the DeArrow thumbnail cache endpoint. This should not be changed unless you know what you are doing."),
+                        StringResource(
+                            "revanced_alt_thumbnail_dearrow_api_url_title",
+                            "DeArrow Thumbnail Cache Endpoint"
+                        ),
+                        StringResource(
+                            "revanced_alt_thumbnail_dearrow_api_url_summary",
+                            "The URL of the DeArrow thumbnail cache endpoint. " +
+                                    "This should not be changed unless you know what you are doing."
+                        ),
                     ),
                     NonInteractivePreference(
-                        StringResource("revanced_alt_thumbnail_about_title", "About Video Stills"),
+                        StringResource("revanced_alt_thumbnail_about_stills_title", "About video stills"),
                         StringResource(
-                            "revanced_alt_thumbnail_about_summary",
-                            "Alternative thumbnails are still images from the beginning/middle/end of each video. No external API is used, as these images are built into YouTube"
+                            "revanced_alt_thumbnail_about_stills_summary",
+                            "Video stills are still images from the beginning/middle/end of each video. " +
+                                    "No external API is used, as these images are built into YouTube"
                         )
                     ),
                     NonInteractivePreference(
                         StringResource("revanced_alt_thumbnail_about_dearrow_title", "About DeArrow"),
                         StringResource(
                             "revanced_alt_thumbnail_about_dearrow_summary",
-                            "DeArrow provides crowdsourced thumbnails for YouTube videos. These thumbnails are often more relevant than the ones provided by YouTube. Using DeArrow will send video URLs to DeArrows servers. No other data is sent."
+                            "DeArrow provides crowd-sourced thumbnails for YouTube videos. " +
+                                    "These thumbnails are often more relevant than the ones provided by YouTube. " +
+                                    "Using DeArrow will send requests to DeArrows servers. No other data is sent."
                         )
                     )
                 ),
