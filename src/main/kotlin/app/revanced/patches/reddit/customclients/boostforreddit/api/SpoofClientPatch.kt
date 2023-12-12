42a5de98becee7fc027c3e7143e071a3447f7077
oSumAtrIX <johan.melkonyan1@web.de>
2023-12-12 22:42:35 +0100

    fix(Boost for Reddit - Spoof client): Fix account login by removing user agent patch

--- src/main/kotlin/app/revanced/patches/reddit/customclients/boostforreddit/api/SpoofClientPatch.kt
+++ src/main/kotlin/app/revanced/patches/reddit/customclients/boostforreddit/api/SpoofClientPatch.kt
@@ -4,7 +4,6 @@ import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.fingerprint.MethodFingerprintResult
 import app.revanced.patches.reddit.customclients.AbstractSpoofClientPatch
-import app.revanced.patches.reddit.customclients.Constants.OAUTH_USER_AGENT
 import app.revanced.patches.reddit.customclients.boostforreddit.api.fingerprints.GetClientIdFingerprint
 import app.revanced.patches.reddit.customclients.boostforreddit.api.fingerprints.LoginActivityOnCreateFingerprint
 
@@ -24,21 +23,5 @@ object SpoofClientPatch : AbstractSpoofClientPatch(
                  return-object v0
             """
         )
-    }
-
-    override fun Set<MethodFingerprintResult>.patchUserAgent(context: BytecodeContext) {
-        first().let { result ->
-            result.mutableMethod.apply {
-                val insertIndex = result.scanResult.patternScanResult!!.endIndex
-
-                addInstructions(
-                    insertIndex,
-                    """
-                        const-string v7, "$OAUTH_USER_AGENT"
-                        invoke-virtual {v4, v7}, Landroid/webkit/WebSettings;->setUserAgentString(Ljava/lang/String;)V
-                    """
-                )
-            }
-        }
     }
 }
