c61c2cfebf84c4eb1d69b4c9fb7e2dc298e334e2
oSumAtrIX <johan.melkonyan1@web.de>
2024-04-05 21:17:39 +0200

    chore: Fix spelling mistake

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
     mavenLocal()
     google()
     maven {
-        // A repository must be speficied for some reason. "registry" is a dummy.
+        // A repository must be specified for some reason. "registry" is a dummy.
         url = uri("https://maven.pkg.github.com/revanced/registry")
         credentials {
             username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
