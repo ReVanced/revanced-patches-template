8f722ba501c227fa69c2098bb4ad7e331d1b9958
Pun Butrach <pun.butrach@gmail.com>
2026-01-08 18:13:25 +0700

    build: Use Gradle credentials system

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,10 +7,7 @@ pluginManagement {
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/revanced/registry")
-            credentials {
-                username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
-                password = providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")
-            }
+            credentials(PasswordCredentials::class)
         }
     }
 }
